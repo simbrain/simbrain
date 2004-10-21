58386206bb54cfe49ae850a72254d751f4eddba7
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2004-10-21 22:04:03 +0000

    Updated scripts for Windows.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,20 +1,19 @@
-SET CLASSPATH=bin;lib\utils.jar;lib\piccolo.jar;lib\piccolox.jar;lib\hisee.jar;lib\simnet.jar;lib\Jama-1.0.1.jar;lib\calpahtml.jar;lib\castor.jar;lib\xerxes.jar;lib\jlinalg.jar
+SET CLASSPATH=src;lib\utils.jar;lib\piccolo.jar;lib\piccolox.jar;lib\hisee.jar;lib\simnet.jar;lib\Jama-1.0.1.jar;lib\calpahtml.jar;lib\castor.jar;lib\xerxes.jar;lib\jlinalg.jar;lib\snarli.jar
 cd ..
-javac -classpath %CLASSPATH% src\org\simbrain\network\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\network\old\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\network\dialogs\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\network\pnodes\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\util\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\simulation\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\world\*.java 
-javac -classpath %CLASSPATH% src\org\simbrain\resource\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\interfaces\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\networks\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\util\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\synapses\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\synapses\rules\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\neurons\*.java 
-javac -classpath %CLASSPATH% src\org\simnet\neurons\rules\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\old\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\dialog\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\pnodes\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\util\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\simulation\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\world\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\resource\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\interfaces\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\networks\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\util\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\rules\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\neurons\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\neurons\rules\*.java 
 cd scripts
-
--- scripts/run.bat
+++ scripts/run.bat
@@ -1,3 +1,3 @@
 cd..
-java -classpath src;lib\utils.jar;lib\piccolo.jar;lib\piccolox.jar;lib\hisee.jar;lib\simnet.jar;lib\Jama-1.0.1.jar;lib\calpahtml.jar;lib\snarli.jar;lib\castor.jar;lib\xerxes.jar\lib\jlinalg.jar org.simbrain.sim.simulation.Simulation
-cd bin 
+java -classpath bin;lib\utils.jar;lib\piccolo.jar;lib\piccolox.jar;lib\hisee.jar;lib\simnet.jar;lib\Jama-1.0.1.jar;lib\calpahtml.jar;lib\snarli.jar;lib\castor.jar;lib\xerxes.jar\lib\jlinalg.jar org.simbrain.simulation.Simulation
+cd scripts 

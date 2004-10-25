0da1846e1eb187ec81c1acda3b484a65fc8a9e50
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2004-10-25 02:58:52 +0000

    Updated archive file

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,4 +1,4 @@
 export ARCHIVE_DIR="Documents/SimbrainARKIVES/zipped"
 gnutar --exclude=*.gif --exclude=.svn -czvf temp.tar.gz ./src
-mv temp.tar.gz src2`date +%m%d%y`.tar.gz
+mv temp.tar.gz src`date +%m%d%y`.tar.gz
 mv *.gz $HOME/$ARCHIVE_DIR

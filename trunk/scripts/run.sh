179c2c2d11ab6f06d38bc3cc7cab95f647bc8634
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-05-21 07:42:27 +0000

    Made unix / os x build and run scripts easier to use

--- trunk/scripts/run.sh
+++ trunk/scripts/run.sh
@@ -1,1 +1,3 @@
+cd ..
 java -classpath .:./bin:./lib/utils.jar:./lib/hisee.jar:./lib/Jama-1.0.1.jar:./lib/simnet.jar:./lib/piccolo.jar:./lib/piccolox.jar:./lib.:./lib/calpahtml.jar:./lib/castor.jar:./lib/snarli.jar:./lib/jlinalg.jar:./lib/xerxes.jar org.simbrain.simulation.Simulation
+cd scripts

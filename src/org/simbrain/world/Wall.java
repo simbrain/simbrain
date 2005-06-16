efea449c328eb33f1d786f0e1eb5fba1ea06fbfc
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-06-16 20:16:17 +0000

    Made world walls persistable.

--- src/org/simbrain/world/Wall.java
+++ src/org/simbrain/world/Wall.java
@@ -1,16 +1,43 @@
+/*
+ * Part of Simbrain--a java-based neural network kit
+ * Copyright (C) 2005 Jeff Yoshimi <www.jeffyoshimi.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.simbrain.world;
+
 
 import java.awt.Point;
 
 import javax.swing.JPanel;
 
-public class Wall extends JPanel {
+/**
+ * <b>Wall</b> represents a wall in the world.
+ * 
+ * @author Ryan Bartley
+ */
+public class Wall {
 	
 	private int upperLeftX;
 	private int upperLeftY;
 	private int width;
 	private int height;
 	
+	public Wall(){		
+	}
+
 	/**
 	 * @return Returns the height.
 	 */
@@ -59,9 +86,5 @@ public class Wall extends JPanel {
 	public void setWidth(int width) {
 		this.width = width;
 	}
-	public Wall(){
-		
-	}
-	
 
 }

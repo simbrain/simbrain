1192f79357996d43ade0d6f28e28eb8ba499ebb6
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-07-27 00:34:51 +0000

    Activation and increment are common to all neuron panels

--- trunk/src/org/simbrain/network/dialog/BinaryNeuronPanel.java
+++ trunk/src/org/simbrain/network/dialog/BinaryNeuronPanel.java
@@ -25,18 +25,14 @@ import org.simnet.neurons.BinaryNeuron;
 
 public class BinaryNeuronPanel extends AbstractNeuronPanel {
 	
-	private JTextField tfActivation = new JTextField();
-	private JTextField tfIncrement = new JTextField();
 	private JTextField tfUpValue = new JTextField();
 	private JTextField tfLowValue = new JTextField();
 	private JTextField tfThreshold = new JTextField();
 	
 	public BinaryNeuronPanel(){
-		this.addItem("Activation", tfActivation);
 		this.addItem("Threshold", tfThreshold);
 		this.addItem("Upper value", tfUpValue);
 		this.addItem("Lower value", tfLowValue);
-		this.addItem("Increment", tfIncrement);	
 	}
 	
 	 
@@ -46,60 +42,44 @@ public class BinaryNeuronPanel extends AbstractNeuronPanel {
 	public void fillFieldValues() {
 		BinaryNeuron neuron_ref = (BinaryNeuron)neuron_list.get(0);
 		
-		tfActivation.setText(Double.toString(neuron_ref.getActivation()));
 		tfLowValue.setText(Double.toString(neuron_ref.getLowerValue()));
 		tfUpValue.setText(Double.toString(neuron_ref.getUpperValue()));
-		tfIncrement.setText(Double.toString(neuron_ref.getIncrement()));
 		tfThreshold.setText(Double.toString(neuron_ref.getThreshold()));
 
 		//Handle consistency of multiple selections
-		if(!NetworkUtils.isConsistent(neuron_list, BinaryNeuron.class, "getActivation")) {
-			tfActivation.setText(NULL_STRING);
-		}
 		if(!NetworkUtils.isConsistent(neuron_list, BinaryNeuron.class, "getLowerValue")) {
 			tfLowValue.setText(NULL_STRING);
 		}	
 		if(!NetworkUtils.isConsistent(neuron_list, BinaryNeuron.class, "getUpperValue")) {
 			tfUpValue.setText(NULL_STRING);
 		}	
-		if(!NetworkUtils.isConsistent(neuron_list, BinaryNeuron.class, "getIncrement")) {
-			tfIncrement.setText(NULL_STRING);
-		}
 		if(!NetworkUtils.isConsistent(neuron_list, BinaryNeuron.class, "getThreshold")) {
 			tfThreshold.setText(NULL_STRING);
 		}
 	}
 	
     /**
-	 * Called externally when the dialog is closed, to commit any changes made
-	 */
-	public void commitChanges() {
-   	
-	for (int i = 0; i < neuron_list.size(); i++) {
-		BinaryNeuron neuron_ref = (BinaryNeuron) neuron_list.get(i);
+     * Called externally when the dialog is closed, to commit any changes made
+     */
+    public void commitChanges() {
 
-		if (tfActivation.getText().equals(NULL_STRING) == false) {
-			neuron_ref.setActivation(
-				Double.parseDouble(tfActivation.getText()));
-		}
-		if (tfUpValue.getText().equals(NULL_STRING) == false) {
-			neuron_ref.setUpperValue(
-				Double.parseDouble(tfUpValue.getText()));
-		}
-		if (tfLowValue.getText().equals(NULL_STRING) == false) {
-			neuron_ref.setLowerValue(
-				Double.parseDouble(tfLowValue.getText()));
-		}
-		if (tfThreshold.getText().equals(NULL_STRING) == false) {
-			neuron_ref.setThreshold(
-				Double.parseDouble(tfThreshold.getText()));
-		}
-		if (tfIncrement.getText().equals(NULL_STRING) == false) {
-			neuron_ref.setIncrement(
-				Double.parseDouble(tfIncrement.getText()));
-		}
-	}
+        for (int i = 0; i < neuron_list.size(); i++) {
+            BinaryNeuron neuron_ref = (BinaryNeuron) neuron_list.get(i);
 
-   }
+            if (tfUpValue.getText().equals(NULL_STRING) == false) {
+                neuron_ref.setUpperValue(Double
+                        .parseDouble(tfUpValue.getText()));
+            }
+            if (tfLowValue.getText().equals(NULL_STRING) == false) {
+                neuron_ref.setLowerValue(Double.parseDouble(tfLowValue
+                        .getText()));
+            }
+            if (tfThreshold.getText().equals(NULL_STRING) == false) {
+                neuron_ref.setThreshold(Double.parseDouble(tfThreshold
+                        .getText()));
+            }
+        }
+
+    }
 
 }

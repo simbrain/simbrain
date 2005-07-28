1b011ca9f23fba68d7ad7b03c082c6f904dd19c2
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-07-28 20:55:46 +0000

    Added "Activation function", Bias and Decay to StandardNeuronPanel

--- trunk/src/org/simbrain/network/dialog/StandardNeuronPanel.java
+++ trunk/src/org/simbrain/network/dialog/StandardNeuronPanel.java
@@ -33,10 +33,16 @@ public class StandardNeuronPanel extends AbstractNeuronPanel {
 	
 	private JTextField tfUpBound = new JTextField();
 	private JTextField tfLowBound = new JTextField();
+	private JTextField tfDecay = new JTextField();
+	private JTextField tfBias = new JTextField();
+	private JComboBox cbActivationRule = new JComboBox(ActivationRule.getList());
 	
 	public StandardNeuronPanel(){
+		this.addItem("Activation function", cbActivationRule);
 		this.addItem("Upper bound", tfUpBound);
 		this.addItem("Lower bound", tfLowBound);
+		this.addItem("Bias", tfBias);
+		this.addItem("Decay", tfDecay);	
 	}
 	
 	 
@@ -44,18 +50,32 @@ public class StandardNeuronPanel extends AbstractNeuronPanel {
 	 * Populate fields with current data
 	 */
 	public void fillFieldValues() {
-		Neuron neuron_ref = (Neuron)neuron_list.get(0);
+		StandardNeuron neuron_ref = (StandardNeuron)neuron_list.get(0);
+		
+		cbActivationRule.setSelectedIndex(ActivationRule.getActivationFunctionIndex(neuron_ref.getActivationFunction().getName()));
 
 		tfLowBound.setText(Double.toString(neuron_ref.getLowerBound()));
 		tfUpBound.setText(Double.toString(neuron_ref.getUpperBound()));
+		tfBias.setText(Double.toString(neuron_ref.getBias()));
+		tfDecay.setText(Double.toString(neuron_ref.getDecay()));
 
 		//Handle consistency of multiple selections
+		if(!NetworkUtils.isConsistent(neuron_list, StandardNeuron.class, "getActivationFunctionS")) {
+			cbActivationRule.addItem(NULL_STRING);
+			cbActivationRule.setSelectedIndex(ActivationRule.getList().length);
+		}
 		if(!NetworkUtils.isConsistent(neuron_list, StandardNeuron.class, "getLowerBound")) {
 			tfLowBound.setText(NULL_STRING);
 		}	
 		if(!NetworkUtils.isConsistent(neuron_list, StandardNeuron.class, "getUpperBound")) {
 			tfUpBound.setText(NULL_STRING);
 		}
+		if(!NetworkUtils.isConsistent(neuron_list, StandardNeuron.class, "getBias")) {
+			tfBias.setText(NULL_STRING);
+		}	
+		if(!NetworkUtils.isConsistent(neuron_list, StandardNeuron.class, "getDecay")) {
+			tfDecay.setText(NULL_STRING);
+		}	
 	}
 	
 
@@ -67,6 +87,9 @@ public class StandardNeuronPanel extends AbstractNeuronPanel {
         for (int i = 0; i < neuron_list.size(); i++) {
             StandardNeuron neuron_ref = (StandardNeuron) neuron_list.get(i);
 
+    		if (cbActivationRule.getSelectedItem().equals(NULL_STRING)== false) {
+    			neuron_ref.setActivationFunction(ActivationRule.getActivationFunction(cbActivationRule.getSelectedItem().toString()));
+    		}
             if (tfUpBound.getText().equals(NULL_STRING) == false) {
                 neuron_ref.setUpperBound(Double
                         .parseDouble(tfUpBound.getText()));
@@ -75,6 +98,13 @@ public class StandardNeuronPanel extends AbstractNeuronPanel {
                 neuron_ref.setLowerBound(Double.parseDouble(tfLowBound
                         .getText()));
             }
+    		if (tfDecay.getText().equals(NULL_STRING) == false) {
+    			neuron_ref.setDecay(
+    				Double.parseDouble(tfDecay.getText()));
+    		}
+    		if (tfBias.getText().equals(NULL_STRING) == false) {
+    			neuron_ref.setBias(Double.parseDouble(tfBias.getText()));
+    		}
 
         }
 

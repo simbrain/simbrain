b32444007d9a3ae174c7a7e5887b18d555a0b9ec
rbartley <rbartley@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-08-11 09:24:54 +0000

    bptDialog thread is now an inner class of bptdialog

--- src/org/simbrain/network/dialog/BackpropTrainingDialog.java
+++ src/org/simbrain/network/dialog/BackpropTrainingDialog.java
@@ -32,6 +32,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import org.simbrain.network.NetworkPanel;
 import org.simbrain.network.NetworkThread;
@@ -242,4 +243,48 @@ public class BackpropTrainingDialog extends StandardDialog implements ActionList
 		
 	}
 
+	
+	public class BPTDialogThread extends Thread {
+
+		private BackpropTrainingDialog dialog = null;
+		private volatile boolean isRunning = false;
+		
+		Runnable iterate = new Runnable() {
+			public void run(){
+				dialog.iterate();
+			}
+		};
+		
+		public BPTDialogThread(BackpropTrainingDialog dialog){
+			this.dialog = dialog;
+		}
+
+		
+		public void run(){
+			try {
+				while(isRunning == true){
+					dialog.setUpdateCompleted(false);
+					SwingUtilities.invokeLater(iterate);
+					while(!dialog.isUpdateCompleted()){
+						sleep(10);
+					}
+				}	
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+			
+		public boolean isRunning() {
+			return isRunning;
+		}
+
+		public void setRunning(boolean isRunning) {
+			this.isRunning = isRunning;
+		}
+
+	}
+
+	
+	
 }

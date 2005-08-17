9b7e3dfba4b09c4e49de4d9ddc9f9943bd18b121
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-08-17 01:37:29 +0000

    Forgot to add this with last commit.

---
+++ trunk/src/org/simnet/interfaces/SpikingNeuron.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Aug 16, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.simnet.interfaces;
+
+/**
+ * @author jyoshimi
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface SpikingNeuron {
+
+	public boolean hasSpiked();
+}

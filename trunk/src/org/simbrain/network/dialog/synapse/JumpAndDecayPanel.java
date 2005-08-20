c0ec8751620b91b50510514241e7505b4a3ca856
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-08-20 00:09:17 +0000

    Added conventions to deal with fillFieldValues and commitChanges

--- trunk/src/org/simbrain/network/dialog/synapse/JumpAndDecayPanel.java
+++ trunk/src/org/simbrain/network/dialog/synapse/JumpAndDecayPanel.java
@@ -2,8 +2,8 @@ package org.simbrain.network.dialog.synapse;
 
 import javax.swing.JTextField;
 
+import org.simbrain.network.NetworkUtils;
 import org.simnet.synapses.spikeresponders.JumpAndDecay;
-import org.simnet.synapses.spikeresponders.Step;
 
 public class JumpAndDecayPanel extends AbstractSpikeResponsePanel {
     
@@ -12,13 +12,29 @@ public class JumpAndDecayPanel extends AbstractSpikeResponsePanel {
     private JTextField tfDecayRate = new JTextField();
     
     public JumpAndDecayPanel(){
+        tfJumpHeight.setColumns(6);
         this.addItem("Jump height", tfJumpHeight);
         this.addItem("Base-line", tfBaseLine);
         this.addItem("Decay rate", tfDecayRate);
     }
 
     public void fillFieldValues() {
-        // TODO Auto-generated method stub
+        JumpAndDecay spikeResponder = (JumpAndDecay)spikeResponderList.get(0);
+        
+        tfJumpHeight.setText(Double.toString(spikeResponder.getJumpHeight()));
+        tfBaseLine.setText(Double.toString(spikeResponder.getBaseLine()));
+        tfDecayRate.setText(Double.toString(spikeResponder.getDecayRate()));
+        
+        //Handle consistency of multiply selections
+        if(!NetworkUtils.isConsistent(spikeResponderList, JumpAndDecay.class, "getJumpHeight")) {
+            tfJumpHeight.setText(NULL_STRING);
+        }
+        if(!NetworkUtils.isConsistent(spikeResponderList, JumpAndDecay.class, "getBaseLine")) {
+            tfBaseLine.setText(NULL_STRING);
+        }
+        if(!NetworkUtils.isConsistent(spikeResponderList, JumpAndDecay.class, "getDecayRate")) {
+            tfDecayRate.setText(NULL_STRING);
+        }
 
     }
 
@@ -30,8 +46,21 @@ public class JumpAndDecayPanel extends AbstractSpikeResponsePanel {
     }
 
     public void commitChanges() {
-        // TODO Auto-generated method stub
 
+        for (int i = 0; i < spikeResponderList.size(); i++) {
+            JumpAndDecay spiker_ref = (JumpAndDecay) spikeResponderList.get(i);
+            if (tfJumpHeight.getText().equals(NULL_STRING) == false) {
+                spiker_ref.setJumpHeight(Double.parseDouble(tfJumpHeight
+                        .getText()));
+            }
+            if (tfBaseLine.getText().equals(NULL_STRING) == false) {
+                spiker_ref.setBaseLine(Double.parseDouble(tfBaseLine
+                        .getText()));
+            }
+            if (tfDecayRate.getText().equals(NULL_STRING) == false) {
+                spiker_ref.setDecayRate(Double.parseDouble(tfDecayRate
+                        .getText()));
+            }
+        }
     }
-
 }

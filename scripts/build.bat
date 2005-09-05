80c80bf1b21f5d1c6056b782f8e78fb9f69bc7a0
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-09-05 19:29:27 +0000

    Updated to reflect changes in build paths

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,23 +1,30 @@
 SET CLASSPATH=src;lib\utils.jar;lib\piccolo.jar;lib\piccolox.jar;lib\simnet.jar;lib\Jama-1.0.1.jar;lib\castor.jar;lib\xerxes.jar;lib\jlinalg.jar;lib\snarli.jar
 cd ..
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\dialog\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\pnodes\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\util\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\workspace\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\coupling\*.java
 javac -d bin -classpath %CLASSPATH% src\org\simbrain\gauge\*.java 
 javac -d bin -classpath %CLASSPATH% src\org\simbrain\gauge\core\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\gauge\graphics\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\world\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simbrain\world\world\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\gauge\graphics\*.java  
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\dialog\*.java
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\dialog\network\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\dialog\neuron\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\dialog\synapse\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\network\pnodes\*.java 
 javac -d bin -classpath %CLASSPATH% src\org\simbrain\resource\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\util\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\workspace\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\world\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\world\dataworld\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simbrain\world\odorworld\*.java 
+
 javac -d bin -classpath %CLASSPATH% src\org\simnet\*.java 
 javac -d bin -classpath %CLASSPATH% src\org\simnet\interfaces\*.java 
 javac -d bin -classpath %CLASSPATH% src\org\simnet\networks\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simnet\util\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\*.java 
-javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\rules\*.java 
 javac -d bin -classpath %CLASSPATH% src\org\simnet\neurons\*.java 
 javac -d bin -classpath %CLASSPATH% src\org\simnet\neurons\rules\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\rules\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\synapses\spikeresponders\*.java 
+javac -d bin -classpath %CLASSPATH% src\org\simnet\util\*.java 
 copy src\org\simbrain\resource\*.gif bin\org\simbrain\resource
 cd scripts

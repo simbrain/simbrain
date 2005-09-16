895c950ee6b67d1ddc3ccb9dc38dbd41610247ce
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-09-16 04:03:40 +0000

    Corrected documentation

--- src/org/simnet/interfaces/ActivationRule.java
+++ src/org/simnet/interfaces/ActivationRule.java
@@ -1,8 +1,20 @@
 /*
- * Created on Sep 13, 2004
+ * Part of Simbrain--a java-based neural network kit
+ * Copyright (C) 2003 Jeff Yoshimi <www.jeffyoshimi.net>
  *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package org.simnet.interfaces;
 
@@ -14,12 +26,7 @@ import org.simnet.neurons.rules.Sigmoidal;
 import org.simnet.neurons.rules.Tanh;
 import org.simnet.neurons.rules.Threshold;
 
-/**
- * @author yoshimi
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+
 public abstract class ActivationRule {
 
 	private static String[] functionList = { "Linear", "Threshold", "Sigmoidal", "Clamped", "Random", "Tanh", "Identity"};

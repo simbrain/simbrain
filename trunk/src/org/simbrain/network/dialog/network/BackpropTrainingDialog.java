425d8c6db7f70c33d73246467ec2eb3e16f66251
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-10-27 19:55:32 +0000

    Committed other network dialogs.

---
+++ trunk/src/org/simbrain/network/dialog/network/BackpropTrainingDialog.java
@@ -0,0 +1,275 @@
+/*
+ * Part of Simbrain--a java-based neural network kit
+ * Copyright (C) 2005 Jeff Yoshimi <www.jeffyoshimi.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.simbrain.network.dialog.network;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import org.simbrain.network.NetworkPanel;
+import org.simbrain.resource.ResourceManager;
+import org.simbrain.util.LabelledItemPanel;
+import org.simbrain.util.SFileChooser;
+import org.simbrain.util.StandardDialog;
+import org.simbrain.util.Utils;
+import org.simnet.networks.Backprop;
+
+
+/**
+ * <b>BackpropTrainingDialog</b> is a dialog box for training backprop networks
+ */
+public class BackpropTrainingDialog extends StandardDialog implements ActionListener, WindowListener {
+    private LabelledItemPanel mainPanel = new LabelledItemPanel();
+    private JButton jbInputsFile = new JButton("None selected");
+    private JButton jbOutputsFile = new JButton("None selected");
+    private JTextField tfEpochs = new JTextField();
+    private JTextField tfEta = new JTextField();
+    private JTextField tfMu = new JTextField();
+    private JTextField tfErrorInterval = new JTextField();
+    private JButton jbRandomize = new JButton("Randomize");
+    private JButton jbTrain = new JButton("Train");
+    private JButton jbPlay = new JButton(ResourceManager.getImageIcon("Play.gif"));
+    private JButton jbStep = new JButton(ResourceManager.getImageIcon("Step.gif"));
+    private JLabel rmsError = new JLabel();
+    double[][] inputs_train;
+    double[][] outputs_train;
+    private boolean updateCompleted = false;
+    private NetworkPanel parentPanel;
+    private Backprop theNet;
+    private BPTDialogThread theThread = null;
+
+    /**
+     * This method is the default constructor.
+     */
+    public BackpropTrainingDialog(NetworkPanel parent, Backprop bp) {
+        parentPanel = parent;
+        theNet = bp;
+        init();
+    }
+
+    /**
+     * This method initialises the components on the panel.
+     */
+    private void init() {
+        //Initialize Dialog
+        setTitle("Train Backprop Network");
+        fillFieldValues();
+        this.setLocation(600, 0); //Sets location of network dialog
+        this.setSize(new Dimension(300, 200));
+
+        //Set up grapics panel
+        mainPanel.addItem("Input file", jbInputsFile);
+        mainPanel.addItem("Output file", jbOutputsFile);
+        mainPanel.addItem("Epochs", tfEpochs);
+        mainPanel.addItem("Learning rate", tfEta);
+        mainPanel.addItem("Momentum", tfMu);
+        mainPanel.addItem("Error Interval", tfErrorInterval);
+        mainPanel.addItem("Randomize network", jbRandomize);
+        mainPanel.addItem("Train network", jbTrain);
+        mainPanel.addItem("Play/Stop", jbPlay);
+        mainPanel.addItem("Step", jbStep);
+        mainPanel.addItem("RMSError", rmsError);
+
+        jbInputsFile.addActionListener(this);
+        jbOutputsFile.addActionListener(this);
+        jbRandomize.addActionListener(this);
+        jbTrain.addActionListener(this);
+        jbPlay.addActionListener(this);
+        jbStep.addActionListener(this);
+
+        setContentPane(mainPanel);
+        this.addWindowListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Object o = e.getSource();
+
+        if (o == jbInputsFile) {
+            SFileChooser chooser = new SFileChooser(parentPanel.getBackropDirectory(), "csv");
+            File theFile = chooser.showOpenDialog();
+
+            if (theFile == null) {
+                return;
+            }
+
+            parentPanel.setBackropDirectory(chooser.getCurrentLocation());
+            inputs_train = Utils.getDoubleMatrix(theFile);
+            jbInputsFile.setText(theFile.getName());
+            theNet.setTraining_inputs(inputs_train);
+        } else if (o == jbOutputsFile) {
+            SFileChooser chooser = new SFileChooser(parentPanel.getBackropDirectory(), "csv");
+            File theFile = chooser.showOpenDialog();
+
+            if (theFile == null) {
+                return;
+            }
+
+            parentPanel.setBackropDirectory(chooser.getCurrentLocation());
+            outputs_train = Utils.getDoubleMatrix(theFile);
+            jbOutputsFile.setText(theFile.getName());
+            theNet.setTraining_outputs(outputs_train);
+        } else if (o == jbRandomize) {
+            theNet.randomize();
+            parentPanel.renderObjects();
+            parentPanel.repaint();
+        } else if (o == jbTrain) {
+            setValues();
+            theNet.train();
+            parentPanel.renderObjects();
+            parentPanel.repaint();
+        } else if (o == jbPlay) {
+            setValues();
+
+            if (theThread == null) {
+                theThread = new BPTDialogThread(this);
+            }
+
+            if (theThread.isRunning() == false) {
+                jbPlay.setIcon(ResourceManager.getImageIcon("Stop.gif"));
+                theThread.setRunning(true);
+                theThread.start();
+            } else {
+                jbPlay.setIcon(ResourceManager.getImageIcon("Play.gif"));
+
+                if (theThread == null) {
+                    return;
+                }
+
+                theThread.setRunning(false);
+                theThread = null;
+            }
+        } else if (o == jbStep) {
+            setValues();
+            iterate();
+        }
+    }
+
+    public void iterate() {
+        theNet.iterate();
+        parentPanel.renderObjects();
+        parentPanel.repaint();
+        rmsError.setText(Double.toString(theNet.getOut().getRMSError()));
+        updateCompleted = true;
+    }
+
+    /**
+     * Populate fields with current data
+     */
+    public void fillFieldValues() {
+        tfEpochs.setText("" + theNet.getEpochs());
+        tfEta.setText("" + theNet.getEta());
+        tfMu.setText("" + theNet.getMu());
+        tfErrorInterval.setText("" + theNet.getError_interval());
+    }
+
+    /**
+     * Set projector values based on fields
+     */
+    public void setValues() {
+        theNet.setEpochs(Integer.parseInt(tfEpochs.getText()));
+        theNet.setEta(Double.parseDouble(tfEta.getText()));
+        theNet.setMu(Double.parseDouble(tfMu.getText()));
+        theNet.setError_interval(Integer.parseInt(tfErrorInterval.getText()));
+    }
+
+    public boolean isUpdateCompleted() {
+        return updateCompleted;
+    }
+
+    public void setUpdateCompleted(boolean updateCompleted) {
+        this.updateCompleted = updateCompleted;
+    }
+
+    public void windowActivated(WindowEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void windowClosed(WindowEvent arg0) {
+        if (theThread != null) {
+            theThread.setRunning(false);
+            theThread = null;
+        }
+    }
+
+    public void windowClosing(WindowEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void windowDeactivated(WindowEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void windowDeiconified(WindowEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void windowIconified(WindowEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public void windowOpened(WindowEvent arg0) {
+        // TODO Auto-generated method stub
+    }
+
+    public class BPTDialogThread extends Thread {
+        private BackpropTrainingDialog dialog = null;
+        private volatile boolean isRunning = false;
+        Runnable iterate = new Runnable() {
+                public void run() {
+                    dialog.iterate();
+                }
+            };
+
+        public BPTDialogThread(BackpropTrainingDialog dialog) {
+            this.dialog = dialog;
+        }
+
+        public void run() {
+            try {
+                while (isRunning == true) {
+                    dialog.setUpdateCompleted(false);
+                    SwingUtilities.invokeLater(iterate);
+
+                    while (!dialog.isUpdateCompleted()) {
+                        sleep(10);
+                    }
+                }
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public boolean isRunning() {
+            return isRunning;
+        }
+
+        public void setRunning(boolean isRunning) {
+            this.isRunning = isRunning;
+        }
+    }
+}
---
+++ trunk/src/org/simbrain/network/dialog/network/HopfieldDialog.java
@@ -0,0 +1,154 @@
+/*
+ * Part of Simbrain--a java-based neural network kit
+ * Copyright (C) 2005 Jeff Yoshimi <www.jeffyoshimi.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.simbrain.network.dialog.network;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+
+import org.simbrain.util.LabelledItemPanel;
+import org.simbrain.util.StandardDialog;
+
+import com.Ostermiller.util.CSVParser;
+
+
+/**
+ * <b>HopfieldDialog</b> is a dialog box for creating hopfield networks.
+ */
+public class HopfieldDialog extends StandardDialog implements ActionListener {
+    private static final String FS = System.getProperty("file.separator");
+    public static final int DISCRETE = 0;
+    public static final int CONTINUOUS = 1;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JPanel tabLogic = new JPanel();
+    private JPanel tabLayout = new JPanel();
+    private LabelledItemPanel logicPanel = new LabelledItemPanel();
+    private LayoutPanel layoutPanel = new LayoutPanel();
+    private JTextField numberOfUnits = new JTextField();
+    private JComboBox cbType = new JComboBox(new String[] {"Discrete", "Continuous" });
+    private JButton trainingFile = new JButton("Set");
+    private String[][] values = null;
+
+    /**
+     * This method is the default constructor.
+     */
+    public HopfieldDialog() {
+        init();
+    }
+
+    /**
+     * This method initialises the components on the panel.
+     */
+    private void init() {
+        //Initialize Dialog
+        setTitle("New Hopfield Network");
+        fillFieldValues();
+        this.setLocation(500, 0); //Sets location of network dialog
+        layoutPanel.setCurrentLayout(LayoutPanel.GRID);
+        layoutPanel.initPanel();
+
+        trainingFile.addActionListener(this);
+
+        //Set up grapics panel
+        logicPanel.addItem("Type", cbType);
+        logicPanel.addItem("Number of Units", numberOfUnits);
+        logicPanel.addItem("Set training file", trainingFile);
+
+        //Set up tab panel
+        tabLogic.add(logicPanel);
+        tabLayout.add(layoutPanel);
+        tabbedPane.addTab("Logic", logicPanel);
+        tabbedPane.addTab("Layout", layoutPanel);
+        setContentPane(tabbedPane);
+    }
+
+    /**
+     * Populate fields with current data
+     */
+    public void fillFieldValues() {
+    }
+
+    /**
+     * Set values based on fields
+     */
+    public void getValues() {
+    }
+
+    public String getCurrentLayout() {
+        return layoutPanel.getCurrentLayout();
+    }
+
+    public int getNumUnits() {
+        return Integer.parseInt(numberOfUnits.getText());
+    }
+
+    public int getType() {
+        if (cbType.getSelectedIndex() == 0) {
+            return DISCRETE;
+        } else {
+            return CONTINUOUS;
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        loadFile();
+    }
+
+    private void loadFile() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("." + FS + "simulations" + FS + "networks"));
+
+        int result = chooser.showDialog(this, "Open");
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            readFile(chooser.getSelectedFile());
+        }
+    }
+
+    public void readFile(File theFile) {
+        CSVParser theParser = null;
+
+        try {
+            theParser = new CSVParser(new FileInputStream(theFile), "", "", "#"); // # is a comment delimeter in net files
+            values = theParser.getAllValues();
+        } catch (java.io.FileNotFoundException e) {
+            JOptionPane.showMessageDialog(
+                                          null, "Could not find the file \n" + theFile, "Warning",
+                                          JOptionPane.ERROR_MESSAGE);
+
+            return;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(
+                                          null, "There was a problem opening the file \n" + theFile, "Warning",
+                                          JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+
+            return;
+        }
+    }
+}

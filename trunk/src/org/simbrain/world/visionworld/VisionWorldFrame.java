1f40594f7b17f91ab3cdbcac45b23f21b1ca1467
rbartley <rbartley@bf00e721-664e-0410-b0bc-3daef764c4be>
2005-11-01 06:28:41 +0000

    VisionWorldFrame menu (first draft) in place

--- trunk/src/org/simbrain/world/visionworld/VisionWorldFrame.java
+++ trunk/src/org/simbrain/world/visionworld/VisionWorldFrame.java
@@ -1,19 +1,26 @@
 package org.simbrain.world.visionworld;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import org.simbrain.util.LabelledItemPanel;
 import org.simbrain.workspace.Workspace;
 
 // Need javadoc
-public class VisionWorldFrame extends JInternalFrame implements ComponentListener {
+public class VisionWorldFrame extends JInternalFrame implements ComponentListener,  ActionListener {
 
     private VisionWorld world;
 //    private static final String FS = "/"; //System.getProperty("file.separator");Separator();
@@ -30,7 +37,7 @@ public class VisionWorldFrame extends JInternalFrame implements ComponentListene
 
 
     //Loader methods for visionworld
-    public VisionWorldFrame(Workspace ws) {
+    public VisionWorldFrame(final Workspace ws) {
         this.workspace = ws;
 
         world = new VisionWorld(this);
@@ -41,33 +48,58 @@ public class VisionWorldFrame extends JInternalFrame implements ComponentListene
         this.setClosable(true);
         this.addComponentListener(this);
         this.getContentPane().setLayout(new BorderLayout());
-        this.getContentPane().add("Center", world);
+        this.getContentPane().add("Center",  world);
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
-        buttonPanel.add(new JButton("Send"));
-        this.getContentPane().add("South", buttonPanel);
+        JButton send = new JButton("Send");
+        send.addActionListener(this);
+        send.setActionCommand("Send");
+        buttonPanel.add(send);
+        this.getContentPane().add("South",  buttonPanel);
         this.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+
+        createMenus();
 
         setVisible(true);
 
+    }
+
+    private void createMenus() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenu settings = new JMenu("Settings");
+        JMenu help = new JMenu("Help");
+        JMenuItem settingsItem = new JMenuItem("Pixels");
+        JMenuItem helpItem = new JMenuItem("Help");
+        settingsItem.addActionListener(this);
+        settingsItem.setActionCommand("pixels");
+        helpItem.addActionListener(this);
+        helpItem.setActionCommand("help");
+        settings.add(settingsItem);
+        help.add(helpItem);
+        menuBar.add(file);
+        menuBar.add(settings);
+        menuBar.add(help);
+        this.setJMenuBar(menuBar);
     }
 
     public VisionWorld getWorld() {
         return world;
     }
 
-    public void componentResized(ComponentEvent arg0) {
+
+    public void componentResized(final ComponentEvent arg0) {
         world.rebuild();
         pack();
     }
 
-    public void componentMoved(ComponentEvent arg0) {
+    public void componentMoved(final ComponentEvent arg0) {
         // TODO Auto-generated method stub
 
     }
 
-    public void componentShown(ComponentEvent arg0) {
+    public void componentShown(final ComponentEvent arg0) {
         // TODO Auto-generated method stub
 
     }
@@ -77,6 +109,77 @@ public class VisionWorldFrame extends JInternalFrame implements ComponentListene
 
     }
 
+    public void actionPerformed(final ActionEvent e) {
+        if (e.getActionCommand().equals("Send")) {
+            send();
+        } else if (e.getActionCommand().equals("pixels")) {
+            showPixelDialog();
+        } else if (e.getActionCommand().equals("help")) {
+            showHelp();
+        }
+    }
+
+
+
+    private void showHelp() {
+    }
+
+	private void showPixelDialog() {
+        final JDialog dia = new JDialog(workspace, "Pixel Settings");
+        dia.setLayout(new BorderLayout());
+        dia.setModal(true);
+        dia.setLocationRelativeTo(this);
+        final JTextField width = new JTextField(world.getNumPixelsRow());
+        final JTextField height = new JTextField(world.getNumPixelsColumn());
+        LabelledItemPanel pan = new LabelledItemPanel();
+        pan.addItem("Width (in pixels)", width);
+        pan.addItem("Height(in pixels)", height);
+        dia.getContentPane().add(pan, BorderLayout.CENTER);
+        ActionListener list = new ActionListener() {
+
+            public void actionPerformed(final ActionEvent e) {
+                if (((JButton) e.getSource()).getText().equals("OK")) {
+                    int w = 0, h = 0;
+                    try {
+                        w = Integer.parseInt(width.getText());
+                    } catch (NumberFormatException e1) {
+                        width.grabFocus();
+                        width.selectAll();
+                    }
+                    try {
+                        h = Integer.parseInt(height.getText());
+                    } catch (NumberFormatException e1) {
+                        height.grabFocus();
+                        height.selectAll();
+                    }
+                    if (w != 0 && h != 0) {
+                        world.redimension(w, h);
+                        world.rebuild();
+                        dia.setVisible(false);
+                    }
+                } else if (((JButton) e.getSource()).getText().equals("Cancel")) {
+                    dia.setVisible(false);
+                }
+            }
+        };
+
+        JPanel buttPan = new JPanel();
+        JButton ok = new JButton("OK");
+        JButton cancel = new JButton("Cancel");
+        ok.addActionListener(list);
+        cancel.addActionListener(list);
+        buttPan.add(ok);
+        buttPan.add(cancel);
+        dia.getContentPane().add(buttPan, BorderLayout.PAGE_END);
+        dia.pack();
+        dia.setVisible(true);
+    }
+
+
+
+    private void send() {
+    }
+
 
 
 
@@ -84,8 +187,5 @@ public class VisionWorldFrame extends JInternalFrame implements ComponentListene
 
 
     //filehandling methods
-
-    //menu handling
-
 
 }

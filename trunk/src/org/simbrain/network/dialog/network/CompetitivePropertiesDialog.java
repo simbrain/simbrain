a21cb0ec875fe101642249a69e656fb6dc22dff3
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2006-01-31 21:11:05 +0000

    Forgot to add with last commit.

---
+++ trunk/src/org/simbrain/network/dialog/network/CompetitivePropertiesDialog.java
@@ -0,0 +1,62 @@
+package org.simbrain.network.dialog.network;
+
+import javax.swing.JTextField;
+
+import org.simbrain.util.LabelledItemPanel;
+import org.simbrain.util.StandardDialog;
+import org.simnet.networks.Competitive;
+import org.simnet.networks.WinnerTakeAll;
+
+public class CompetitivePropertiesDialog extends StandardDialog {
+
+    /** Main Panel. */
+    private LabelledItemPanel mainPanel = new LabelledItemPanel();
+
+    /** Epsilon value field. */
+    private JTextField epsilon = new JTextField();
+
+    /** Winner value field. */
+    private JTextField winnerValue = new JTextField();
+
+    /** Loser value field. */
+    private JTextField loserValue = new JTextField();
+
+    /** The model subnetwork. */
+    private Competitive competitive;
+
+    /**
+     * Default constructor.
+     *
+     * @param wta WinnerTakeAll network being modified.
+     */
+    public CompetitivePropertiesDialog(final Competitive competitive) {
+        this.competitive = competitive;
+        setTitle("Set Competitive Properties");
+        fillFieldValues();
+        this.setLocation(500, 0); //Sets location of network dialog
+
+        mainPanel.addItem("Epsilon", epsilon);
+        mainPanel.addItem("Winner Value", winnerValue);
+        mainPanel.addItem("Loser Value", loserValue);
+        setContentPane(mainPanel);
+    }
+
+    /**
+     * Called when dialog closes.
+     */
+    protected void closeDialogOk() {
+      competitive.setEpsilon(Double.parseDouble(epsilon.getText()));
+      competitive.setWinValue(Double.parseDouble(winnerValue.getText()));
+      competitive.setLoseValue(Double.parseDouble(loserValue.getText()));
+      super.closeDialogOk();
+    }
+
+    /**
+     * Populate fields with current data.
+     */
+    public void fillFieldValues() {
+        epsilon = new JTextField("" + competitive.getEpsilon());
+        loserValue = new JTextField("" + competitive.getLoseValue());
+        winnerValue = new JTextField("" + competitive.getWinValue());
+    }
+}

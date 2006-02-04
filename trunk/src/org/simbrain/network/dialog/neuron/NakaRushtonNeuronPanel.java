135572745a520e95d6ae10fe2bf22bd6cbaaf82e
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2006-02-04 23:44:01 +0000

    Minor simnet changes.

--- trunk/src/org/simbrain/network/dialog/neuron/NakaRushtonNeuronPanel.java
+++ trunk/src/org/simbrain/network/dialog/neuron/NakaRushtonNeuronPanel.java
@@ -32,25 +32,35 @@ import org.simnet.neurons.NakaRushtonNeuron;
 
 
 /**
- * <b>NakaRushtonNeuronPanel</b>
+ * <b>NakaRushtonNeuronPanel</b>.
  */
 public class NakaRushtonNeuronPanel extends AbstractNeuronPanel {
-    private JTextField tfMaxSpikeRate = new JTextField();
+    /** Steepness field. */
     private JTextField tfSteepness = new JTextField();
+    /** Semi saturation field. */
     private JTextField tfSemiSaturation = new JTextField();
+    /** Time step field. */
     private JTextField tfTimeStep = new JTextField();
+    /** Time constant field. */
     private JTextField tfTimeConstant = new JTextField();
+    /** Noise combo box. */
     private TristateDropDown tsNoise = new TristateDropDown();
+    /** Tabbed pane. */
     private JTabbedPane tabbedPane = new JTabbedPane();
+    /** Main tab. */
     private LabelledItemPanel mainTab = new LabelledItemPanel();
+    /** Random tab. */
     private RandomPanel randTab = new RandomPanel(true);
 
+    /**
+     * Creates a new  Naka-Rushton neuron panel.
+     * @param net Reference to network
+     */
     public NakaRushtonNeuronPanel(final Network net) {
         parentNet = net;
 
         this.add(tabbedPane);
         mainTab.addItem("Time step", tfTimeStep);
-        mainTab.addItem("Maximum spike rate", tfMaxSpikeRate);
         mainTab.addItem("Steepness", tfSteepness);
         mainTab.addItem("Semi-saturation constant", tfSemiSaturation);
         mainTab.addItem("Time constant", tfTimeConstant);
@@ -59,21 +69,19 @@ public class NakaRushtonNeuronPanel extends AbstractNeuronPanel {
         tabbedPane.add(randTab, "Noise");
     }
 
+    /**
+     * Populate fields with current data.
+     */
     public void fillFieldValues() {
-        NakaRushtonNeuron neuron_ref = (NakaRushtonNeuron) neuron_list.get(0);
+        NakaRushtonNeuron neuronRef = (NakaRushtonNeuron) neuron_list.get(0);
 
-        tfMaxSpikeRate.setText(Double.toString(neuron_ref.getMaximumSpikeRate()));
-        tfSemiSaturation.setText(Double.toString(neuron_ref.getSemiSaturationConstant()));
-        tfSteepness.setText(Double.toString(neuron_ref.getSteepness()));
-        tfTimeConstant.setText(Double.toString(neuron_ref.getTimeConstant()));
+        tfSemiSaturation.setText(Double.toString(neuronRef.getSemiSaturationConstant()));
+        tfSteepness.setText(Double.toString(neuronRef.getSteepness()));
+        tfTimeConstant.setText(Double.toString(neuronRef.getTimeConstant()));
         tfTimeStep.setText(Double.toString(parentNet.getTimeStep()));
-        tsNoise.setSelected(neuron_ref.getAddNoise());
+        tsNoise.setSelected(neuronRef.getAddNoise());
 
         //Handle consistency of multiple selections
-        if (!NetworkUtils.isConsistent(neuron_list, NakaRushtonNeuron.class, "getMaximumSpikeRate")) {
-            tfMaxSpikeRate.setText(NULL_STRING);
-        }
-
         if (!NetworkUtils.isConsistent(neuron_list, NakaRushtonNeuron.class, "getTimeConstant")) {
             tfTimeConstant.setText(NULL_STRING);
         }
@@ -93,6 +101,9 @@ public class NakaRushtonNeuronPanel extends AbstractNeuronPanel {
         randTab.fillFieldValues(getRandomizers());
     }
 
+    /**
+     * @return an arraylist of randomizers.
+     */
     private ArrayList getRandomizers() {
         ArrayList ret = new ArrayList();
 
@@ -103,44 +114,45 @@ public class NakaRushtonNeuronPanel extends AbstractNeuronPanel {
         return ret;
     }
 
+    /**
+     * Fill field values to default values for this synapse type.
+     */
     public void fillDefaultValues() {
-        NakaRushtonNeuron neuron_ref = new NakaRushtonNeuron();
-        tfMaxSpikeRate.setText(Double.toString(neuron_ref.getMaximumSpikeRate()));
-        tfSemiSaturation.setText(Double.toString(neuron_ref.getSemiSaturationConstant()));
-        tfSteepness.setText(Double.toString(neuron_ref.getSteepness()));
-        tfTimeConstant.setText(Double.toString(neuron_ref.getTimeConstant()));
+        NakaRushtonNeuron neuronRef = new NakaRushtonNeuron();
+        tfSemiSaturation.setText(Double.toString(neuronRef.getSemiSaturationConstant()));
+        tfSteepness.setText(Double.toString(neuronRef.getSteepness()));
+        tfTimeConstant.setText(Double.toString(neuronRef.getTimeConstant()));
         tfTimeStep.setText(Double.toString(parentNet.getTimeStep()));
-        tsNoise.setSelected(neuron_ref.getAddNoise());
+        tsNoise.setSelected(neuronRef.getAddNoise());
         randTab.fillDefaultValues();
     }
 
+    /**
+     * Called externally when the dialog is closed, to commit any changes made.
+     */
     public void commitChanges() {
         parentNet.setTimeStep(Double.parseDouble(tfTimeStep.getText()));
 
         for (int i = 0; i < neuron_list.size(); i++) {
-            NakaRushtonNeuron neuron_ref = (NakaRushtonNeuron) neuron_list.get(i);
+            NakaRushtonNeuron neuronRef = (NakaRushtonNeuron) neuron_list.get(i);
 
-            if (tfMaxSpikeRate.getText().equals(NULL_STRING) == false) {
-                neuron_ref.setMaximumSpikeRate(Double.parseDouble(tfMaxSpikeRate.getText()));
+            if (!tfTimeConstant.getText().equals(NULL_STRING)) {
+                neuronRef.setTimeConstant(Double.parseDouble(tfTimeConstant.getText()));
             }
 
-            if (tfTimeConstant.getText().equals(NULL_STRING) == false) {
-                neuron_ref.setTimeConstant(Double.parseDouble(tfTimeConstant.getText()));
+            if (!tfSemiSaturation.getText().equals(NULL_STRING)) {
+                neuronRef.setSemiSaturationConstant(Double.parseDouble(tfSemiSaturation.getText()));
             }
 
-            if (tfSemiSaturation.getText().equals(NULL_STRING) == false) {
-                neuron_ref.setSemiSaturationConstant(Double.parseDouble(tfSemiSaturation.getText()));
+            if (!tfSteepness.getText().equals(NULL_STRING)) {
+                neuronRef.setSteepness(Double.parseDouble(tfSteepness.getText()));
             }
 
-            if (tfSteepness.getText().equals(NULL_STRING) == false) {
-                neuron_ref.setSteepness(Double.parseDouble(tfSteepness.getText()));
+            if (!tsNoise.isNull()) {
+                neuronRef.setAddNoise(tsNoise.isSelected());
             }
 
-            if (tsNoise.isNull() == false) {
-                neuron_ref.setAddNoise(tsNoise.isSelected());
-            }
-
-            randTab.commitRandom(neuron_ref.getNoiseGenerator());
+            randTab.commitRandom(neuronRef.getNoiseGenerator());
         }
     }
 }

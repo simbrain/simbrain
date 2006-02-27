91cf1f5d69462d0aa13e4dc9f4a69975d7bbc527
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2006-02-27 05:26:40 +0000

    Fixed bug with LMS Neurons, added a class I left out of a previous commit, and updated a few networks.

---
+++ src/org/simnet/networks/LMSNetwork.java
@@ -0,0 +1,253 @@
+/*
+ * Part of Simbrain--a java-based neural network kit
+ * Copyright (C) 2005 Jeff Yoshimi <www.jeffyoshimi.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.simnet.networks;
+
+import org.simnet.interfaces.ComplexNetwork;
+import org.simnet.interfaces.Neuron;
+import org.simnet.interfaces.Synapse;
+import org.simnet.layouts.Layout;
+import org.simnet.neurons.ClampedNeuron;
+import org.simnet.neurons.LinearNeuron;
+import org.simnet.neurons.SigmoidalNeuron;
+import org.simnet.util.ConnectNets;
+
+
+/**
+ * <b>LMSNetwork</b> implements a least means squared network.
+ */
+public class LMSNetwork extends ComplexNetwork {
+
+    /** Input layer of LMSNetwork network. */
+    private StandardNetwork inputLayer;
+
+    /** Output layer of LMSNetwork network. */
+    private StandardNetwork outputLayer;
+
+    /** Default number of input units. */
+    private int defaultInputs = 3;
+
+    /** Default number of output units. */
+    private int defaultOutputs = 3;
+
+    /** Number of epochs. */
+    private int epochs = 1000;
+
+    /** Current error. */
+    private double rmsError;
+
+    /** Learning rate. */
+    private double eta = .5;
+
+    /** Input portion of training corpus. */
+    private double[][] trainingInputs;
+
+    /** Output portion of training corpus. */
+    private double[][] trainingOutputs;
+
+    /**
+     * Default constructor.
+     *
+     */
+    public LMSNetwork() {
+        super();
+    }
+
+    /**
+     * Construct a backprop network with a specified number of input and output layers.
+     *
+     * @param nInputs number of input nodes
+     * @param nOutputs number of output nodes
+     * @param layout layout of the new nodes
+     */
+    public LMSNetwork(final int nInputs, final int nOutputs, final Layout layout) {
+        super();
+        buildNetwork(nInputs, nInputs);
+        layout.layoutNeurons(this);
+    }
+
+    /**
+     * Build the network.
+     *
+     * @param nInputs number of input nodes
+     * @param nOutputs number of output nodes
+     */
+    private void buildNetwork(final int nInputs, final int nOutputs) {
+        inputLayer = new StandardNetwork();
+        outputLayer = new StandardNetwork();
+
+        for (int i = 0; i < nInputs; i++) {
+            inputLayer.addNeuron(new ClampedNeuron());
+        }
+
+        for (int i = 0; i < nOutputs; i++) {
+            outputLayer.addNeuron(getDefaultNeuron());
+        }
+
+        addNetwork(inputLayer);
+        addNetwork(outputLayer);
+
+        ConnectNets.oneWayFull(this, inputLayer, outputLayer);
+
+        for (int i = 0; i < getFlatSynapseList().size(); i++) {
+            ((Synapse) getFlatSynapseList().get(i)).setUpperBound(10);
+            ((Synapse) getFlatSynapseList().get(i)).setLowerBound(-10);
+            ((Synapse) getFlatSynapseList().get(i)).randomize();
+        }
+
+        for (int i = 0; i < getFlatNeuronList().size(); i++) {
+            ((Neuron) getFlatNeuronList().get(i)).setUpperBound(1);
+            ((Neuron) getFlatNeuronList().get(i)).setLowerBound(-1);
+            ((Neuron) getFlatNeuronList().get(i)).setIncrement(1);
+        }
+
+    }
+
+    /**
+     * Return the default neuron, with settings, for backprop nets.
+     *
+     * @return the neuron, with appropriate settings, that should be used in
+     *         building a backprop net.
+     */
+    protected LinearNeuron getDefaultNeuron() {
+        LinearNeuron ret = new LinearNeuron();
+        ret.setLowerBound(-10);
+        ret.setUpperBound(10);
+        return ret;
+    }
+
+    /**
+     * The core update function of the neural network. Calls the current update function on each neuron, decays all the
+     * neurons, and checks their bounds.
+     */
+    public void update() {
+        time++;
+        updateAllNetworks();
+        // Perhaps this should move explicitly from input to output layers?
+        checkAllBounds();
+    }
+
+    /**
+     * Train the network.
+     */
+    public void train() {
+        for (int i = 0; i < epochs; i++) {
+            iterate();
+        }
+    }
+
+    /**
+     * Iterate network training.
+     */
+    public void iterate() {
+        rmsError = 0;
+        for (int row = 0; row < trainingInputs.length; row++) {
+            for (int i = 0; i < trainingInputs[row].length; i++) {
+                inputLayer.getNeuron(i).setActivation(trainingInputs[row][i]);
+            }
+            outputLayer.update();
+            for (int i = 0; i < trainingInputs[row].length; i++) {
+                for (int j = 0; j < trainingOutputs[row].length; j++) {
+                    Synapse s = inputLayer.getWeight(i,j);                    
+                    double error  = trainingOutputs[row][j] - outputLayer.getNeuron(j).getActivation();
+                    rmsError += (error * error);
+                    s.setStrength(s.getStrength() + (eta * error *  trainingInputs[row][i]));                    
+                }
+            }
+        }
+        inputLayer.clearActivations();
+        outputLayer.clearActivations();
+        rmsError = Math.sqrt(rmsError/(trainingInputs[0].length * trainingOutputs[0].length));
+    }
+
+    public double getRMSError() {
+        return rmsError;
+    }
+
+    /**
+     * @return Returns the epochs.
+     */
+    public int getEpochs() {
+        return epochs;
+    }
+
+    /**
+     * @param epochs The epochs to set.
+     */
+    public void setEpochs(final int epochs) {
+        this.epochs = epochs;
+    }
+
+    /**
+     * @return Returns the eta.
+     */
+    public double getEta() {
+        return eta;
+    }
+
+    /**
+     * @param eta The eta to set.
+     */
+    public void setEta(final double eta) {
+        this.eta = eta;
+    }
+
+    /**
+     * @return Returns the training_inputs.
+     */
+    public double[][] getTrainingInputs() {
+        return trainingInputs;
+    }
+
+    /**
+     * @param trainingInputs The trainingInputs to set.
+     */
+    public void setTrainingInputs(final double[][] trainingInputs) {
+        this.trainingInputs = trainingInputs;
+    }
+
+    /**
+     * @return Returns the trainingOutputs.
+     */
+    public double[][] getTrainingOutputs() {
+        return trainingOutputs;
+    }
+
+    /**
+     * @param trainingOutputs The trainingOutputs to set.
+     */
+    public void setTrainingOutputs(final double[][] trainingOutputs) {
+        this.trainingOutputs = trainingOutputs;
+    }
+
+    /**
+     * @return Returns the defaultInputs.
+     */
+    public int getDefaultInputs() {
+        return defaultInputs;
+    }
+
+    /**
+     * @return Returns the defaultOutputs.
+     */
+    public int getDefaultOutputs() {
+        return defaultOutputs;
+    }
+
+
+}

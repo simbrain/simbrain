85bc7eefc48ade1f8420c9ddef7a1cdebcbc2263
kbaron <kbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2006-02-28 00:04:25 +0000

    Created clamp neurons menu item.

---
+++ trunk/src/org/simbrain/network/actions/ClampNeuronsAction.java
@@ -0,0 +1,51 @@
+
+package org.simbrain.network.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JCheckBoxMenuItem;
+
+import org.simbrain.network.NetworkPanel;
+import org.simbrain.resource.ResourceManager;
+
+/**
+ * Clamps weights action.
+ */
+public final class ClampNeuronsAction
+    extends AbstractAction {
+
+    /** Network panel. */
+    private final NetworkPanel networkPanel;
+
+
+    /**
+     * Create a new clamp weights action with the specified
+     * network panel.
+     *
+     * @param networkPanel networkPanel, must not be null
+     */
+    public ClampNeuronsAction(final NetworkPanel networkPanel) {
+
+        super("Clamp Neurons");
+
+        if (networkPanel == null) {
+            throw new IllegalArgumentException("networkPanel must not be null");
+        }
+
+        this.networkPanel = networkPanel;
+
+        putValue(SMALL_ICON, ResourceManager.getImageIcon("Clamp.gif"));
+    }
+
+    /** @see AbstractAction */
+    public void actionPerformed(final ActionEvent event) {
+
+        // Perform action
+        JCheckBoxMenuItem cb = (JCheckBoxMenuItem) event.getSource();
+
+        // Determine status
+        networkPanel.getNetwork().setClampNeurons(cb.isSelected());
+
+    }
+}

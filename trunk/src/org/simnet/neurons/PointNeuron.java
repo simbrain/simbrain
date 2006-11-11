a3c314c688d5ba7f461599f5298cc5ed958a5447
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2006-11-11 11:15:24 +0000

    Provisional implementation of input machinery for Point neurons, but highly inefficient!

--- trunk/src/org/simnet/neurons/PointNeuron.java
+++ trunk/src/org/simnet/neurons/PointNeuron.java
@@ -18,15 +18,20 @@
  */
 package org.simnet.neurons;
 
+import java.util.ArrayList;
+
 import org.simnet.interfaces.Neuron;
 import org.simnet.interfaces.Synapse;
 
 
 /**
  * <b>PointNeuron</b> from O'Reilley and Munakata, Computational Explorations in Cognitive Neuroscience, chapter 2.
+ * All references to charts, etc. below are to chapter 2 of this book.
  */
 public class PointNeuron extends Neuron {
 	
+    //TODO: Steal javadocs from PointNeuronPanel
+    // TODO: Rename badly named variables below
     double ExcitatoryReversal = 55;
     double InhibitoryReversal = -70;
     double LeakReversal = -70;
@@ -36,9 +41,15 @@ public class PointNeuron extends Neuron {
     private int NONE = 0;
 	private int SIGMOIDAL = 1;
     private int outputFunction = NONE;
+    private double norm_factor = 1;
+    private double time_averaging = .7;
+    private double bias = 0;
+    double previous_excitatory_current = 0;
+    private ArrayList<Synapse> excitatoryInputs = new ArrayList<Synapse>();
+    private ArrayList<Synapse> inhibitoryInputs = new ArrayList<Synapse>();
 
     
-	double TimeStep, output, voltage;
+	double TimeStep, output, voltage, current;
 	
 	private static String[] functionList = {"None", "Sigmoidal" };
     /**
@@ -91,31 +102,21 @@ public class PointNeuron extends Neuron {
     }
 
     /**
-     * Update neuron.
+     * Update neuron.  See Box 2.2.  Note that projections are not currently used.
      */
     public void update() {
 
 
-		// Leak currents
-		double current = LeakConductance * (activation - LeakReversal);
-		if (fanIn.size() > 0) {
-			for (int j = 0; j < fanIn.size(); j++) {
-				Synapse synapse = (Synapse) fanIn.get(j);
-				Neuron source = synapse.getSource();
+		// Set currents
+        setInputLists();
+        current = LeakConductance * (activation - LeakReversal);
+        current += getInhibitoryNetInput() * (activation - InhibitoryReversal);
+        current +=  getExcitatoryNetInput() * (activation - ExcitatoryReversal);
 
-				if (synapse.getStrength() > 0) {
-					// Excitatory current
-					current += (source.getActivation() * synapse.getStrength() * (activation - ExcitatoryReversal));
-				} else {
-					// Inhibitory current
-					current += (source.getActivation() * -synapse.getStrength() * (activation - InhibitoryReversal));
-				}
-			}
-		}
+        // Update voltage
+		voltage = activation - this.getParentNetwork().getTimeStep() * current;
 
-		voltage = activation - this.getParentNetwork().getTimeStep()
-				* current;
-
+        // Apply output function
 		if (outputFunction == NONE) {
 	        output = voltage;
 		} else if (outputFunction == SIGMOIDAL) {
@@ -124,6 +125,65 @@ public class PointNeuron extends Neuron {
 
 	setBuffer(output);
 	
+    }
+        
+    /**
+     * Update the lists of excitatory and inhibitory currents based on synapse values.
+     * TODO: This is majorly slow!  only do it when synapses change!
+     */
+    private void setInputLists() {
+        excitatoryInputs.clear();
+        inhibitoryInputs.clear();
+        if (fanIn.size() > 0) {
+            for (int j = 0; j < fanIn.size(); j++) {
+                Synapse synapse = (Synapse) fanIn.get(j);
+                if (synapse.getStrength() > 0) {
+                    excitatoryInputs.add(synapse);
+                } else {
+                   inhibitoryInputs.add(synapse);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Returns the inhibitory currents.
+     *
+     * @return inhibitory current
+     */
+    private double getInhibitoryNetInput() {
+        double ret = 0;
+        if (inhibitoryInputs.size() > 0) {
+            for (int j = 0; j < inhibitoryInputs.size(); j++) {
+                Synapse synapse = inhibitoryInputs.get(j);
+                Neuron source = synapse.getSource();
+                ret += (source.getActivation() * synapse.getStrength());
+             }  
+             System.out.println("inhibitory = " + ret);            
+        }
+        return ret;
+    }
+    
+    /**
+     * Returns the excitatory net input.  See equation 2.16
+     *
+     * @return excitatory net input
+     */
+    private double getExcitatoryNetInput() {
+        double ret = 0;
+        if (excitatoryInputs.size() > 0) {
+            for (int j = 0; j < excitatoryInputs.size(); j++) {
+                Synapse synapse = excitatoryInputs.get(j);
+                Neuron source = synapse.getSource();
+                ret += (source.getActivation() * synapse.getStrength());
+             }  
+             // TODO: Ask David if N (fan_in.size()) is the same as total inputs or just excitatory inputs
+             ret = (1 - time_averaging) * previous_excitatory_current + time_averaging *
+                     (1 / (norm_factor * excitatoryInputs.size()) * ret + (bias / fanIn.size()));  
+             previous_excitatory_current = ret;
+             System.out.println("excitatory = " + ret);            
+        }
+        return ret;
     }
     
     /**
@@ -226,6 +286,48 @@ public class PointNeuron extends Neuron {
      */
     public void setThreshold(double threshold) {
         this.threshold = threshold;
+    }
+
+    /**
+     * @return Returns the bias.
+     */
+    public double getBias() {
+        return bias;
+    }
+
+    /**
+     * @param bias The bias to set.
+     */
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
+
+    /**
+     * @return Returns the norm_factor.
+     */
+    public double getNorm_factor() {
+        return norm_factor;
+    }
+
+    /**
+     * @param norm_factor The norm_factor to set.
+     */
+    public void setNorm_factor(double norm_factor) {
+        this.norm_factor = norm_factor;
+    }
+
+    /**
+     * @return Returns the time_averaging.
+     */
+    public double getTime_averaging() {
+        return time_averaging;
+    }
+
+    /**
+     * @param time_averaging The time_averaging to set.
+     */
+    public void setTime_averaging(double time_averaging) {
+        this.time_averaging = time_averaging;
     }
 
 

8d61d75b7f96cc24af5e6599de82099683c915b9
jyoshimi <jyoshimi@bf00e721-664e-0410-b0bc-3daef764c4be>
2007-05-03 04:14:48 +0000

    Group / Ungroup functions added.   Not completely working yet but pretty close.

--- src/org/simbrain/network/nodes/TextObject.java
+++ src/org/simbrain/network/nodes/TextObject.java
@@ -7,12 +7,25 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import javax.swing.JDialog;
+import javax.swing.JMenu;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 
 import org.simbrain.network.NetworkPanel;
+import org.simbrain.network.actions.CopyAction;
+import org.simbrain.network.actions.CutAction;
+import org.simbrain.network.actions.DeleteAction;
+import org.simbrain.network.actions.GroupAction;
+import org.simbrain.network.actions.PasteAction;
+import org.simbrain.network.actions.SetNeuronPropertiesAction;
+import org.simbrain.network.actions.SetSourceNeuronsAction;
+import org.simbrain.network.actions.UngroupAction;
+import org.simbrain.network.actions.connection.ConnectNeuronsAction;
+import org.simbrain.network.actions.connection.ConnectNeuronsSimpleAction;
+import org.simbrain.network.actions.connection.ShowConnectDialogAction;
+import org.simbrain.workspace.Workspace;
 
 import edu.umd.cs.piccolo.event.PBasicInputEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
@@ -44,61 +57,75 @@ public class TextObject extends ScreenElement implements PropertyChangeListener 
         addPropertyChangeListener(PROPERTY_FULL_BOUNDS, this);
     }
 
-    @Override
+    /** @Override. */
     public boolean isSelectable() {
         // TODO Auto-generated method stub
         return true;
     }
 
-    @Override
+    /** @Override. */
     public boolean showSelectionHandle() {
         // TODO Auto-generated method stub
         return true;
     }
 
-    @Override
+    /** @Override. */
     public boolean isDraggable() {
         // TODO Auto-generated method stub
         return true;
     }
 
-    @Override
+    /** @Override. */
     protected boolean hasToolTipText() {
         // TODO Auto-generated method stub
         return false;
     }
 
-    @Override
+    /** @Override. */
     protected String getToolTipText() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /** @Override. */
     protected boolean hasContextMenu() {
         // TODO Auto-generated method stub
-        return false;
+        return true;
     }
 
-    @Override
+    /** @Override. */
     protected JPopupMenu getContextMenu() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        JPopupMenu contextMenu = new JPopupMenu();
 
-    @Override
+        contextMenu.add(new CutAction(getNetworkPanel()));
+        contextMenu.add(new CopyAction(getNetworkPanel()));
+        contextMenu.add(new PasteAction(getNetworkPanel()));
+        contextMenu.addSeparator();
+
+        contextMenu.add(new GroupAction(getNetworkPanel()));
+        contextMenu.addSeparator();
+
+        contextMenu.add(new DeleteAction(getNetworkPanel()));
+        contextMenu.addSeparator();
+
+       contextMenu.add(new SetNeuronPropertiesAction(getNetworkPanel()));
+
+       return contextMenu;
+
+  }
+
+    /** @Override. */
     protected boolean hasPropertyDialog() {
-        // TODO Auto-generated method stub
         return false;
     }
 
-    @Override
+    /** @Override. */
     protected JDialog getPropertyDialog() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
+    /** @Override. */
     public void resetColors() {
         // TODO Auto-generated method stub
         

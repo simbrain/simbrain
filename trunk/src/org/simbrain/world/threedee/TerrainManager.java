5108f759897680a9223bfb24650831e115565ce9
jwatson <jwatson@bf00e721-664e-0410-b0bc-3daef764c4be>
2007-08-19 02:59:46 +0000

    changing TerrainManager to Terrain

--- trunk/src/org/simbrain/world/threedee/TerrainManager.java
+++ trunk/src/org/simbrain/world/threedee/TerrainManager.java
@@ -1,8 +1,5 @@
 package org.simbrain.world.threedee;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.swing.ImageIcon;
 
 import com.jme.bounding.BoundingBox;
@@ -15,12 +12,16 @@ import com.jmex.terrain.TerrainBlock;
 import com.jmex.terrain.util.MidPointHeightMap;
 import com.jmex.terrain.util.ProceduralTextureGenerator;
 
-public class TerrainManager {
-    private Map<Renderer, TerrainBlock> blocks = new HashMap<Renderer, TerrainBlock>();
+public class Terrain extends MultipleViewElement<TerrainBlock> {
     private MidPointHeightMap heightMap = new MidPointHeightMap(64, 1f);
-    private final TerrainBlock heightBlock = rawBlock();
+    private final TerrainBlock heightBlock = create();
     
-    private TerrainBlock rawBlock() {
+    public float getHeight(Vector3f location) {
+        return heightBlock.getHeight(location);
+    }
+    
+    @Override
+    protected TerrainBlock create() {
         Vector3f terrainScale = new Vector3f(4, 0.0575f, 4);
         TerrainBlock tb = new TerrainBlock("Terrain", heightMap.getSize(), terrainScale,
             heightMap.getHeightMap(), new Vector3f(0, 0, 0), false);
@@ -31,10 +32,9 @@ public class TerrainManager {
         return tb;
     }
     
-    private TerrainBlock createTerrainBlock(Renderer renderer) {
-        TerrainBlock tb = rawBlock();
-    
-        // generate a terrain texture with 2 textures
+    @Override
+    public void initSpatial(Renderer renderer, TerrainBlock block) {
+        /* generate a terrain texture with 2 textures */
         ProceduralTextureGenerator pt = new ProceduralTextureGenerator(heightMap);
         pt.addTexture(new ImageIcon(getClass().getClassLoader()
             .getResource("jmetest/data/texture/grassb.png")), -128, 0, 128);
@@ -44,31 +44,19 @@ public class TerrainManager {
             .getResource("jmetest/data/texture/highest.jpg")), 128, 255, 384);
         pt.createTexture(32);
        
-        // assign the texture to the terrain
+        /* assign the texture to the terrain */
         TextureState ts = renderer.createTextureState();
         Texture t1 = TextureManager.loadTexture(pt.getImageIcon().getImage(),
             Texture.MM_LINEAR_LINEAR, Texture.FM_LINEAR, true);
         
         ts.setTexture(t1, 0);
     
-        tb.setRenderState(ts);
-        tb.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
-        
-        return tb;
+        block.setRenderState(ts);
+        block.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
     }
-    
-    public TerrainBlock getTerrainBlock(Renderer renderer) {
-        TerrainBlock tb = blocks.get(renderer);
-        
-        if (tb == null) {
-            tb = createTerrainBlock(renderer);
-            blocks.put(renderer, tb);
-        }
-        
-        return tb;
-    }
-    
-    public float getHeight(Vector3f location) {
-        return heightBlock.getHeight(location);
+
+    @Override
+    public void updateSpatial(TerrainBlock block) {
+        /* no implementation */
     }
 }

3ede94879d1e54af86bda1c00990a8ff20ded169
jwatson <jwatson@bf00e721-664e-0410-b0bc-3daef764c4be>
2008-02-21 03:25:53 +0000

    more serialization changes.  zip in and out working, basic component and coupling serialization implemented

--- trunk/src/org/simbrain/workspace/WorkspaceSerializer.java
+++ trunk/src/org/simbrain/workspace/WorkspaceSerializer.java
@@ -67,168 +67,63 @@ public class WorkspaceSerializer {
         zipStream.finish();
     }
     
+    @SuppressWarnings("unchecked")
     public void deserialize(InputStream stream) throws IOException {
         Map<String, byte[]> entries = new HashMap<String, byte[]>();
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         
-        CachingInputStream cachedStream = new CachingInputStream(stream);
+        byte[] buffer = new byte[1024];
         
-        ZipInputStream zip = new ZipInputStream(cachedStream);
+        for (int read; (read = stream.read(buffer)) >= 0; ) {
+            bytes.write(buffer, 0, read);
+        }
         
-        try {
-            ArchiveContents contents = null;
-            WorkspaceComponentDeserializer componentDeserializer = new WorkspaceComponentDeserializer();
+        ZipInputStream zip = new ZipInputStream(new ByteArrayInputStream(bytes.toByteArray()));
+        
+        ArchiveContents contents = null;
+        WorkspaceComponentDeserializer componentDeserializer = new WorkspaceComponentDeserializer();
+        
+        System.out.println("deserializing");
+        
+        ZipEntry entry = zip.getNextEntry();
+        
+        for (ZipEntry next; entry != null; entry = next) {
+            next = zip.getNextEntry();
             
-            System.out.println("deserializing");
+            String name = entry.getName();
+            System.out.println("entry: " + name);
+            System.out.println("entry.getSize(): " + entry.getSize());
             
-            ZipEntry entry = zip.getNextEntry();
+            entries.put(name, new byte[(int) entry.getSize()]);
+        }
+        
+        zip = new ZipInputStream(new ByteArrayInputStream(bytes.toByteArray()));
+        
+        while ((entry = zip.getNextEntry()) != null) {
+            byte[] data = entries.get(entry.getName());
             
-            for (ZipEntry next; entry != null; entry = next) {
-                next = zip.getNextEntry();
-                
-                String name = entry.getName();
-                System.out.println("entry: " + name);
-                System.out.println("entry.getSize(): " + entry.getSize());
-                
-                byte[] last = cachedStream.getCached();
-                
-                System.out.println("bytes: " + last.length);
-                
-                if ("contents.xml".equals(name)) {
-                    contents = (ArchiveContents) ArchiveContents.xstream().fromXML(
-                        new ByteArrayInputStream(last));
-//                        new SubsetInputStream(zip, entry.getSize()));
-                    
-    //                System.out.println("entry.getSize(): " + entry.getSize());
-    //                for (TableOfContents.Component component : contents.components) {
-    //                    System.out.println("component: " + component.uri);
-    //                }
-                } else {
-//                    byte[] bytes = new byte[(int) entry.getSize()];
-                    
-//                    int pos = 0;
-//                    int read;
-//                    
-//                    do {
-//                        read = zip.read(bytes, pos, bytes.length - pos);
-//                        System.out.println("read: " + read);
-//                        
-//                        if (read < 0) break;
-//                        pos += read;
-//                    } while (pos < bytes.length);
-//                    
-//                    if (pos != bytes.length) {
-//                        throw new IllegalArgumentException("did not get full entry");
-//                    }
-                    
-                    entries.put(entry.getName(), last);
-                }
-            }
-            
+            zip.read(data);
+        }
+        
+        contents = (ArchiveContents) ArchiveContents.xstream().fromXML(
+            new ByteArrayInputStream(entries.get("contents.xml")));
+        
+        if (contents.components != null) {
             for (ArchiveContents.Component component : contents.components) {
                 WorkspaceComponent<?> wc = componentDeserializer.deserializeWorkspaceComponent(
                     component.className, new ByteArrayInputStream(
                     entries.get(component.uri)), "name", null);
-
-//                if (component.desktopComponent != null) {
-//                    workspace.toggleEvents(false);
-//                    
-//                    DesktopComponent dc = componentDeserializer.deserializeDesktopComponent(
-//                        component.desktopComponent.className, new ByteArrayInputStream(
-//                        entries.get(component.desktopComponent.uri)), "name");
-//                    
-//                    SimbrainDesktop.getDesktop(workspace).addComponent(wc, dc);
-//                    
-//                    workspace.toggleEvents(true);
-//                }
-                
+    
                 workspace.addWorkspaceComponent(wc);
             }
-            
+        }
+        
+        if (contents.couplings != null) {
             for (ArchiveContents.Coupling coupling : contents.couplings) {
                 workspace.addCoupling(new Coupling(
                     (ProducingAttribute<?>) componentDeserializer.getAttribute(coupling.source),
                     (ConsumingAttribute<?>) componentDeserializer.getAttribute(coupling.target)));
             }
-        } finally {
-            zip.close();
         }
     }
-    
-    private class SubsetInputStream extends InputStream {
-        int index = 0;
-        final InputStream stream;
-        final long length;
-        
-        SubsetInputStream(InputStream stream, long length) {
-            this.stream = stream;
-            this.length = length;
-        }
-
-        @Override
-        public int read() throws IOException {
-            if (index++ < length) return stream.read();
-            else return -1;
-        }
-    }
-    
-    class CachingInputStream extends InputStream {
-        private final InputStream wrapped;
-        private final ByteArrayOutputStream cache = new ByteArrayOutputStream();
-        
-        CachingInputStream(InputStream istream) {
-            this.wrapped = istream;
-        }
-        
-        public int available() throws IOException {
-            return wrapped.available();
-        }
-        
-        public void close() throws IOException {
-            wrapped.close();
-        }
-        
-        public int read() throws IOException {
-            int b = wrapped.read();
-            
-            cache.write(b);
-            
-            return wrapped.read();
-        }
-        
-        public int read(byte[] b, int off, int len) throws IOException {
-            byte[] bytes = new byte[len];
-            
-            int read = wrapped.read(bytes);
-            cache.write(bytes, 0, read);
-            
-            System.arraycopy(bytes, 0, b, off, len);
-            
-            return read;
-        }
-        
-        public long skip(long n) throws IOException {
-            int len = (int) n;
-            
-            byte[] bytes = new byte[len];
-            
-            int read = wrapped.read(bytes);
-            cache.write(bytes, 0, read);
-            
-            return read;
-        }
-        
-        byte[] getCached() {
-            byte[] bytes = cache.toByteArray();
-            
-            return bytes;
-        }
-    }
-    
-//    class Zip {
-//        final ZipInputStream stream;
-//        
-//        Zip(InputStream istream) {
-//            stream = new ZipInputStream(istream);
-//        }
-//    }
 }

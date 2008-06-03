a73f8fb754e89cb223a0f0b686aa38208d04246c
heuermh <heuermh@bf00e721-664e-0410-b0bc-3daef764c4be>
2008-06-03 20:00:11 +0000

    adding simple MIDI note consumer, using proMIDI library

---
+++ src/org/simbrain/world/midiworld/AbstractMidiConsumer.java
@@ -0,0 +1,103 @@
+package org.simbrain.world.midiworld;
+
+import java.lang.reflect.Type;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.simbrain.workspace.Consumer;
+import org.simbrain.workspace.ConsumingAttribute;
+
+import org.simbrain.workspace.WorkspaceComponent;
+
+/**
+ * Abstract MIDI consumer.
+ */
+abstract class AbstractMidiConsumer
+    implements Consumer {
+
+    /** Trigger attribute.  Any value above <code>0.0d</code> enables the trigger. */
+    private final ConsumingAttribute<Double> trigger;
+
+    /** MIDI world component. */
+    private final MidiWorldComponent component;
+
+
+    /**
+     * Create a new abstract MIDI consumer with the specified MIDI world component.
+     *
+     * @param component MIDI world component, must not be null
+     */
+    protected AbstractMidiConsumer(final MidiWorldComponent component) {
+        if (component == null) {
+            throw new IllegalArgumentException("component must not be null");
+        }
+        this.component = component;
+        trigger = new TriggerAttribute();
+    }
+
+
+    /**
+     * Notify subclasses the value for the trigger attribute has been set
+     * to a value above <code>0.0d</code>.
+     *
+     * @param value new trigger value
+     */
+    protected abstract void trigger(final double value);
+
+    /** {@inheritDoc} */
+    public final WorkspaceComponent<?> getParentComponent() {
+        return component;
+    }
+
+    /** {@inheritDoc} */
+    public final List<? extends ConsumingAttribute<?>> getConsumingAttributes() {
+        return Collections.singletonList(trigger);
+    }
+
+    /** {@inheritDoc} */
+    public final ConsumingAttribute<?> getDefaultConsumingAttribute() {
+        return trigger;
+    }
+
+    /** {@inheritDoc} */
+    public final void setDefaultConsumingAttribute(final ConsumingAttribute<?> consumingAttribute) {
+        throw new UnsupportedOperationException("default attribute is not modifiable");
+    }
+
+    /**
+     * Trigger attribute.
+     */
+    private final class TriggerAttribute
+        implements ConsumingAttribute<Double> {
+
+        /** Value. */
+        private Double value = 0.0d;
+
+        /** {@inheritDoc} */
+        public String getAttributeDescription() {
+            return "Trigger Attribute";
+        }
+
+        /** {@inheritDoc} */
+        public Type getType() {
+            return Double.class;
+        }
+
+        /** {@inheritDoc} */
+        public Consumer getParent() {
+            return AbstractMidiConsumer.this;
+        }
+
+        /** {@inheritDoc} */
+        public void setValue(final Double value) {
+            if (value == null) {
+                throw new IllegalArgumentException("value must not be null");
+            }
+            this.value = value;
+            if (this.value.doubleValue() > 0.0d) {
+                trigger(this.value.doubleValue());
+            }
+        }
+    }
+}
--- src/org/simbrain/world/midiworld/MidiWorldComponent.java
+++ src/org/simbrain/world/midiworld/MidiWorldComponent.java
@@ -2,14 +2,30 @@ package org.simbrain.world.midiworld;
 
 import java.io.OutputStream;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.simbrain.workspace.Consumer;
 import org.simbrain.workspace.WorkspaceComponent;
 import org.simbrain.workspace.WorkspaceComponentListener;
+
+import promidi.MidiIO;
+import promidi.MidiOut;
 
 /**
  * MIDI world component.
  */
 public final class MidiWorldComponent
     extends WorkspaceComponent<WorkspaceComponentListener> {
+
+    /** MIDI out. */
+    private final MidiOut midiOut;
+
+    /** List of MIDI note consumers. */
+    private final List<MidiNoteConsumer> consumers;
+
 
     /**
      * Create a new MIDI world component with the specified name.
@@ -18,6 +34,10 @@ public final class MidiWorldComponent
      */
     public MidiWorldComponent(final String name) {
         super(name);
+        midiOut = MidiIO.getInstance().getMidiOut(0, 0);
+        consumers = new ArrayList<MidiNoteConsumer>();
+        // TODO: just as an example for now...
+        consumers.add(new MidiNoteConsumer(new promidi.Note(40, 80, 600), this));
     }
 
 
@@ -34,5 +54,19 @@ public final class MidiWorldComponent
     /** {@inheritDoc} */
     public void update() {
         // empty
+    }
+
+    /** {@inheritDoc} */
+    public Collection<? extends Consumer> getConsumers() {
+        return Collections.unmodifiableList(consumers);
+    }
+
+    /**
+     * Return the MIDI out  for this MIDI world component.
+     *
+     * @return the MIDI out for this MIDI world component
+     */
+    MidiOut getMidiOut() {
+        return midiOut;
     }
 }

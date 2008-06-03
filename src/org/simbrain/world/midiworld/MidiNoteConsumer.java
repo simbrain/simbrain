869ba4f82c7c0f9b19524b8debaa699a8f6235d5
heuermh <heuermh@bf00e721-664e-0410-b0bc-3daef764c4be>
2008-06-03 20:01:20 +0000

    adding simple MIDI note consumer, using proMIDI library

---
+++ src/org/simbrain/world/midiworld/MidiNoteConsumer.java
@@ -0,0 +1,39 @@
+package org.simbrain.world.midiworld;
+
+import promidi.Note;
+
+/**
+ * MIDI note consumer.
+ */
+final class MidiNoteConsumer
+    extends AbstractMidiConsumer {
+
+    /** MIDI note. */
+    private final Note note;
+
+
+    /**
+     * Create a new MIDI note consumer with the specified note and MIDI world workspace component.
+     *
+     * @param note MIDI note, must not be null
+     * @param component MIDI world component, must not be null
+     */
+    MidiNoteConsumer(final Note note, final MidiWorldComponent component) {
+        super(component);
+        if (note == null) {
+            throw new IllegalArgumentException("note must not be null");
+        }
+        this.note = note;
+    }
+
+
+    /** {@inheritDoc} */
+    protected void trigger(final double value) {
+        ((MidiWorldComponent) getParentComponent()).getMidiOut().sendNote(note);
+    }
+
+    /** {@inheritDoc} */
+    public String getDescription() {
+        return "MIDI Note Consumer";
+    }
+}

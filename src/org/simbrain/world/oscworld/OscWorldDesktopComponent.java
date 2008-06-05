3b40af232488bc2daa00466072744563b84bb89b
heuermh <heuermh@bf00e721-664e-0410-b0bc-3daef764c4be>
2008-06-05 17:57:43 +0000

    adding create OSC message action and menu/tool bar

--- src/org/simbrain/world/oscworld/OscWorldDesktopComponent.java
+++ src/org/simbrain/world/oscworld/OscWorldDesktopComponent.java
@@ -1,5 +1,18 @@
 package org.simbrain.world.oscworld;
 
+import java.awt.BorderLayout;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+
 import org.simbrain.workspace.gui.DesktopComponent;
 
 /**
@@ -8,6 +21,10 @@ import org.simbrain.workspace.gui.DesktopComponent;
 public final class OscWorldDesktopComponent
     extends DesktopComponent<OscWorldComponent> {
 
+    /** Create OSC message action. */
+    private final Action createOscMessageAction;
+
+
     /**
      * Create a new OSC world desktop component with the specified OSC world component.
      *
@@ -15,12 +32,55 @@ public final class OscWorldDesktopComponent
      */
     public OscWorldDesktopComponent(final OscWorldComponent oscWorldComponent) {
         super(oscWorldComponent);
-        add("Center", new OscWorld());
+
+        createOscMessageAction = new CreateOscMessageAction();
+
+        JMenuBar menuBar = new JMenuBar();
+        JToolBar toolBar = new JToolBar();
+
+        JMenu file = new JMenu("File");
+        file.add(createOscMessageAction);
+        toolBar.add(createOscMessageAction);
+
+        menuBar.add(file);
+        setJMenuBar(menuBar);
+
+        JPanel contentPane = (JPanel) getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add("North", toolBar);
+        contentPane.add("Center", new OscWorld());        
     }
 
 
     /** {@inheritDoc} */
     public void close() {
         // empty
+    }
+
+    /**
+     * Create OSC message action.
+     */
+    private final class CreateOscMessageAction
+        extends AbstractAction {
+
+        /**
+         * Create a new create OSC message action.
+         */
+        CreateOscMessageAction() {
+            super("Create OSC message");
+            putValue(Action.LONG_DESCRIPTION, "Create a new OSC message");
+        }
+
+
+        /** {@inheritDoc} */
+        public void actionPerformed(final ActionEvent event) {
+            SwingUtilities.invokeLater(new Runnable() {
+                    /** {@inheritDoc} */
+                    public void run() {
+                        String address = JOptionPane.showInputDialog(null, "Create a new OSC message with the specified address.\nThe address must begin with a '/' character.\n\n\nOSC message address:", "Create a new OSC message", JOptionPane.QUESTION_MESSAGE);
+                        getWorkspaceComponent().addMessage(address);
+                    }
+                });
+        }
     }
 }

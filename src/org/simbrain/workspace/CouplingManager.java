6eb21d80feff13d14ca5574bc864043177b9e968
klbaron <klbaron@bf00e721-664e-0410-b0bc-3daef764c4be>
2008-12-11 21:12:51 +0000

    Minor refactor of CouplingComponentListener.

--- src/org/simbrain/workspace/CouplingManager.java
+++ src/org/simbrain/workspace/CouplingManager.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.simbrain.workspace.gui.CouplingComponentListener;
 
 /**
  * Manages all the couplings for a Workspace instance.

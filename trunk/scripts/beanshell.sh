35c56b4cb701899079b3dc54d52cdc63f5f574f3
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2009-03-11 19:42:58 +0000

    Removing script directory (will re-add and restructure it next).

--- trunk/scripts/beanshell.sh
+++
@@ -1,2 +0,0 @@
-cd ..
-java -cp ./bin:./lib/bsh-2.0b4.jar:./lib/log4j-1.2.14.jar org.simbrain.console.SimbrainInterpreter
--- trunk/scripts/test3d.sh
+++
@@ -1,2 +0,0 @@
-cd ..
-java -cp bin:lib/jme.jar:lib/jme-awt.jar:lib/jme-terrain.jar:lib/lwjgl.jar:lib/log4j-1.2.14.jar -Djava.library.path=lib/native/macosx org.simbrain.world.threedee.Test

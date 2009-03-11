3c6e0f53d10a83a74283a05f99cca6539a532e2c
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2009-03-11 19:42:58 +0000

    Removing script directory (will re-add and restructure it next).

--- scripts/archive.sh
+++
@@ -1,4 +0,0 @@
-export ARCHIVE_DIR="Documents/SimbrainARKIVES/zipped"
-gnutar --exclude=*.gif --exclude=.svn -czvf temp.tar.gz ./src
-mv temp.tar.gz src`date +%m%d%y`.tar.gz
-mv *.gz $HOME/$ARCHIVE_DIR

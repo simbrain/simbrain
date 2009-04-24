cddc4fe4828859ba4872e986eaa216a9422200f4
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2009-04-24 19:48:12 +0000

    Improvements to the connection package, including an initial attempt to allow connections to use specifiable synapse types, and fixes to the 1-1 connection type.

--- trunk/src/org/simbrain/network/connections/OneToOne.java
+++ trunk/src/org/simbrain/network/connections/OneToOne.java
@@ -1,31 +1,56 @@
 package org.simbrain.network.connections;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
 import org.simbrain.network.interfaces.Network;
 import org.simbrain.network.interfaces.Neuron;
+import org.simbrain.network.interfaces.Synapse;
 import org.simbrain.network.synapses.ClampedSynapse;
+import org.simbrain.network.synapses.RandomSynapse;
 
 /**
  * Connect each source neuron to a single target.
- *
+ * 
  * @author jyoshimi
- *
  */
 public class OneToOne extends ConnectNeurons {
 
     /**
-     * See super class description.
-     *
-     * @param network network with neurons to be connected.
-     * @param neurons source neurons.
-     * @param neurons2 target neurons.
+     * The synapse to be used as a basis for the connection. Default to a
+     * clamped synapse.
      */
-    public OneToOne(final Network network, final ArrayList neurons, final ArrayList neurons2) {
+    private Synapse baseSynapse = new ClampedSynapse(null, null);
+    
+    /**
+     * Comparator which orders by X coordinate.
+     */
+    static final Comparator<Neuron> X_ORDER = new Comparator<Neuron>() {
+        public int compare(final Neuron neuron1, final Neuron neuron2) {
+            return Double.compare(neuron1.getX(), neuron2.getX());
+        }
+    };
+
+    /**
+     * Comparator which orders by X coordinate.
+     */
+    static final Comparator<Neuron> Y_ORDER = new Comparator<Neuron>() {
+        public int compare(final Neuron neuron1, final Neuron neuron2) {
+            return Double.compare(neuron1.getY(), neuron2.getY());
+        }
+    };
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public OneToOne(final Network network, final ArrayList<Neuron> neurons,
+            final ArrayList<Neuron> neurons2) {
         super(network, neurons, neurons2);
     }
-    
+
     /** {@inheritDoc} */
     public OneToOne() {
     }
@@ -35,15 +60,55 @@ public class OneToOne extends ConnectNeurons {
         return "One to one";
     }
 
+    /**
+     * Returns a sorted list of neurons, given a comparator.
+     *
+     * @param neuronList the base list of neurons.
+     * @param comparator the comparator.
+     * @return the sorted list.
+     */
+    private ArrayList<Neuron> getSortedNeuronList(final ArrayList<Neuron> neuronList,
+            final Comparator<Neuron> comparator) {
+        ArrayList<Neuron> list = new ArrayList<Neuron>();
+        list.addAll(neuronList);
+        Collections.sort(list, comparator);
+        return list;
+    }
+
     /** @inheritDoc */
     public void connectNeurons() {
-        Iterator targets = targetNeurons.iterator();
-        for (Iterator sources = sourceNeurons.iterator(); sources.hasNext(); ) {
+        
+        //TODO: Flags for which comparator to use, including no comparator
+        //          (Some users might want random but 1-1 couplings)
+        Iterator<Neuron> targets = getSortedNeuronList(targetNeurons, X_ORDER)
+                .iterator();
+
+        for (Iterator<Neuron> sources = getSortedNeuronList(sourceNeurons,
+                X_ORDER).iterator(); sources.hasNext();) {
             Neuron source = (Neuron) sources.next();
             if (targets.hasNext()) {
                 Neuron target = (Neuron) targets.next();
-                network.addSynapse(new ClampedSynapse(source, target));
+                Synapse synapse = baseSynapse.duplicate();
+                synapse.setSource(source);
+                synapse.setTarget(target);
+                network.addSynapse(synapse);
+            } else {
+                return;
             }
         }
+    }
+
+    /**
+     * @return the baseSynapse
+     */
+    public Synapse getBaseSynapse() {
+        return baseSynapse;
+    }
+
+    /**
+     * @param baseSynapse the baseSynapse to set
+     */
+    public void setBaseSynapse(final Synapse baseSynapse) {
+        this.baseSynapse = baseSynapse;
     }
 }

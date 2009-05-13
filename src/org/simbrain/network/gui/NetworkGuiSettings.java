df0b17bbb32bea8f643cf13f9147d6a66da585ec
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2009-05-13 20:53:02 +0000

    Added comments to clarify the way settings work in the neural network component.

--- src/org/simbrain/network/gui/NetworkGuiSettings.java
+++ src/org/simbrain/network/gui/NetworkGuiSettings.java
@@ -1,7 +1,39 @@
+/*
+* Part of Simbrain--a java-based neural network kit
+* Copyright (C) 2005,2007 The Authors.  See http://www.simbrain.net/credits
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 package org.simbrain.network.gui;
 
 import java.awt.Color;
 
+/**
+ * This class stores the settings for the network GUI. They are set to default
+ * values which can be modified by the user. This class is used in two ways:
+ * 
+ * 1) By itself: When the network is run as an applet, or separately from the
+ * desktop environment, this class is used by itself. Settings last as long as
+ * the application is open.
+ * 
+ * 2) In conjunction with user preferences. When the network is run in the
+ * simbrain desktop, these settings are overwritten by
+ * org.simbrain.network.desktop.NetworkGuiPreferences, which uses the java user
+ * preferences API. This means that settings last between sessions, and are
+ * stored on a users local machine.
+ */
 public class NetworkGuiSettings {
 
     /** Color of all lines in rootNetwork panel. */

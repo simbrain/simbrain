c20e40d8d265f1a3d6022550c4a0d43668efc4f5
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2009-06-22 23:19:12 +0000

    Improved String representation of SingleAttribute Consumers and Producers, and some related documentation updates.

--- src/org/simbrain/workspace/Attribute.java
+++ src/org/simbrain/workspace/Attribute.java
@@ -17,16 +17,19 @@ public interface Attribute {
     String getAttributeDescription();
 
     /**
-     * Returns the key for this attribute.
-     * This is used for serialization and in interface
-     * elements which display attributes.
-     *
-     * NOTE 1: This description must be unique relative to
-     * other Attributes in an AttributeHolder.
+     * Returns the key for this attribute. This key serves two purposes.
      * 
-     * NOTE 2: The key of the attribute must be exactly
-     * the same after deserialization as it was before serialization,
-     * even if the description of the attribute was not serialized.
+     * First, it is used for serialization, as a String id for a given Attribute.
+     * Because of this, NOTE, the key must be unique relative to other
+     * Attributes in an AttributeHolder.
+     * 
+     * Second, this is used as a String description of the attribute, which is
+     * used at various places in the GUI. So, the key value should be
+     * human-readable.
+     * 
+     * Also note that the key of an attribute must be exactly the same after
+     * deserialization as it was before serialization, even if the description
+     * of the attribute was not serialized.
      * 
      * @return the key for this attribute.
      */

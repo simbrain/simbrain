3f7e0ed0271ffc43e2aa2f96d9f613b49974c11b
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2011-06-06 21:36:28 +0000

    Fixing default value representation for spike timing dependent plasticitiy synapses.

--- src/org/simbrain/network/gui/dialogs/synapse/STDPSynapsePanel.java
+++ src/org/simbrain/network/gui/dialogs/synapse/STDPSynapsePanel.java
@@ -25,7 +25,7 @@ import org.simbrain.network.synapses.STDPSynapse;
 
 
 /**
- * <b>ShortTermPlasticitySynapsePanel</b>.
+ * <b>ShortTermPlasticitySynapsePanel</b> allows users to edit STDP synapses.
  */
 public class STDPSynapsePanel extends AbstractSynapsePanel {
 
@@ -33,7 +33,7 @@ public class STDPSynapsePanel extends AbstractSynapsePanel {
     private JTextField tfTauMinus = new JTextField();
 
     /** Tau plus. */
-    private JTextField tfTauPLus= new JTextField();
+    private JTextField tfTauPlus= new JTextField();
 
     /** W minus. */
     private JTextField tfWMinus = new JTextField();
@@ -52,7 +52,7 @@ public class STDPSynapsePanel extends AbstractSynapsePanel {
      */
     public STDPSynapsePanel() {
         this.addItem("Tau minus", tfTauMinus);
-        this.addItem("Tau plus", tfTauPLus);
+        this.addItem("Tau plus", tfTauPlus);
         this.addItem("W+", tfWPlus);
         this.addItem("W-", tfWMinus);
         this.addItem("Learning rate", tfLearningRate);
@@ -65,7 +65,7 @@ public class STDPSynapsePanel extends AbstractSynapsePanel {
         synapseRef = (STDPSynapse) ruleList.get(0);
 
         tfTauMinus.setText(Double.toString(synapseRef.getTau_minus()));
-        tfTauPLus.setText(Double.toString(synapseRef.getTau_plus()));
+        tfTauPlus.setText(Double.toString(synapseRef.getTau_plus()));
         tfWMinus.setText(Double.toString(synapseRef.getW_minus()));
         tfWPlus.setText(Double.toString(synapseRef.getW_plus()));
         tfLearningRate.setText(Double.toString(synapseRef.getLearningRate()));
@@ -75,7 +75,7 @@ public class STDPSynapsePanel extends AbstractSynapsePanel {
             tfTauMinus.setText(NULL_STRING);
         }
         if (!NetworkUtils.isConsistent(ruleList, STDPSynapse.class, "getTau_plus")) {
-            tfTauPLus.setText(NULL_STRING);
+            tfTauPlus.setText(NULL_STRING);
         }
         if (!NetworkUtils.isConsistent(ruleList, STDPSynapse.class, "getW_minus")) {
             tfWMinus.setText(NULL_STRING);
@@ -92,13 +92,12 @@ public class STDPSynapsePanel extends AbstractSynapsePanel {
      * Fill field values to default values for this synapse type.
      */
     public void fillDefaultValues() {
-        // TODO
-////        ShortTermPlasticitySynapse synapseRef = new ShortTermPlasticitySynapse();
-//        cbPlasticityType.setSelectedIndex(ShortTermPlasticitySynapse.DEFAULT_PLASTICITY_TYPE);
-//        tfBaseLineStrength.setText(Double.toString(ShortTermPlasticitySynapse.DEFAULT_BASE_LINE_STRENGTH));
-//        tfFiringThreshold.setText(Double.toString(ShortTermPlasticitySynapse.DEFAULT_FIRING_THRESHOLD));
-//        tfBumpRate.setText(Double.toString(ShortTermPlasticitySynapse.DEFAULT_BUMP_RATE));
-//        tfDecayRate.setText(Double.toString(ShortTermPlasticitySynapse.DEFAULT_DECAY_RATE));
+        STDPSynapse template = new STDPSynapse();
+        tfTauPlus.setText("" +  template.getTau_plus());
+        tfTauMinus.setText("" +  template.getTau_minus());
+        tfWPlus.setText("" +  template.getW_plus());
+        tfWMinus.setText("" +  template.getW_minus());
+        tfLearningRate.setText("" +  template.getLearningRate());
     }
 
     /**
@@ -112,8 +111,8 @@ public class STDPSynapsePanel extends AbstractSynapsePanel {
             if (!tfTauMinus.getText().equals(NULL_STRING)) {
                 synapseRef.setTau_minus(Double.parseDouble(tfTauMinus.getText()));
             }
-            if (!tfTauPLus.getText().equals(NULL_STRING)) {
-                synapseRef.setTau_plus(Double.parseDouble(tfTauPLus.getText()));
+            if (!tfTauPlus.getText().equals(NULL_STRING)) {
+                synapseRef.setTau_plus(Double.parseDouble(tfTauPlus.getText()));
             }
             if (!tfWMinus.getText().equals(NULL_STRING)) {
                 synapseRef.setW_minus(Double.parseDouble(tfWMinus.getText()));

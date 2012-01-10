cdfc884605d4780d06d43ae742a591b0cb36c44e
ZachTosi@gmail.com <ZachTosi@gmail.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2012-01-10 23:26:11 +0000

    Moved excitatory/inhibitory part of the panel from abstractconnectionpanel to a new class (ExcitatoryInhibitoryPropertiesPanel)

--- trunk/src/org/simbrain/network/gui/dialogs/connect/AllToAllPanel.java
+++ trunk/src/org/simbrain/network/gui/dialogs/connect/AllToAllPanel.java
@@ -42,6 +42,8 @@ import org.simbrain.network.interfaces.Synapse;
  */
 public class AllToAllPanel extends AbstractConnectionPanel {
 
+	private ExcitatoryInhibitoryPropertiesPanel eipPanel; 
+	
     /** Allow self connection check box. */
     private JCheckBox allowSelfConnect = new JCheckBox();
 
@@ -51,115 +53,40 @@ public class AllToAllPanel extends AbstractConnectionPanel {
      */
     public AllToAllPanel(final AllToAll connection) {
         super(connection);
-        fillFieldValues();
-        initializeLayout();
-    }
-    
-    /**
-     * Initializes the custom layout for the all to all panel
-     */
-    public void initializeLayout(){ 	
-    	
-    	this.setLayout(new GridBagLayout());
+        eipPanel = new ExcitatoryInhibitoryPropertiesPanel(connection);
+        this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 9;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        this.add(eipPanel, gbc);
+        
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 10, 0, 10);
-        
-        this.add(new JLabel("Excitatory/Inhibitory:"), gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 3;
-        this.add(ratioSlider, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 1;
-        this.add(new JLabel("% Excitatory"), gbc);
-        
-        gbc.gridx = 1;
-        Dimension tRatioSize = tRatio.getPreferredSize();
-        tRatioSize.width = 30;
-        tRatio.setPreferredSize(tRatioSize);
-        
-        //the ratio text field gets its own panel to prevent distortion
-        JPanel tRatioPanel = new JPanel();
-        tRatioPanel.setLayout(new BorderLayout());
-        tRatioPanel.add(tRatio, BorderLayout.WEST);
-        this.add(tRatioPanel, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        this.add(new JLabel("Allow Self-Connection: "), gbc);
-        
-        gbc.gridx = 1;
-        this.add(allowSelfConnect, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.gridwidth = 3;
-        this.add(new JSeparator(JSeparator.HORIZONTAL), gbc);
-        
-        gbc.gridwidth = 1;
-        gbc.gridy = 5;
-        this.add(new JLabel("Excitatory Synapse Type: "), gbc);
-        
-        gbc.gridx = 1;
-        this.add(excitatorySynType, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 6;
-        this.add(new JLabel("Inhibitory Synapse Type: "), gbc);
-        
-        gbc.gridx = 1;
-        this.add(inhibitorySynType, gbc);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 7;
-        gbc.gridwidth = 3;
-        this.add(new JSeparator(JSeparator.HORIZONTAL), gbc);
-        
-        gbc.gridy = 8;
-        gbc.gridwidth = 1;
-        this.add(new JLabel("Randomize Excitatory Weights: "), gbc);
-        
-        gbc.gridx = 1;
-        this.add(randExcite, gbc);
-        
-        gbc.gridx = 2;
-        this.add(randExButton, gbc);
-        
-        gbc.gridx = 0;
         gbc.gridy = 9;
-        this.add(new JLabel("Randomize Inhibitory Weights: "), gbc);
+        gbc.gridheight = 1;
+        this.add(new JSeparator(), gbc);
         
-        gbc.gridx = 1;
-        this.add(randInhib, gbc);
+        gbc.gridy = 10;
+        gbc.gridwidth = 1;
+        this.add(new JLabel("Allow Self-Connection"), gbc);
         
         gbc.gridx = 2;
-        this.add(randInButton, gbc);
-        
+        this.add(allowSelfConnect, gbc); 
     }
-
+    
     /**
      * {@inheritDoc}
      */
     public void commitChanges() {
-        connection.setPercentExcitatory(((Number)tRatio.getValue()).doubleValue() / 100);
-        connection.setEnableInRand(randInhib.isSelected());
-        connection.setEnableExRand(randExcite.isSelected());
-    	if(randInhib.isSelected()) {
-    		connection.setInhibitoryRand(inhibRS);
-    	}
-    	if(randExcite.isSelected()) {
-    		connection.setExcitatoryRand(exciteRS);
-    	}
     	((AllToAll) connection).setAllowSelfConnection(allowSelfConnect
                 .isSelected());
+    	eipPanel.commitChanges();
     }
     
 
@@ -167,10 +94,7 @@ public class AllToAllPanel extends AbstractConnectionPanel {
      * {@inheritDoc}
      */
     public void fillFieldValues() {
-       	Synapse e = Synapse.getTemplateSynapse(excitatorySynType.getText());
-       	connection.setBaseExcitatorySynapse(e);
-   		Synapse i = Synapse.getTemplateSynapse(inhibitorySynType.getText());
-   		connection.setBaseInhibitorySynapse(i);
+       
     }
 
 }

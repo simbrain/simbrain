2c66c126f1050777d8f07ba024a0185ad91a0446
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2012-06-22 22:55:46 +0000

    Cleanup of etc. directory, minor changes to build and build.xml, and improvements to document / quickstart page.

--- etc/run.sh
+++
@@ -1,9 +0,0 @@
-OS=`uname -s`
-if [ $OS -eq 'Windows_NT' ] ; then
-	SEP=";"
-else 
-	SEP=":"
-fi
-cd ..
-java -classpath .$SEP./bin$SEP./lib/utils.jar$SEP./lib/Jama-1.0.1.jar$SEP./lib/simnet.jar$SEP./lib/piccolo.jar$SEP./lib/piccolox.jar$SEP./lib.$SEP./lib/castor.jar$SEP./lib/snarli.jar$SEP./lib/jlinalg.jar$SEP./lib/xerxes.jar org.simbrain.workspace.Splasher
-cd etc

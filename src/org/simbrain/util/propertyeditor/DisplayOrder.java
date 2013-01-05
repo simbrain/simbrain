4bb3a9bbf98a624cec189621b158acb27138f403
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2013-01-05 01:39:21 +0000

    Changes to reflective property editor to allow for ordering the display of fields using a custom annotation.

---
+++ src/org/simbrain/util/propertyeditor/DisplayOrder.java
@@ -0,0 +1,42 @@
+/*
+ * Part of Simbrain--a java-based neural network kit
+ * Copyright (C) 2005,2007 The Authors.  See http://www.simbrain.net/credits
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.simbrain.util.propertyeditor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * An annotation that can be used to control the order in which fields are
+ * displayed in a reflective property editor.
+ *
+ *
+ * @author jeffyoshimi
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DisplayOrder {
+
+    /**
+     * An integer that determines the order in which a field is shown in the
+     * display. Lower numbers first.
+     */
+    int val() default 1;
+}

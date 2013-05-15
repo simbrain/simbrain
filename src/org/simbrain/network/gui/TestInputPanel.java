3fa6eb667d5c6fc9d38be79d37bbdd4036dda5f7
jeffyoshimi@yahoo.com <jeffyoshimi@yahoo.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2013-05-15 00:03:55 +0000

    Generalizing "test input panel" so that it can be used for other subnetworks and groups of neurons.

--- src/org/simbrain/network/gui/TestInputPanel.java
+++ src/org/simbrain/network/gui/TestInputPanel.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package org.simbrain.network.gui.trainer;
+package org.simbrain.network.gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -30,20 +30,22 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
-import org.simbrain.network.subnetworks.BackpropNetwork;
+import org.simbrain.network.core.Neuron;
+import org.simbrain.network.groups.Subnetwork;
 import org.simbrain.resource.ResourceManager;
 import org.simbrain.util.table.NumericTable;
 import org.simbrain.util.table.SimbrainJTable;
 import org.simbrain.util.table.SimbrainJTableScrollPanel;
 
 /**
- * Panel to test a Backprop network by allowing user to cycle
- * through a set of inputs.
+ * Component to send inputs from a table to a network. The user of this class
+ * provides a set of input neurons, and a subnetwork to be updated. If the
+ * subnetwork is null, the update the whole network.
  *
- * @author jyoshimi
+ * @author Jeff Yoshimi
  * @author Lam Nguyen
  */
-public class TestNetworkPanel extends JPanel {
+public class TestInputPanel extends JPanel {
 
     /** JTable contained in scroller. */
     private SimbrainJTable table;
@@ -51,24 +53,30 @@ public class TestNetworkPanel extends JPanel {
     /** Scroll panel for table. */
     private SimbrainJTableScrollPanel scroller;
 
-    /** The Backprop network to test. */
-    private BackpropNetwork network;
+    /**
+     * This is the network that should be updated whenever the input neurons are
+     * updated. If null, update the whole network
+     */
+    private Subnetwork subNetwork;
+
+    /** The nodes to test. */
+    private List<Neuron> inputNeurons;
 
     /**
      * Construct a test network panel using a specified Backprop network.
      *
      * @param network the Backprop network to test
      */
-    public TestNetworkPanel(final BackpropNetwork network) {
-        this.network = network;
-        table = new SimbrainJTable(new NumericTable(5, network
-                .getInputNeurons().size()));
+    public TestInputPanel(final Subnetwork network, final List<Neuron> inputNeurons) {
+        this.subNetwork = network;
+        this.inputNeurons = inputNeurons;
+        table = new SimbrainJTable(new NumericTable(5, inputNeurons.size()));
         ((NumericTable) table.getData()).setIterationMode(true);
         // Set up column headings
         List<String> colHeaders = new ArrayList<String>();
-        for (int i = 0; i < network.getInputNeurons().size(); i++) {
+        for (int i = 0; i < inputNeurons.size(); i++) {
             colHeaders.add(new String("" + (i + 1) + " ("
-                    + network.getInputNeurons().get(i).getId())
+                    + inputNeurons.get(i).getId())
                     + ")");
         }
         table.setColumnHeadings(colHeaders);
@@ -99,6 +107,7 @@ public class TestNetworkPanel extends JPanel {
             putValue(SMALL_ICON, ResourceManager.getImageIcon("plus.png"));
             putValue(SHORT_DESCRIPTION, "Advance row");
         }
+
         /**
          * {@ineritDoc}
          */
@@ -115,6 +124,7 @@ public class TestNetworkPanel extends JPanel {
             putValue(SMALL_ICON, ResourceManager.getImageIcon("Step.png"));
             putValue(SHORT_DESCRIPTION, "Test network");
         }
+
         /**
          * {@ineritDoc}
          */
@@ -136,17 +146,21 @@ public class TestNetworkPanel extends JPanel {
      */
     private void testRow() {
         int testRow = ((NumericTable) table.getData()).getCurrentRow();
-        if (testRow >=  ((NumericTable) table.getData()).getRowCount()) {
+        if (testRow >= ((NumericTable) table.getData()).getRowCount()) {
             testRow = 0;
         }
         table.updateRowSelection();
-        for (int j = 0; j < network.getInputNeurons().size(); j++) {
-            network.getInputNeurons().get(j)
-            .setInputValue(((NumericTable) table.getData()).
-                    getValue(testRow, j));
+        for (int j = 0; j < inputNeurons.size(); j++) {
+            inputNeurons.get(j).setInputValue(
+                    ((NumericTable) table.getData()).getValue(testRow, j));
         }
-        network.update();
-        network.getParentNetwork().fireNetworkChanged();
+        if (subNetwork != null) {
+            subNetwork.update();
+            subNetwork.getParentNetwork().fireNetworkChanged();
+        } else {
+            inputNeurons.get(0).getParentNetwork().update();
+            inputNeurons.get(0).getParentNetwork().fireNetworkChanged();
+        }
     }
 
     /**

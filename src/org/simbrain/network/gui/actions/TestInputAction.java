93b7b2bc984b1d3b1b90f2ab0d3bce2431f9b4f4
linkedlam@gmail.com <linkedlam@gmail.com@bf00e721-664e-0410-b0bc-3daef764c4be>
2013-05-28 01:39:53 +0000

    Changed TestInputPanel to a general network level TestInputAction. Added this action to the network action manager, edit menu and popup menu.

--- src/org/simbrain/network/gui/actions/TestInputAction.java
+++ src/org/simbrain/network/gui/actions/TestInputAction.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package org.simbrain.network.gui;
+package org.simbrain.network.gui.actions;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -30,22 +30,38 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
+import org.simbrain.network.core.Network;
 import org.simbrain.network.core.Neuron;
-import org.simbrain.network.groups.Subnetwork;
+import org.simbrain.network.gui.NetworkPanel;
+import org.simbrain.network.gui.NetworkSelectionEvent;
+import org.simbrain.network.gui.NetworkSelectionListener;
 import org.simbrain.resource.ResourceManager;
 import org.simbrain.util.table.NumericTable;
 import org.simbrain.util.table.SimbrainJTable;
 import org.simbrain.util.table.SimbrainJTableScrollPanel;
 
 /**
- * Component to send inputs from a table to a network. The user of this class
- * provides a set of input neurons, and a subnetwork to be updated. If the
- * subnetwork is null, the update the whole network.
+ * Action to send inputs from a table to a network. The user of this class
+ * provides the input neurons and network panel from which the action gets
+ * the network to be updated. If input neurons are not provided, selected
+ * neurons are used as input neurons.
  *
  * @author Jeff Yoshimi
  * @author Lam Nguyen
  */
-public class TestInputPanel extends JPanel {
+public class TestInputAction extends AbstractAction {
+
+    {
+        putValue(NAME, "Test network...");
+        putValue(SHORT_DESCRIPTION, "Test network...");
+        putValue(SMALL_ICON, ResourceManager.getImageIcon("Trainer.png"));
+    }
+
+    /** Network panel. */
+    private NetworkPanel networkPanel;
+
+    /** The panel used to test inputs to a network. */
+    private JPanel testInputPanel;
 
     /** JTable contained in scroller. */
     private SimbrainJTable table;
@@ -57,33 +73,96 @@ public class TestInputPanel extends JPanel {
      * This is the network that should be updated whenever the input neurons are
      * updated. If null, update the whole network
      */
-    private Subnetwork subNetwork;
+    private Network network;
 
     /** The nodes to test. */
     private List<Neuron> inputNeurons;
 
     /**
-     * Construct a test network panel using a specified Backprop network.
+     * Construct action.
      *
-     * @param network the Backprop network to test
+     * @param networkPanel networkPanel, must not be null
      */
-    public TestInputPanel(final Subnetwork network, final List<Neuron> inputNeurons) {
-        this.subNetwork = network;
+    public TestInputAction(NetworkPanel networkPanel) {
+
+        super("Test inputs...");
+
+        if (networkPanel == null) {
+            throw new IllegalArgumentException("networkPanel must not be null");
+        }
+
+        this.networkPanel = networkPanel;
+
+        //        updateAction();
+
+        // add a selection listener to update state based on selection
+        networkPanel.addSelectionListener(new NetworkSelectionListener() {
+            /** @see NetworkSelectionListener */
+            public void selectionChanged(NetworkSelectionEvent event) {
+                updateAction();
+            }
+        });
+    }
+
+    /**
+     * Construct action.
+     *
+     * @param networkPanel networkPanel, must not be null
+     * @param inputNeurons input neurons of the network to be tested
+     */
+    public TestInputAction(NetworkPanel networkPanel, List<Neuron> inputNeurons) {
+
+        super("Test inputs...");
+
+        if (networkPanel == null) {
+            throw new IllegalArgumentException("networkPanel must not be null");
+        }
+
+        this.networkPanel = networkPanel;
         this.inputNeurons = inputNeurons;
+    }
+
+    /**
+     * Set test input panel based on number of selected neurons.
+     */
+    private void updateAction() {
+        int numNeurons = networkPanel.getSelectedNeurons().size();
+
+        if (numNeurons > 0) {
+            inputNeurons = networkPanel.getSelectedModelNeurons();
+            setEnabled(true);
+        } else {
+            setEnabled(false);
+        }
+    }
+
+    /**
+     * Initialize and display the test input panel.
+     */
+    public void actionPerformed(ActionEvent event) {
+        initTestInputPanel();
+        networkPanel.displayPanel(testInputPanel, "Test inputs");
+    }
+
+    /**
+     * Construct a test network panel using the network panel.
+     */
+    private void initTestInputPanel() {
+        testInputPanel = new JPanel();
+        network = networkPanel.getNetwork();
         table = new SimbrainJTable(new NumericTable(5, inputNeurons.size()));
         ((NumericTable) table.getData()).setIterationMode(true);
         // Set up column headings
         List<String> colHeaders = new ArrayList<String>();
         for (int i = 0; i < inputNeurons.size(); i++) {
             colHeaders.add(new String("" + (i + 1) + " ("
-                    + inputNeurons.get(i).getId())
-                    + ")");
+                    + inputNeurons.get(i).getId()) + ")");
         }
         table.setColumnHeadings(colHeaders);
         table.getData().fireTableStructureChanged();
         scroller = new SimbrainJTableScrollPanel(table);
-        setLayout(new BorderLayout());
-        add("Center", scroller);
+        testInputPanel.setLayout(new BorderLayout());
+        testInputPanel.add("Center", scroller);
         JPanel toolbar = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JToolBar editRowToolBar = new JToolBar();
         toolbar.add(table.getToolbarCSV(true, false));
@@ -96,7 +175,7 @@ public class TestInputPanel extends JPanel {
         testToolBar.add(test);
         testToolBar.add(advance);
         toolbar.add(testToolBar);
-        add("North", toolbar);
+        testInputPanel.add("North", toolbar);
     }
 
     /**
@@ -154,9 +233,9 @@ public class TestInputPanel extends JPanel {
             inputNeurons.get(j).setInputValue(
                     ((NumericTable) table.getData()).getValue(testRow, j));
         }
-        if (subNetwork != null) {
-            subNetwork.update();
-            subNetwork.getParentNetwork().fireNetworkChanged();
+        if (network != null) {
+            network.update();
+            network.fireNetworkChanged();
         } else {
             inputNeurons.get(0).getParentNetwork().update();
             inputNeurons.get(0).getParentNetwork().fireNetworkChanged();
@@ -169,5 +248,4 @@ public class TestInputPanel extends JPanel {
     public SimbrainJTable getTable() {
         return table;
     }
-
 }

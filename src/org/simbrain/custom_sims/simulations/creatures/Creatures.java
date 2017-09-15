19f0902749eb094eb0cd656d54bb7c731cabc296
Insectoid <arthropodlover@gmail.com>
2017-09-14 21:25:18 -0700

    Setting up default lobes and lobe helper functions for creatures

--- src/org/simbrain/custom_sims/simulations/creatures/Creatures.java
+++ src/org/simbrain/custom_sims/simulations/creatures/Creatures.java
@@ -13,31 +13,24 @@ import org.simbrain.workspace.gui.SimbrainDesktop;
 import org.simbrain.workspace.updater.UpdateActionAdapter;
 import org.simbrain.world.odorworld.OdorWorld;
 import org.simbrain.world.odorworld.OdorWorldComponent;
+import org.simbrain.custom_sims.helper_classes.NetBuilder;
+import org.simbrain.network.groups.NeuronGroup;
 
 /**
  * A simulation of an A-Life agent based off of the Creatures entertainment
  * software by Cyberlife Technology & Steve Grand, as seen in "Creatures:
  * Entertainment software agents with artificial life" (D. Cliff & S. Grand,
  * 1998)
- * 
+ *
  * @author Sharai
  *
  */
 public class Creatures extends RegisteredSimulation {
 
 	/**
-     * Stolen from the Simulation class: Associate networks and worlds with
-     * their respective components. Entries are added when networks or worlds
-     * are added using the sim object. Facilitates making couplings using
-     * methods with fewer arguments.
-     */
-	Hashtable<Network, NetworkComponent> netMap = new Hashtable();
-	Hashtable<OdorWorld, OdorWorldComponent> odorMap = new Hashtable();
-
-	/**
 	 * A list of brains. Good for updating and maintaining multiple brains
 	 */
-	List<CreaturesBrain> brainList = new ArrayList();
+	private List<CreaturesBrain> brainList = new ArrayList();
 
 	@Override
 	public void run() {
@@ -45,41 +38,24 @@ public class Creatures extends RegisteredSimulation {
 		// Clear workspace
 		sim.getWorkspace().clearWorkspace();
 
-		// Add doc viewer // TODO: Rename to creaturesDocs.html or something
+		// Add doc viewer
 		sim.addDocViewer(0, 0, 450, 600, "Doc",
-				"src/org/simbrain/custom_sims/simulations/creatures/CreaturesMain.html");
+				"src/org/simbrain/custom_sims/simulations/creatures/CreaturesDoc.html");
 
 		// Create starting brain
-		CreaturesBrain brain1 = createBrain(451, 0, 550, 600, "Ron");
-		brainList.add(brain1);
-			
-		// TODO: Rename / redesign as you like
-		initBrain1(brain1);
-		
+		CreaturesBrain ronBrain = createBrain(451, 0, 550, 600, "Ron");
+		initDefaultBrain(ronBrain);
+
 		// Create update action
-		sim.getWorkspace().addUpdateAction(new UpdateActionAdapter("Update Creatures Sim") {
-			@Override
-			public void invoke() {
-				updateCreaturesSim();
-			}
-		});
+		sim.getWorkspace().addUpdateAction(
+				new UpdateActionAdapter("Update Creatures Sim") {
+					@Override
+					public void invoke() {
+						updateCreaturesSim();
+					}
+				});
 
 	}
-
-    private void initBrain1(CreaturesBrain brain1) {
-        // Assign a neuron a label then access the neuron using that label 
-        brain1.lobes.get(0).getNeuronList().get(0).setLabel("Hungry");
-        brain1.lobes.get(0).getNeuronList().get(1).setLabel("Happy");
-        Neuron hungerNeuron = brain1.lobes.get(0).getNeuronByLabel("Hungry");
-        Neuron happyNeuron = brain1.lobes.get(0).getNeuronByLabel("Happy");
-		hungerNeuron.setActivation(4);
-		brain1.getNetwork().fireNeuronsUpdated(); // So the activation is visible
-
-		// Adding a custom synapse.   Prob. not necessary to give them names in most cases...
-        Synapse happyToHungry = brain1.builder.connect(hungerNeuron,
-                happyNeuron, new CreaturesSynapseRule(), 1);
-		brain1.getNetwork().fireSynapseAdded(happyToHungry);
-    }
 
 	/**
 	 * Update function for the Creatures simulation.
@@ -91,52 +67,188 @@ public class Creatures extends RegisteredSimulation {
 		}
 
 	}
-	//TODO: Refactor below.
-	
+
+	/**
+	 * Gives an empty creature's brain a network from a template.
+	 * @param brain
+	 */
+	private void initDefaultBrain(CreaturesBrain brain) {
+		// Init Lobe #1: Drives
+		NeuronGroup drives =
+				brain.createLobe(1580, 1300, 12, "grid", "Lobe #1: Drives");
+		brain.setLobeColumns(drives, 6);
+		brain.nameNeuron(drives, 0, "Pain");
+		brain.nameNeuron(drives, 1, "Comfort");
+		brain.nameNeuron(drives, 2, "Hunger");
+		brain.nameNeuron(drives, 3, "Temperature");
+		brain.nameNeuron(drives, 4, "Fatigue");
+		brain.nameNeuron(drives, 5, "Drowsiness");
+		brain.nameNeuron(drives, 6, "Lonliness");
+		brain.nameNeuron(drives, 7, "Crowdedness");
+		brain.nameNeuron(drives, 8, "Fear");
+		brain.nameNeuron(drives, 9, "Boredom");
+		brain.nameNeuron(drives, 10, "Anger");
+		brain.nameNeuron(drives, 11, "Arousal");
+
+		// Init Lobe #2: Stimulus Source
+		NeuronGroup stimulus =
+				brain.createLobe(610, 995, 7,
+						"line", "Lobe #2: Stimulus Source");
+		brain.nameNeuron(stimulus, 0, "Toy");
+		brain.nameNeuron(stimulus, 1, "Fish");
+		brain.nameNeuron(stimulus, 2, "Cheese");
+		brain.nameNeuron(stimulus, 3, "Poison");
+		brain.nameNeuron(stimulus, 4, "Hazard");
+		brain.nameNeuron(stimulus, 5, "Flower");
+		brain.nameNeuron(stimulus, 6, "Mouse");
+
+		// Init Lobe #3: Verbs
+		NeuronGroup verbs =
+				brain.createLobe(1715, 1000, 13, "grid", "Lobe #3: Verbs");
+		brain.setLobeColumns(verbs, 7);
+		brain.nameNeuron(verbs, 0, "Wait");
+		brain.nameNeuron(verbs, 1, "Left");
+		brain.nameNeuron(verbs, 2, "Right");
+		brain.nameNeuron(verbs, 3, "Forward");
+		brain.nameNeuron(verbs, 4, "Backward");
+		brain.nameNeuron(verbs, 5, "Sleep");
+		brain.nameNeuron(verbs, 6, "Approach");
+		brain.nameNeuron(verbs, 7, "Ingest");
+		brain.nameNeuron(verbs, 8, "Look");
+		brain.nameNeuron(verbs, 9, "Smell");
+		brain.nameNeuron(verbs, 10, "Attack");
+		brain.nameNeuron(verbs, 11, "Play");
+		brain.nameNeuron(verbs, 12, "Mate");
+
+		// Init Lobe #4: Nouns
+		NeuronGroup nouns =
+				brain.createLobe(910, -40, stimulus.size(), "line",
+						"Lobe #4: Nouns");
+		brain.copyLabels(stimulus, nouns);
+
+		// Init Lobe #5: General Senses
+		NeuronGroup senses =
+				brain.createLobe(1490, 1550, 14,
+						"grid", "Lobe #5: General Senses");
+		brain.setLobeColumns(senses, 7);
+		brain.nameNeuron(senses, 0, "Attacked");
+		brain.nameNeuron(senses, 1, "Playing");
+		brain.nameNeuron(senses, 2, "User Talked");
+		brain.nameNeuron(senses, 3, "Mouse Talked");
+		brain.nameNeuron(senses, 4, "It Approaches");
+		brain.nameNeuron(senses, 5, "It is Near");
+		brain.nameNeuron(senses, 6, "It Retreats");
+		brain.nameNeuron(senses, 7, "Is Object");
+		brain.nameNeuron(senses, 8, "Is Mouse");
+		brain.nameNeuron(senses, 9, "Is Parent");
+		brain.nameNeuron(senses, 10, "Is Sibling");
+		brain.nameNeuron(senses, 11, "Is Child");
+		brain.nameNeuron(senses, 12, "Opposite Sex");
+		brain.nameNeuron(senses, 13, "Audible Event");
+
+		// Init Lobe #6: Decisions
+		NeuronGroup decisions =
+				brain.createLobe(2510, 440, verbs.size(),
+						"vertical line", "Lobe #6: Decisions");
+		brain.copyLabels(verbs, decisions);
+
+		// Init Lobe #7: Attention
+		NeuronGroup attention =
+				brain.createLobe(2090, 1260, stimulus.size(),
+						"vertical line", "Lobe #7: Attention");
+		brain.copyLabels(stimulus, attention);
+
+		// Init Lobe #8: Concepts
+		NeuronGroup concepts =
+				brain.createLobe(460, 95, 640, "grid", "Lobe #8: Concepts");
+		brain.setLobeColumns(concepts, 40);
+
+		// Init Lobe #0: Perception
+		NeuronGroup perception =
+				brain.createLobe(65, 440,
+							(drives.size() + verbs.size()
+							+ senses.size() + attention.size()),
+							"grid", "Lobe #0: Perception");
+		brain.setLobeColumns(perception, 7);
+		// Copy labels from multiple lobes to this one
+		for (int i = 0; i < perception.size(); i++) {
+			// Labeling after Lobe #1
+			if (i < drives.size()) {
+				brain.nameNeuron(perception, i,
+						brain.getNeuronLabel(drives, i));
+			}
+			// Labeling after Lobe #3
+			else if (i < (drives.size() + verbs.size())) {
+				brain.nameNeuron(perception, i,
+						brain.getNeuronLabel(verbs, i - drives.size()));
+			}
+			// Labeling after Lobe #5
+			else if (i < (drives.size() + verbs.size() + senses.size())) {
+				brain.nameNeuron(perception, i,
+						brain.getNeuronLabel(senses,
+								i - (drives.size() + verbs.size())));
+			}
+			// Labeling after Lobe #7
+			else {
+				brain.nameNeuron(perception, i,
+						brain.getNeuronLabel(attention, i
+								- (drives.size()
+										+ verbs.size() + senses.size())));
+			}
+		}
+
+	}
+
 	/**
 	 * Creates a brain network and displays it in the workspace.
-	 * 
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
 	 * @param name
 	 * @return a CreaturesBrain object
 	 */
-	public CreaturesBrain createBrain(int x, int y, int width, int height, String name) {
-		NetworkComponent networkComponent = new NetworkComponent(name + "'s Brain");
-		sim.getWorkspace().addWorkspaceComponent(networkComponent);
-		sim.getDesktop().getDesktopComponent(networkComponent).getParentFrame().setBounds(x, y, width, height);
-		netMap.put(networkComponent.getNetwork(), networkComponent);
-
-		CreaturesBrain brain = new CreaturesBrain(networkComponent);
-
-		brain.setUp();
+	public CreaturesBrain createBrain(int x, int y,
+			int width, int height, String name) {
+		NetBuilder net = sim.addNetwork(x, y, width, height, name + "'s Brain");
+		CreaturesBrain brain = new CreaturesBrain(net);
+		brainList.add(brain);
 		return brain;
 	}
 
-	public CreaturesBrain createBrain(int x, int y, int width, int height) {
-	    return createBrain(x,y,width,height,"Brain");
+	public CreaturesBrain createBrain(int x, int y,
+			int width, int height) {
+		return createBrain(x, y, width, height, "Creature");
 	}
-	
-    public Creatures(SimbrainDesktop desktop) {
-        super(desktop);
-    }
 
-    public Creatures() {
-        super();
-    }
+	/**
+	 * Constructor.
+	 * @param desktop
+	 */
+	public Creatures(SimbrainDesktop desktop) {
+		super(desktop);
+	}
 
-    /**
-     * Runs the constructor for the simulation.
-     */
-    @Override
-    public Creatures instantiate(SimbrainDesktop desktop) {
-        return new Creatures(desktop);
-    }
+	public Creatures() {
+		super();
+	}
 
-    /**
-     * Supplies the name of the simulation.
-     */
-    @Override
-    public String getName() {
-        return "Creatures";
-    }
+	/**
+	 * Runs the constructor for the simulation.
+	 */
+	@Override
+	public Creatures instantiate(SimbrainDesktop desktop) {
+		return new Creatures(desktop);
+	}
+
+	// Accessor methods below this point
+	@Override
+	public String getName() {
+		return "Creatures";
+	}
+
+	public List<CreaturesBrain> getBrainlist() {
+		return brainList;
+	}
 
 }
--- src/org/simbrain/custom_sims/simulations/creatures/CreaturesBrain.java
+++ src/org/simbrain/custom_sims/simulations/creatures/CreaturesBrain.java
@@ -7,59 +7,191 @@ import org.simbrain.custom_sims.helper_classes.NetBuilder;
 import org.simbrain.network.NetworkComponent;
 import org.simbrain.network.core.Network;
 import org.simbrain.network.groups.NeuronGroup;
+import org.simbrain.network.layouts.GridLayout;
 
 /**
  * A helper class of Creatures for filling in networks, from either a base
  * template or from genetic code. (Better here than cluttering up the main
  * class)
- * 
+ *
  * @author Sharai
  *
  */
 public class CreaturesBrain {
-    
-    //TODO: General OOP principle.  Anything common across all creature brains
-    //  should be here.Also any methods for easily customizing
+
+    // Anything common across all creature brains
+    // should be here. Also any methods for easily customizing
     // creature brains should be here. Then at Creatures.java level
     // individual brains can be further customized.
 
-    // TODO: Make private then add accessor
-    public List<NeuronGroup> lobes = new ArrayList();
-    public NetBuilder builder;
+	/**
+	 * List of lobes.
+	 */
+    private List<NeuronGroup> lobes = new ArrayList();
+    /**
+     * The NetBuilder object this wraps around.
+     */
+    private NetBuilder builder;
+    /**
+     * The amount of space to spread between neurons.
+     */
+    private double gridSpace = 50;
 
+    /**
+     * Constructor.
+     * @param component
+     */
     public CreaturesBrain(NetworkComponent component) {
         this.builder = new NetBuilder(component);
     }
 
     /**
-     * Sets up a "brain" network from a template.
-     * 
-     * @param brain The network to set up the brain in
+     * Constructor.
+     * @param netBuilder
      */
-    public void setUp() {
-        System.out.println("One empty thinkpan, coming up!");
-
-        // Set up Lobe #1: Drive Lobe
-        NeuronGroup driveLobe = builder.addNeuronGroup(0, 0, 12, "grid",
-                new CreaturesNeuronRule());
-        driveLobe.setLabel("Drive Lobe");
-        lobes.add(driveLobe);
-
-        // Label drive lobe neurons
-        driveLobe.getNeuronList().get(0).setLabel("Myself");
-
-        // Set up Lobe #2: Stimulus Source Lobe
-        NeuronGroup stimulusLobe = builder.addNeuronGroup(150, 0, 10, "grid",
-                new CreaturesNeuronRule());
-        stimulusLobe.setLabel("Object Lobe");
-        lobes.add(stimulusLobe);
-        
-        // set up standard node labels
-
+    public CreaturesBrain(NetBuilder netBuilder) {
+    	this.builder = netBuilder;
     }
 
+    /**
+     * Creates a new lobe.
+     * @param x
+     * @param y
+     * @param numNeurons
+     * @param layoutName Valid input includes "line",
+     * "vertical line", and "grid".
+     * @param lobeName (optional)
+     * @param neuronRule (optional)
+     * @return
+     */
+    public NeuronGroup createLobe(double x, double y,
+    		int numNeurons, String layoutName, String lobeName,
+    		CreaturesNeuronRule neuronRule) {
+    	NeuronGroup lobe = builder.addNeuronGroup(x, y, numNeurons,
+    			layoutName, neuronRule);
+    	lobe.setLabel(lobeName);
+    	lobes.add(lobe);
+    	return lobe;
+    }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param numNeurons
+     * @param layoutName
+     * @param lobeName
+     * @return
+     */
+    public NeuronGroup createLobe(double x, double y, 
+    		int numNeurons, String layoutName, String lobeName) {
+    	return createLobe(x, y, numNeurons, layoutName,
+    			lobeName, new CreaturesNeuronRule());
+    }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param numNeurons
+     * @param layoutName
+     * @param neuronRule
+     * @return
+     */
+    public NeuronGroup createLobe(double x, double y,
+    		int numNeurons, String layoutName,
+    		CreaturesNeuronRule neuronRule) {
+    	return createLobe(x, y, numNeurons, layoutName,
+    			"Lobe #" + lobes.size(), neuronRule);
+    }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @param numNeurons
+     * @param layoutName
+     * @return
+     */
+    public NeuronGroup createLobe(double x, double y,
+    		int numNeurons, String layoutName) {
+    	return createLobe(x, y, numNeurons, layoutName,
+    			"Lobe #" + lobes.size());
+    }
+
+   /**
+    * Names a neuron.
+    * @param lobe
+    * @param neuronIndex
+    * @param name
+    */
+    public void nameNeuron(NeuronGroup lobe, int neuronIndex, String name) {
+    	lobe.getNeuronList().get(neuronIndex).setLabel(name);
+    }
+
+    /**
+     * Names a neuron.
+     * @param lobeIndex
+     * @param neuronIndex
+     * @param name
+     */
+    public void nameNeuron(int lobeIndex, int neuronIndex, String name) {
+    	nameNeuron(lobes.get(lobeIndex), neuronIndex, name);
+    }
+
+    /**
+	 * Copies neuron labels from one neuron group 
+	 * to another if they are the same size.
+	 * @param lobeToCopy
+	 * @param lobeToPaste
+	 */
+	public void copyLabels(NeuronGroup lobeToCopy, NeuronGroup lobeToPaste) {
+		if (lobeToCopy.size() == lobeToPaste.size()) {
+			for (int i = 0; i < lobeToCopy.size(); i++) {
+				lobeToPaste.getNeuronList().get(i).setLabel(
+						lobeToCopy.getNeuronList().get(i).getLabel());
+			}
+		} else {
+			System.out.print(
+					"copyToLabels error: Lobe " + lobeToPaste.getLabel()
+					+ " is not the same size as Lobe " + lobeToCopy.getLabel());
+		}
+	}
+
+	/**
+	 * Manually gives a grid layout with a 
+	 * set number of columns to a certain lobe.
+	 * @param lobe
+	 * @param numColumns
+	 */
+	public void setLobeColumns(NeuronGroup lobe, int numColumns) {
+		GridLayout gridLayout = new GridLayout(
+				gridSpace, gridSpace, numColumns);
+		lobe.setLayout(gridLayout);
+		lobe.applyLayout();
+	}
+
+    // Accessor methods below this point
     public Network getNetwork() {
         return builder.getNetwork();
+    }
+
+    public List<NeuronGroup> getLobeList() {
+    	return lobes;
+    }
+
+    public NetBuilder getBuilder() {
+    	return builder;
+    }
+
+    /**
+     * Returns the label of a neuron of a given lobe.
+     * @param lobe
+     * @param neuronIndex
+     * @return String
+     */
+    public String getNeuronLabel(NeuronGroup lobe, int neuronIndex) {
+    	return lobe.getNeuronList().get(neuronIndex).getLabel();
     }
 
 }

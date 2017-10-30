edd121309e52580c64e0b525424491416dbf159f
Leo Yulin Li <LeoYulinLi@gmail.com>
2017-10-30 13:34:07 -0700

    Resolve compile error
    Since copy() was implemented, SynapseIndex would not be used, but keep
    it for now.

---
+++ src/org/simbrain/custom_sims/simulations/simpleNeuroevolution/SynapseIndex.java
@@ -0,0 +1,40 @@
+package org.simbrain.custom_sims.simulations.simpleNeuroevolution;
+
+
+public class SynapseIndex {
+	int src;
+	int tgt;
+	double str;
+	
+	public SynapseIndex(int src, int tgt, double str) {
+		setSrc(src);
+		setTgt(tgt);
+		setStrength(str);
+	}
+
+	public void setSrc(int src) {
+		this.src = src;
+	}
+	
+	public void setTgt(int tgt) {
+		this.tgt = tgt;
+	}
+	
+	public void setStrength(double str) {
+		this.str = str;
+	}
+	
+	public int getSrc() {
+		return this.src;
+	}
+	
+	public int getTgt() {
+		return this.tgt;
+	}
+	
+	
+	public double getStrength() {
+		return this.str;
+	}
+	
+}

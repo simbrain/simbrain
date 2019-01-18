7c2510b1655303c0835f282b471fcb4b3a665a7f
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2019-01-17 17:44:36 -0800

    Undoing my test merge 785b1f4

--- src/org/simbrain/workspace/gui/Splasher.java
+++ src/org/simbrain/workspace/gui/Splasher.java
@@ -61,7 +61,5 @@ public class Splasher {
         SplashWindow.invokeMain("org.simbrain.workspace.gui.SimbrainDesktop",
             args);
         SplashWindow.disposeSplash();
-
-        System.out.println("Hello World");
     }
 }

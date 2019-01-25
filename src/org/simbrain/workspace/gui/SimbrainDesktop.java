44c60db5bd1f28d4a6596e7a705ac019e426803a
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2019-01-25 15:34:08 -0800

    Updating master version number

--- src/org/simbrain/workspace/gui/SimbrainDesktop.java
+++ src/org/simbrain/workspace/gui/SimbrainDesktop.java
@@ -207,7 +207,7 @@ public class SimbrainDesktop {
     private JLabel runningLabel = new JLabel();
 
     /** Name to display in Simbrain desktop window. */
-    private static String FRAME_TITLE = "Simbrain 3.03";
+    private static String FRAME_TITLE = "Simbrain 3.04 beta";
 
     /**
      * Associates workspace components with their corresponding gui components.

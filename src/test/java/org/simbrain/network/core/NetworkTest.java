f1b010aeede217543a9e19fdde87cf4c3d12df0f
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2020-03-26 20:13:37 -0700

    Network xml test and initial work on workspace test

--- src/test/java/org/simbrain/network/core/NetworkTest.java
+++ src/test/java/org/simbrain/network/core/NetworkTest.java
@@ -1,33 +1,56 @@
 package org.simbrain.network.core;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.simbrain.network.groups.NeuronGroup;
+import org.simbrain.util.Utils;
+import org.simbrain.workspace.WorkspaceComponent;
 
 import static org.junit.Assert.*;
 
 public class NetworkTest {
 
-    @Test
-    public void getByLabel() {
-        Network net = new Network();
+    Network net;
+    Neuron n1, n2;
+    NeuronGroup ng1, ng2;
+    // TODO: Collections, neuronarrays, synapsegroups, update, copy/paste
 
-        Neuron n1 = new Neuron(net);
+    @Before
+    public void setUpNetwork() {
+        net = new Network();
+
+        n1 = new Neuron(net);
         n1.setLabel("neuron1");
         net.addLooseNeuron(n1);
-        Neuron n2 = new Neuron(net);
+        n2 = new Neuron(net);
         n2.setLabel("neuron2");
         net.addLooseNeuron(n2);
-        assertEquals(n1, net.getNeuronByLabel("neuron1"));
-        assertEquals(n2, net.getNeuronByLabel("neuron2"));
 
-        NeuronGroup ng1 = new NeuronGroup(net);
+        ng1 = new NeuronGroup(net);
         ng1.setLabel("neuron_group_1");
         net.addNeuronGroup(ng1);
-        NeuronGroup ng2 = new NeuronGroup(net);
+        ng2 = new NeuronGroup(net);
         ng2.setLabel("ng2");
         net.addNeuronGroup(ng2);
+    }
+
+    @Test
+    public void getByLabel() {
+        assertEquals(n1, net.getNeuronByLabel("neuron1"));
+        assertEquals(n2, net.getNeuronByLabel("neuron2"));
         assertEquals(ng1, net.getNeuronGroupByLabel("neuron_group_1"));
         assertEquals(ng2, net.getNeuronGroupByLabel("ng2"));
+    }
+
+    @Test
+    public void testXML() {
+        String xmlRep = Utils.getSimbrainXStream().toXML(net);
+        Network fromXml = (Network) Utils.getSimbrainXStream().fromXML(xmlRep);
+
+        assertNotNull(fromXml.getNeuronByLabel("neuron1") );
+        assertNotNull(fromXml.getNeuronByLabel("neuron2") );
+        assertNotNull(fromXml.getNeuronGroupByLabel("neuron_group_1") );
+        assertNotNull(fromXml.getNeuronGroupByLabel("ng2") );
 
     }
 }
---
+++ src/test/java/org/simbrain/workspace/WorkspaceTest.java
@@ -0,0 +1,50 @@
+package org.simbrain.workspace;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.simbrain.network.NetworkComponent;
+import org.simbrain.plot.projection.ProjectionComponent;
+import org.simbrain.workspace.serialization.WorkspaceSerializer;
+import org.simbrain.world.odorworld.OdorWorldComponent;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class WorkspaceTest {
+
+    Workspace workspace;
+
+    @Before
+    public void setUpTestWorkspace() {
+        workspace = new Workspace();
+        workspace.addWorkspaceComponent(new NetworkComponent("network"));
+        workspace.addWorkspaceComponent(new OdorWorldComponent("odorworld"));
+        workspace.addWorkspaceComponent(new ProjectionComponent("projection"));
+
+        // TODO: Set up a coupling and test it
+
+    }
+
+    @Test
+    public void testComponents() {
+        assertEquals(3, workspace.getComponentList().size());
+    }
+
+    @Test
+    public void testSerialization() {
+        WorkspaceSerializer serializer = new WorkspaceSerializer(workspace);
+        try {
+            serializer.serialize(System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // TODO: Improve the serialization api a bit to make it easier to test
+        // (I also always thought it was kind of hard to use)
+    }
+
+
+
+
+    }
+}

7a4dca3d676f599e43a26752acafdc501f7cd0c6
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2020-11-04 17:35:11 -0800

    Initial work on integer evolution in new framework

--- src/main/kotlin/org/simbrain/util/geneticalgorithms/IntegerGenetics.kt
+++ src/main/kotlin/org/simbrain/util/geneticalgorithms/IntegerGenetics.kt
@@ -1,19 +1,64 @@
 package org.simbrain.util.geneticalgorithms
 
-// TODO: Design problem Yulin mentioned.  Also cohere with IntEvolution and IntEvolutionDSL.
-inline fun intGene(initVal : Int): IntGene {
-    return IntGene(initVal)
+import kotlin.random.Random
+
+
+// Yulin will make work and make corresponding API changes
+// Also cohere with IntEvolution and IntEvolutionDSL.
+
+class IntWrapper (var value: Int = 0)
+
+inline fun intGene(initVal : IntWrapper.() -> Unit = { }): IntGene {
+    return IntGene(IntWrapper().apply(initVal))
 }
 
-class IntGene (template: Int) : Gene<Int>(template) {
+class IntGene (template: IntWrapper) : Gene<IntWrapper>(template) {
 
     override fun copy(): IntGene {
         return IntGene(template);
     }
 
-    fun build() : Int {
+    fun build() : IntWrapper {
         return template;
     }
+}
+
+fun main() {
+
+    val environmentBuilder = environmentBuilder {
+
+        val intChromosome = chromosome(100) {
+            intGene{value = 5}
+        }
+
+        onMutate {
+            intChromosome.eachMutate {
+                value = Random.nextInt(-2,2)
+            }
+        }
+
+        onBuild {
+            intChromosome.genes.forEach{
+                it.build()
+            }
+        }
+
+        onEval {
+            val total = intChromosome.products.sumByDouble {it.value.toDouble() }
+            Math.abs(total - 10)
+        }
+
+    }
+
+    val evolution  = evaluator(environmentBuilder) {
+        populationSize = 100
+        eliminationRatio = 0.5
+        optimizationMethod = Evaluator.OptimizationMethod.MINIMIZE_FITNESS
+        runUntil { generation == 500 || fitness < .2 }
+    }
+
+    val best = evolution.start().last().first()
+    print(best)
 
 }
 

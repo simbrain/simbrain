870ae4fc065f6c8161a81164bf0ef5a2e8496d2f
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2020-11-13 14:55:29 -0800

    Quick cleanup on datavector example.

--- src/main/java/org/simbrain/network/dl4j/DL4JSandbox/DataVectorExample.java
+++ src/main/java/org/simbrain/network/dl4j/DL4JSandbox/DataVectorExample.java
@@ -3,22 +3,12 @@ package org.simbrain.network.dl4j.DL4JSandbox;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
 
-import java.util.Date;
-
 /**
  * <b>Reference: https://github.com/SkymindIO/screencasts/tree/master/datavec_spark_transform</b>
  */
 public class DataVectorExample {
 
     public static void main(String[] args) throws Exception {
-        int numLinesToSkip = 0;
-        String delimiter = ",";
-
-        String baseDir = "C:\\Users\\kenfukuyama\\Documents\\GitHub\\simbrain\\src\\main\\java\\org\\simbrain\\network\\dl4j\\DL4JSandbox\\";
-        String fileName = "reports.csv";
-        String inputPath = baseDir + fileName;
-        String timeStamp = String.valueOf(new Date().getTime());
-        String outputPath = baseDir + "reports_processed_" + timeStamp;
 
         // Changing the column names based on the data type.
         Schema inputDataSchema = new Schema.Builder()
@@ -40,6 +30,8 @@ public class DataVectorExample {
                     " (" + tp.getActionList().get(i) + ")--" );
             System.out.println(tp.getSchemaAfterStep(i));
         }
+
+        // TODO: External libraries are needed to complete the tutorial
 
     }
 

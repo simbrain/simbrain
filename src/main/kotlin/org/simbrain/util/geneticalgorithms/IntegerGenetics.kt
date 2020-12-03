72166250a1ebebf3fa62f7b18ca73075e360d97c
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2020-12-02 16:40:47 -0800

    Adding comments to IntegerGenetics.kt and removing some old code.

--- src/main/kotlin/org/simbrain/util/geneticalgorithms/IntegerGenetics.kt
+++ src/main/kotlin/org/simbrain/util/geneticalgorithms/IntegerGenetics.kt
@@ -4,10 +4,10 @@ import org.simbrain.util.propertyeditor.CopyableObject
 import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
-
-// Yulin will make work and make corresponding API changes
-// Also cohere with IntEvolution and IntEvolutionDSL.
-
+/**
+ * The "gene product" that we are evolving. Must be a [CopyableObject].
+ * Must take some mutable var property (here value) so that something can happen during mutation.
+ */
 class IntWrapper(var value: Int = 0) : CopyableObject {
     override fun toString(): String {
         return "" + value
@@ -18,10 +18,18 @@ class IntWrapper(var value: Int = 0) : CopyableObject {
     }
 }
 
+/**
+ * Builder function for [IntGene].
+ */
 inline fun intGene(initVal: IntWrapper.() -> Unit = { }): IntGene {
     return IntGene(IntWrapper().apply(initVal))
 }
 
+/**
+ * The integer gene, which takes a template "product" ([IntWrapper]) as an argument and extends
+ * [Gene] and implements [TopLevelGene], which allows you to add this gene directly into an onBuild context.
+ * function.
+ */
 class IntGene(template: IntWrapper) : Gene<IntWrapper>(template), TopLevelGene<IntWrapper> {
 
     override fun copy(): IntGene {
@@ -33,12 +41,19 @@ class IntGene(template: IntWrapper) : Gene<IntWrapper>(template), TopLevelGene<I
     }
 }
 
+/**
+ * The main integer evolution simulation.
+ */
 fun main() {
 
     val environmentBuilder = environmentBuilder {
 
-        val intChromosome = chromosome(10) {
-            intGene { value = -100 }
+        /**
+         * Set number of integers per chromosome here
+         */
+        val intChromosome = chromosome(5) {
+            // Default value for integers
+            intGene { value = 1 }
         }
 
         onMutate {
@@ -53,10 +68,12 @@ fun main() {
 
         onEval {
             val total = intChromosome.products.sumByDouble { it.value.toDouble() }
-            Math.abs(total - 10)
+            val targetSum = 10
+            Math.abs(total - targetSum)
         }
 
         onPeek {
+            print("Integer genes:")
             println(intChromosome.products.map { it.value }.joinToString(", "))
         }
 
@@ -72,7 +89,7 @@ fun main() {
     val time = measureTimeMillis {
         val (builder, fitness) = evolution.start().last().first()
         builder.build().peek()
-        println(fitness)
+        println("Fitness: $fitness $")
     }
 
     println("Finished in ${time / 1000.0}s")

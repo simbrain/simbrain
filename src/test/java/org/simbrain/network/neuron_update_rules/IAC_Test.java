562d19f09b0bb43be6526c49883b9a9a7b82391a
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2021-02-05 14:16:50 -0800

    Improvements to IAC test

--- src/test/java/org/simbrain/network/neuron_update_rules/IAC_Test.java
+++ src/test/java/org/simbrain/network/neuron_update_rules/IAC_Test.java
@@ -3,45 +3,45 @@ package org.simbrain.network.neuron_update_rules;
 import org.junit.Test;
 import org.simbrain.network.core.Network;
 import org.simbrain.network.core.Neuron;
-import org.simbrain.network.core.Synapse;
-import org.simbrain.util.math.SquashingFunctionEnum;
+
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class IAC_Test {
     @Test
     public void testUpdate1InputToOutput() {
+
         Network net = new Network();
+        net.setTimeStep(1); // Simplifies computations
+
+        // Set up the IAC node.
         IACRule iacRule = new IACRule();
-
-        // Setup the input neuron
-        Neuron input1 =  new Neuron(net);
-        input1.setActivation(0.5);
-        input1.setClamped(true);
-        net.addLooseNeuron(input1);
-
-        // Set up the IAC rule.
         iacRule.setUpperBound(1);
         iacRule.setLowerBound(-1);
-        iacRule.setDecay(0.05);
+        Neuron iacNeuron = new Neuron(net, iacRule);
+        iacNeuron.setActivation(0.0);
+        net.addLooseNeuron(iacNeuron);
+
+        // See if it decays to provided rest value
         iacRule.setRest(0.1);
+        IntStream.range(0, 100).forEach(v -> net.update());
+        assertEquals(0.1, iacNeuron.getActivation(), 0.01);
 
-        // Set up the output neuron
-        Neuron output = new Neuron(net, iacRule);
-        output.setActivation(0.0);
-        net.addLooseNeuron(output);
-
-        // Connect the input to the output
-        Synapse w12 = new Synapse(input1, output, 1);
-        net.addLooseSynapse(w12);
-
+        // Test decay from above
+        iacNeuron.setActivation(1);
+        iacRule.setDecay(.05);
         // Upper Bound (u) = 1, lower bound (l) = -1, decay rate(λ) = 0.05, resting value (b) = 0.1,
-        // weighted input (W) = 0.5 * 1, where w > 0
-        // The interactive activation and competition networks
+        // weighted input (W) = 0
+        // a = 1 - ((1 - 1)0 - 0.05 (1 - 0.1)) = .955, where w > 0
         net.update();
-        // a = 0 + ((1 - 0)(0.5) - 0.05 (0 - 0.1)) = 0.505, where w > 0
-        // Timestampd is 0.1, so it's 0.0505 instead of 0.505
-        assertEquals(0.505, output.getActivation(), 0.00001);
+        assertEquals(0.955, iacNeuron.getActivation(), 0.00001);
+
+        // Decay from below
+        iacNeuron.setActivation(-1);
+        // a = -1 - ((1 - 1)0 - 0.05 (-1 - 0.1)) = -.945, where w < 0
+        net.update();
+        assertEquals(0-.945, iacNeuron.getActivation(), 0.00001);
 
     }
 

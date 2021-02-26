ee0f08e7ed1820f89b62b4f5aa65e5e661396c2d
Devanshu Kumar <devanshu.kumar07@gmail.com>
2021-02-26 07:25:09 -0800

    added compute time statements in WeightMatrixTest

--- src/test/java/org/simbrain/network/dl4j/WeightMatrixTest.java
+++ src/test/java/org/simbrain/network/dl4j/WeightMatrixTest.java
@@ -18,22 +18,42 @@ public class WeightMatrixTest {
         NeuronGroup ng2 = new NeuronGroup(net, 2);
         WeightMatrix wm = new WeightMatrix(net, ng1, ng2);
 
+        long start_time;
+        long stop_time;
+        long difference;
+
         // Set first entry to 4
+        start_time = System.currentTimeMillis();
         wm.getWeightMatrix().putScalar(0,4);
+        stop_time = System.currentTimeMillis();
+        difference = stop_time - start_time;
         assertEquals(4, wm.getWeightMatrix().getDouble(0),0.0);
+        System.out.println("Compute Time: "+difference+" ms");
 
         // Set to ((0,0);(0,0))
+        start_time = System.currentTimeMillis();
         wm.setWeights(new double[]{0,0,0,0});
+        stop_time = System.currentTimeMillis();
+        difference = stop_time-start_time;
         System.out.println(wm);
         assertEquals(0.0, wm.getWeightMatrix().sumNumber().doubleValue(),0.0);
+        System.out.println("Compute Time: "+difference+" ms");
 
         // Add 1 to each entry. Should get ((1,1);(1,1))
+        start_time = System.currentTimeMillis();
         wm.getWeightMatrix().addi(1.0);
+        stop_time = System.currentTimeMillis();
         System.out.println(wm);
+        difference = stop_time-start_time;
+        System.out.println("Compute Time: "+difference+" ms");
         assertEquals(4.0, wm.getWeightMatrix().sumNumber().doubleValue(),0.0);
 
         // Multiply by itself.  Should get ((2,2);(2,2))
+        start_time = System.currentTimeMillis();
         wm.getWeightMatrix().mmuli(wm.getWeightMatrix());
+        stop_time = System.currentTimeMillis();
+        difference = stop_time - start_time;
+        System.out.println("Compute Time: "+difference+" ms");
         System.out.println(wm);
         assertEquals(8.0, wm.getWeightMatrix().sumNumber().doubleValue(),0.0);
 
@@ -52,8 +72,12 @@ public class WeightMatrixTest {
         System.out.println(m3);
         System.out.println("------");
 
+        long start_time = System.currentTimeMillis();
         m1.mmuli(m2,m3);
+        long stop_time = System.currentTimeMillis();
+        long difference = stop_time - start_time;
 
+        System.out.println("Compute Time: "+difference+" ms");
         System.out.println(m1);
         System.out.println(m2);
         System.out.println(m3);

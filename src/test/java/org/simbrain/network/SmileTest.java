2e52e92f2be2c210f52d46b3f037d91c5a8d9adf
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2021-02-27 15:03:07 -0800

    Some improvements to weight matrix tests

--- src/test/java/org/simbrain/network/SmileTest.java
+++ src/test/java/org/simbrain/network/SmileTest.java
@@ -3,7 +3,9 @@ package org.simbrain.network;
 import org.junit.Test;
 import org.opencv.core.Mat;
 import smile.math.matrix.Matrix;
+
 import static org.junit.Assert.assertEquals;
+
 import smile.math.matrix.Matrix.EVD;
 import smile.stat.distribution.GaussianDistribution;
 
@@ -17,75 +19,77 @@ public class SmileTest {
     long difference;
 
     @Test
-    public void initialize_2d_matrix(){
+    public void initialize_2d_matrix() {
 
         // Create a 2x3 zero matrix
         start_time = System.currentTimeMillis();
-        var matrix_a = Matrix.eye(2,3);
+        var matrix_a = Matrix.eye(2, 3);
 
         var rows = matrix_a.nrows();
         var cols = matrix_a.ncols();
-        var result = "Shape: "+rows+" x "+cols;
+        var result = "Shape: " + rows + " x " + cols;
         // System.out.println("Shape: "+rows+" x "+cols);
         assertEquals("Shape: 2 x 3", result);
         stop_time = System.currentTimeMillis();
         difference = stop_time - start_time;
-        System.out.println("Compute time for 2d init: "+difference+" ms");
+        System.out.println("Compute time for 2d init: " + difference + " ms");
     }
 
     @Test
-    public void matrix_2x3_ones(){
-        double[][] ones = {{1.0,1.0,1.0},{1.0,1.0,1.0}};
-        var matrix_ones = new Matrix(2,3,ones);
+    public void matrix_2x3_ones() {
+        double[][] ones = {{1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}};
+        var matrix_ones = new Matrix(2, 3, ones);
         // System.out.println(matrix_ones);
         var sums = matrix_ones.sum();
         // System.out.println("Sums: "+sums);
-        assertEquals(6.0, sums,0.0);
+        assertEquals(6.0, sums, 0.0);
     }
 
     @Test
-    public void set_entry_matrix(){
+    public void set_entry_matrix() {
 
-        double[][] ones = {{1.0,1.0,1.0},{1.0,1.0,1.0}};
-        var matrix_ones = new Matrix(2,3,ones);
+        double[][] ones = {{1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}};
+        var matrix_ones = new Matrix(2, 3, ones);
         // Set the value of entry 1,3 to 3
 
-        matrix_ones.set(0,2,3);
+        matrix_ones.set(0, 2, 3);
 
         // Retrieve the value of that entry and confirm it is 3
 
         // System.out.println("Value at 1,3: "+matrix_ones.get(0,2));
-        assertEquals(3,matrix_ones.get(0,2),0.0);
+        assertEquals(3, matrix_ones.get(0, 2), 0.0);
     }
 
     @Test
-    public void matrix_multiplication(){
+    public void matrix_multiplication() {
         start_time = System.currentTimeMillis();
-        double[][] twos = {{2.0,2.0,2.0},{2.0,2.0,2.0}};
-        var matrix_twos = new Matrix(2,3,twos);
-        double[][] threes = {{3.0,3.0},{3.0,3.0},{3.0,3.0}};
-        var matrix_threes = new Matrix(3,2,threes);
+        double[][] twos = {{2.0, 2.0, 2.0}, {2.0, 2.0, 2.0}};
+        var matrix_twos = new Matrix(2, 3, twos);
+        double[][] threes = {{3.0, 3.0}, {3.0, 3.0}, {3.0, 3.0}};
+        var matrix_threes = new Matrix(3, 2, threes);
         stop_time = System.currentTimeMillis();
         // System.out.println(result_matrix);
         difference = stop_time - start_time;
-        System.out.println("Compute time for matrix product: "+difference+" ms");
+        System.out.println("Compute time for matrix product: " + difference + " ms");
     }
 
-    @Test
-    public void large_matrix_multiplication(){
-        var large_matrix =  new Matrix(500,500);
-        large_matrix.add(2);
+    // @Test
+    public void large_matrix_multiplication() {
+        var large_matrix = new Matrix(1000, 1000);
+        large_matrix.add(1.0001);
         start_time = System.currentTimeMillis();
-        large_matrix.mm(large_matrix);
+        for (int i = 0; i < 1000; i++) {
+            large_matrix.mm(large_matrix);
+        }
         stop_time = System.currentTimeMillis();
         difference = stop_time - start_time;
         System.out.println("Compute time for large matrix product: " + difference + " ms");
     }
 
     @Test
-    public void compute_matrix_eigenvalue(){
+    public void compute_matrix_eigenvalue() {
 
-        var large_matrix =  Matrix.rand(50,50, new GaussianDistribution(0,1));
+        var large_matrix = Matrix.rand(50, 50, new GaussianDistribution(0, 1));
 
         // Testing for eigenvalue
 
@@ -94,20 +98,20 @@ public class SmileTest {
         stop_time = System.currentTimeMillis();
         difference = stop_time - start_time;
         var eigen_matrix = new Matrix.EVD(eigenvalue.wr, eigenvalue.wi, eigenvalue.Vl, eigenvalue.Vr);
-        System.out.println("Compute time for eigenvalue computation: "+difference+" ms");
+        System.out.println("Compute time for eigenvalue computation: " + difference + " ms");
         // System.out.println(eigen_matrix.diag());
     }
 
     @Test
-    public void compute_matrix_LU_Decomposition(){
+    public void compute_matrix_LU_Decomposition() {
         //Matrix Decomposition
 
-        var large_matrix =  Matrix.rand(50,50, new GaussianDistribution(0,1));
+        var large_matrix = Matrix.rand(50, 50, new GaussianDistribution(0, 1));
         start_time = System.currentTimeMillis();
         var decompose = large_matrix.lu();
         stop_time = System.currentTimeMillis();
         difference = stop_time - start_time;
-        System.out.println("Compute time for LU Decomposition: "+difference+" ms");
+        System.out.println("Compute time for LU Decomposition: " + difference + " ms");
         // System.out.println(decompose.lu);
     }
 
--- src/test/java/org/simbrain/network/dl4j/WeightMatrixTest.java
+++ src/test/java/org/simbrain/network/dl4j/WeightMatrixTest.java
@@ -20,38 +20,40 @@ public class WeightMatrixTest {
 
         // Set first entry to 4
         long start_time = System.currentTimeMillis();
-        wm.getWeightMatrix().putScalar(0,4);
-        assertEquals(4, wm.getWeightMatrix().getDouble(0),0.0);
+        wm.getWeightMatrix().putScalar(0, 4);
+        assertEquals(4, wm.getWeightMatrix().getDouble(0), 0.0);
 
         // Set to ((0,0);(0,0))
         start_time = System.currentTimeMillis();
-        wm.setWeights(new double[]{0,0,0,0});
+        wm.setWeights(new double[]{0, 0, 0, 0});
         System.out.println(wm);
-        assertEquals(0.0, wm.getWeightMatrix().sumNumber().doubleValue(),0.0);
+        assertEquals(0.0, wm.getWeightMatrix().sumNumber().doubleValue(), 0.0);
 
         // Add 1 to each entry. Should get ((1,1);(1,1))
         wm.getWeightMatrix().addi(1.0);
         System.out.println(wm);
-        assertEquals(4.0, wm.getWeightMatrix().sumNumber().doubleValue(),0.0);
+        assertEquals(4.0, wm.getWeightMatrix().sumNumber().doubleValue(), 0.0);
 
         // Multiply by itself.  Should get ((2,2);(2,2))
         start_time = System.currentTimeMillis();
         wm.getWeightMatrix().mmuli(wm.getWeightMatrix());
         System.out.println(wm);
-        assertEquals(8.0, wm.getWeightMatrix().sumNumber().doubleValue(),0.0);
+        assertEquals(8.0, wm.getWeightMatrix().sumNumber().doubleValue(), 0.0);
     }
 
-    @Test
-    public void large_matrix_multiplication(){
+    // @Test
+    public void large_matrix_multiplication() {
 
         Network net = new Network();
-        NeuronGroup ng1 = new NeuronGroup(net, 500);
-        NeuronGroup ng2 = new NeuronGroup(net, 500);
+        NeuronGroup ng1 = new NeuronGroup(net, 1000);
+        NeuronGroup ng2 = new NeuronGroup(net, 1000);
         WeightMatrix wm = new WeightMatrix(net, ng1, ng2);
-        wm.getWeightMatrix().addi(2.0);
+        wm.getWeightMatrix().addi(1.0001);
 
         long start_time = System.currentTimeMillis();
+        // for (int i = 0; i < 1000; i++) {
         wm.getWeightMatrix().mmuli(wm.getWeightMatrix());
+        // }
         long stop_time = System.currentTimeMillis();
         long difference = stop_time - start_time;
         System.out.println("Compute time for large matrix product: " + difference + " ms");
@@ -60,9 +62,9 @@ public class WeightMatrixTest {
     // Scratch-pad for quick nd4j testing
     //@Test
     public void testMMuli() {
-        INDArray m1 = Nd4j.zeros(2,2);
-        INDArray m2 = Nd4j.ones(2,2);
-        INDArray m3 = Nd4j.ones(2,2);
+        INDArray m1 = Nd4j.zeros(2, 2);
+        INDArray m2 = Nd4j.ones(2, 2);
+        INDArray m3 = Nd4j.ones(2, 2);
         m3.addi(1);
 
         System.out.println(m1);
@@ -71,11 +73,11 @@ public class WeightMatrixTest {
         System.out.println("------");
 
         long start_time = System.currentTimeMillis();
-        m1.mmuli(m2,m3);
+        m1.mmuli(m2, m3);
         long stop_time = System.currentTimeMillis();
         long difference = stop_time - start_time;
 
-        System.out.println("Compute Time: "+difference+" ms");
+        System.out.println("Compute Time: " + difference + " ms");
         System.out.println(m1);
         System.out.println(m2);
         System.out.println(m3);

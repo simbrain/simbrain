51a3ec6ec13de9ce27c645177d945ee6dc1efcfd
LeoYulinLi <LeoYulinLi@gmail.com>
2021-09-17 18:05:07 -0700

    added windows build script vendor

--- etc/make_app.bat
+++ etc/make_app.bat
@@ -1,1 +1,1 @@
-jpackage --input ..\build\main\ --main-jar Simbrain.jar --dest ..\dist --name "Simbrain" --app-version 3.05 --icon simbrain.ico --java-options "-Duser.dir=`$APPDIR --illegal-access=permit" --win-menu --verbose
+jpackage --input ..\build\main\ --main-jar Simbrain.jar --dest ..\dist --name "Simbrain" --app-version 3.05 --icon simbrain.ico --java-options "-Duser.dir=`$APPDIR --illegal-access=permit" --win-menu --win-menu-group Simbrain --vendor Simbrain --verbose

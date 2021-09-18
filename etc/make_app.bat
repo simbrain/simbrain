48137f762e63471f09e569557cf053044efa21ea
LeoYulinLi <LeoYulinLi@gmail.com>
2021-09-17 17:49:28 -0700

    added windows build script

---
+++ etc/make_app.bat
@@ -0,0 +1,1 @@
+jpackage --input ..\build\main\ --main-jar Simbrain.jar --dest ..\dist --name "Simbrain" --app-version 3.05 --icon simbrain.ico --java-options "-Duser.dir=`$APPDIR --illegal-access=permit" --win-menu --verbose

ddabe5677ddbd7abf2d12c5a21aa279335b02715
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2021-12-06 17:53:50 -0800

    Updating packaging instructions with code signing information

--- etc/make_app.sh
+++ etc/make_app.sh
@@ -1,3 +1,9 @@
 # Assumes "ant build" has been called
 jpackage --input ../build/main/	 --main-jar Simbrain.jar --dest ../dist --name "Simbrain" \
 	 --app-version 3.05 --icon simbrain.icns --java-options "-Duser.dir=\$APPDIR --illegal-access=permit"
+
+# To code sign, unpackage the dmg after it's made, go to the folder it is in (dist) and run
+# `codesign -fs "University of California, Merced" Simbrain.app`
+# Then repackage it into a dmg by placing it in a folder and using disk utility File > New Image
+
+# TODO: fold code-signing into the jpackage command

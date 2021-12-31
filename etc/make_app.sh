2befd7f80520993b1dc47d2e55b76e29424ebd2c
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2021-12-30 17:19:36 -0800

    Updating build with new jvm args for xstream compatibility with java 17+.

--- etc/make_app.sh
+++ etc/make_app.sh
@@ -1,6 +1,7 @@
 # Assumes "ant build" has been called
 jpackage --input ../build/main/	 --main-jar Simbrain.jar --dest ../dist --name "Simbrain" \
-	 --app-version 3.05 --icon simbrain.icns --java-options "-Duser.dir=\$APPDIR --illegal-access=permit"
+	 --app-version 3.05 --icon simbrain.icns \
+	 --java-options "-Duser.dir=\$APPDIR --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.desktop/java.awt=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
 
 # To code sign, unpackage the dmg after it's made, go to the folder it is in (dist) and run
 # `codesign -fs "University of California, Merced" Simbrain.app`

5bc410b9c8c01d753f77752e6a22fceb0816eae7
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2022-04-28 18:29:09 -0700

    More work on ProbabilityDistributionTest.kt, with proper confidence interval computations

--- src/test/kotlin/org/simbrain/util/ProbabilityDistributionTest.kt
+++ src/test/kotlin/org/simbrain/util/ProbabilityDistributionTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.simbrain.util.stats.ProbabilityDistribution
 import org.simbrain.util.stats.distributions.*
-import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * Tests of probability distributions.
@@ -18,19 +18,19 @@ class ProbabilityDistributionTest {
 
     // TODO: Finish for all probability distributions
 
-    val alpha = .05 // For 1 - alpha confidence level
+    val alpha = .01 // For 1 - alpha confidence level
     val N = 1000
 
     @Test
     fun `test uniform`() {
-        val dist = UniformRealDistribution(0.0, 1.0)
-        var nums = dist.sampleDouble(N)
+        val sample = UniformRealDistribution(0.0, 1.0)
+        var nums = sample.sampleDouble(N)
         assertTrue(nums.average() in ((.5 - alpha)..(.5 + alpha)))
         assertTrue(nums.maxOrNull()!! <= 1.0)
         assertTrue(nums.minOrNull()!! >= 0.0)
-        dist.floor = -2.0
-        dist.ceil = -1.0
-        nums = dist.sampleDouble(N)
+        sample.floor = -2.0
+        sample.ceil = -1.0
+        nums = sample.sampleDouble(N)
         println(nums.average())
         assertTrue(nums.average() in ((-1.5 - alpha)..(-1.5 + alpha)))
         assertTrue(nums.maxOrNull()!! <= -1.0)
@@ -45,44 +45,57 @@ class ProbabilityDistributionTest {
         assertTrue(nums.minOrNull()!! >= 1.0)
     }
 
-    fun tscoreNormalMean(alpha: Double, sampleSize: Int, sampleStdev: Double): Double {
-        val tdist = TDistribution((sampleSize - 1).toDouble())
-        val stderr = sampleStdev / Math.sqrt(sampleSize.toDouble())
-        // This works but there is un unexplained negative
-        return -tdist.inverseCumulativeProbability(alpha / 2) * stderr
+    fun stderr(sampleStdev: Double, sampleSize: Int): Double {
+        return sampleStdev / sqrt(sampleSize.toDouble())
     }
 
-    // Rename lower/upper after reversal issue figured out
-    fun tscoreNormalVarianceLeft(alpha: Double, sampleSize: Int, sampleStdev: Double): Double {
-        val tdist = ChiSquaredDistribution((sampleSize - 1).toDouble())
-        return ((sampleSize - 1) / tdist.inverseCumulativeProbability(alpha / 2)) * sampleStdev.pow(2)
+    fun tscore(alpha: Double, df: Int): Double {
+        val tdist = TDistribution(df.toDouble())
+        // Absolute because t-values from t-tables are the absolute value of inverse cumulative probabilities
+        return Math.abs(tdist.inverseCumulativeProbability(alpha))
     }
 
-    fun tscoreNormalVarianceRight(alpha: Double, sampleSize: Int, sampleStdev: Double): Double {
-        val tdist = ChiSquaredDistribution((sampleSize - 1).toDouble())
-        return ((sampleSize - 1) / tdist.inverseCumulativeProbability(1 - alpha / 2)) * sampleStdev.pow(2)
+    fun chiSquareScore(alpha: Double, df: Int): Double {
+        val dist = ChiSquaredDistribution(df.toDouble())
+        return dist.inverseCumulativeProbability(alpha)
+    }
+
+    fun ProbabilityDistribution.sampleStats(): Stats {
+        var sample = sampleDouble(N)
+        val sampleMean = sample.average()
+        val sampleStdev = sample.stdev()
+        val sampleVar = sampleStdev * sampleStdev
+        return Stats(sampleMean, sampleStdev, sampleVar)
+    }
+
+    data class Stats(val mean:Double, val stdev: Double, val variance: Double)
+
+    // TODO: Discuss names with Scott
+    fun Stats.normalMeanHalfInterval() = tscore(alpha / 2, N - 1) * stderr(stdev, N)
+    fun Stats.confidenceIntervalNormalMean(): ClosedFloatingPointRange<Double> {
+        val halfInterval = normalMeanHalfInterval()
+        return (mean - halfInterval)..(mean + halfInterval)
+    }
+    fun Stats.normalVarianceLeftEndpoint() = (N - 1) / chiSquareScore(1 - alpha / 2, N - 1) * variance
+    fun Stats.normalVarianceRightEndpoint() = (N - 1) / chiSquareScore(alpha / 2, N - 1) * variance
+    fun Stats.confidenceIntervalNormalVariance() = (normalVarianceLeftEndpoint() .. normalVarianceRightEndpoint())
+
+    @Test
+    fun `test normal distribution mean 1 and stdev point-5`() {
+        val mean = 1.0
+        val stdev = .5
+        val stats = NormalDistribution(mean,stdev).sampleStats()
+        assertTrue(mean in stats.confidenceIntervalNormalMean())
+        assertTrue(stdev*stdev in stats.confidenceIntervalNormalVariance())
     }
 
     @Test
-    fun `test normal`() {
-        val dist = NormalDistribution(1.0, .5)
-        var nums = dist.sampleDouble(N)
-        val sampleMean = nums.average()
-        val sampleStdev = nums.stdev()
-        val tscoreMean = tscoreNormalMean(alpha, N, sampleStdev)
-        // println("" + sampleMean + " in " + (1.0 - tscore) + ".." + (1.0 + tscore))
-        assertTrue(sampleMean in ((1.0 - tscoreMean)..(1.0 + tscoreMean)))
-        val tLeft = tscoreNormalVarianceLeft(alpha, N, sampleStdev)
-        val tRight = tscoreNormalVarianceRight(alpha, N, sampleStdev)
-        println("" + sampleStdev.pow(2) + " in " + tLeft + ".." + tRight)
-        assertTrue(sampleStdev.pow(2) in tRight..tLeft)
-
-        // assertTrue(nums.stdev() in ((0.5 - alpha)..(0.5 + alpha)))
-        // dist.mean = -.5
-        // dist.standardDeviation = .25
-        // nums = dist.sampleDouble(N)
-        // assertTrue(nums.average() in ((-.5 - alpha)..(-.5 + alpha)))
-        // assertTrue(nums.stdev() in ((0.25 - alpha)..(.25 + alpha)))
+    fun `test normal distribution mean -1 and stdev point-25`() {
+        val mean = -1.0
+        val stdev = .25
+        val stats = NormalDistribution(mean,stdev).sampleStats()
+        assertTrue(mean in stats.confidenceIntervalNormalMean())
+        assertTrue(stdev*stdev in stats.confidenceIntervalNormalVariance())
     }
 
     @Test
@@ -102,8 +115,8 @@ class ProbabilityDistributionTest {
         dist.negate = true
         assertTrue(dist.sampleDouble() <= 0)
         assertTrue(dist.sampleInt() <= 0)
-        var nums = dist.sampleDouble(5)
-        assertTrue(nums.minOrNull()!! <= 0)
+        var sample = dist.sampleDouble(5)
+        assertTrue(sample.minOrNull()!! <= 0)
         var intNums = dist.sampleInt(5)
         assertTrue(intNums.minOrNull()!! <= 0)
     }
@@ -161,14 +174,14 @@ class ProbabilityDistributionTest {
     fun `test two valued`() {
         val dist = TwoValued(-3.0, 3.0)
         var nums = dist.sampleDouble(N)
-        assertTrue(nums.all{ it == -3.0 || it == 3.0 })
-        assertTrue{nums.average() in (-.5 .. .5) } // TODO
+        assertTrue(nums.all { it == -3.0 || it == 3.0 })
+        assertTrue { nums.average() in (-.5.. .5) } // TODO
         dist.p = 1.0
         nums = dist.sampleDouble(N)
-        assertTrue(nums.all{ it == 3.0 })
+        assertTrue(nums.all { it == 3.0 })
         dist.p = 0.0
         nums = dist.sampleDouble(N)
-        assertTrue(nums.all{ it == -3.0 })
+        assertTrue(nums.all { it == -3.0 })
     }
 
     @Test

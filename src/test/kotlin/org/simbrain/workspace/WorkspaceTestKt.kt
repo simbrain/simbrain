35f2036c8a5aaaaaee48e57387ee07a19e2fbefd
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2022-06-15 17:29:39 -0700

    Adding new workspace iteration tests that illustrate some of the current issues with iterate(n)

---
+++ src/test/kotlin/org/simbrain/workspace/WorkspaceTestKt.kt
@@ -0,0 +1,49 @@
+package org.simbrain.workspace
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Also see [WorkspaceTest] for java based test
+ */
+class WorkspaceTestKt {
+
+    val workspace = Workspace()
+
+    @Test
+    fun `simple iterate should call a custom action each iteration`() {
+        var counter = 0
+        workspace.addUpdateAction("increment counter"){
+            counter++
+        }
+        repeat(10) {
+            workspace.simpleIterate()
+        }
+        assertEquals(10, counter)
+    }
+
+    @Test
+    fun `iterate(n) should call a custom action each iteration`() {
+        var counter = 0
+        workspace.addUpdateAction("increment counter"){
+            println("Update action: $counter")
+            counter++
+        }
+        workspace.iterate(10)
+        assertEquals(10, counter)
+    }
+
+    @Test
+    fun `iterate(n)'s finishing task should be called after n iterations'`() {
+        var counter = 0
+        workspace.addUpdateAction("increment counter"){
+            println("Update action: $counter")
+            counter++
+        }
+        workspace.iterate(10) {
+            counter++
+        }
+        assertEquals(11, counter)
+    }
+
+}

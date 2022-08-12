b353e40aad1e1dea8e5567c4523c9c4c5fb89560
Ellis Cain <escred@gmail.com>
2022-08-12 10:33:26 -0700

    First draft of distributional semantics

--- src/main/kotlin/org/simbrain/util/TextUtilities.kt
+++ src/main/kotlin/org/simbrain/util/TextUtilities.kt
@@ -1,6 +1,9 @@
 package org.simbrain.util
 
+import smile.math.MathEx.cos
+import smile.nlp.dictionary.EnglishStopWords
 import smile.nlp.tokenizer.SimpleSentenceSplitter
+import smile.math.matrix.Matrix
 
 /**
  * Sentence tokenizer: parse document into sentences and return as a list of sentences.
@@ -8,7 +11,7 @@ import smile.nlp.tokenizer.SimpleSentenceSplitter
  * Forward to Smile's sentence splitter.
  */
 fun tokenizeSentencesFromDoc(docString: String) : List<String> {
-    return  SimpleSentenceSplitter.getInstance().split(docString).toList()
+    return  SimpleSentenceSplitter.getInstance().split(docString.lowercase()).toList()
 }
 
 /**
@@ -22,5 +25,120 @@ fun removePunctuation(str: String) : String {
  * Word tokenizer: parse sentence into words.
  */
 fun tokenizeWordsFromSentence(sentence: String) : List<String> {
-    return removePunctuation(sentence).split(" ")
+    return removePunctuation(sentence.lowercase()).split(" ")
 }
+
+/**
+ * Unique tokens: all unique tokens (contexts)
+ * Converts to lowercase
+ */
+fun uniqueTokensFromArray(words: List<String>) : List<String> {
+    return words.distinctBy { it.lowercase() }
+}
+
+
+/**
+ * Unique tokens: content words only / remove stop words (targets) -- not working at the moment
+ */
+//fun uniqueTargetsFromArray(words: List<String>) : List<String> {
+//    var stopWords = EnglishStopWords.DEFAULT
+//    print("Stopwords:")
+//    println(stopWords)
+//    var uniqueTargets = words.distinctBy { it.lowercase() }
+//    val filteredTargets = listOf<String>()
+//    for (target in uniqueTargets) {
+//        if (!stopWords.contains(target)) filteredTargets + target
+//    }
+//    return filteredTargets
+//}
+
+
+
+/**
+ * Generates co-occ matrix w/ window size n
+ */
+fun generateCooccurrenceMatrix(docString: String, windowSize: Int): Matrix  {
+    // get tokens from whole document
+    var tokenizedSentence = tokenizeWordsFromSentence(docString)
+    var tokens = uniqueTokensFromArray(tokenizedSentence)
+    println("Tokens:")
+    println(tokens)
+
+    // Split document into sentences
+    var sentences = tokenizeSentencesFromDoc(docString)
+
+    // Set up matrix
+    var matrixSize = tokens.size
+    var cooccurrenceSmileMatrix = Matrix(matrixSize, matrixSize)
+
+//    cooccurrenceMatrix[0][1] = 2 // cooccurrenceMatrix[target][context]
+
+    // Loop through sentences, through words
+    for (sentence in sentences) {
+        println(sentence)
+        var tokenizedSentence = tokenizeWordsFromSentence(sentence)
+        for (sentenceIndex in tokenizedSentence.indices) {
+            var maxIndex = tokenizedSentence.size -1  // used for window range check
+
+            var currentToken = tokenizedSentence[sentenceIndex] // Current iterated token
+
+            var contextLowerLimit = sentenceIndex - windowSize
+            var contextUpperLimit = sentenceIndex + windowSize
+
+            for (contextIndex in contextLowerLimit..contextUpperLimit){
+                if (contextIndex in 0..maxIndex && contextIndex != sentenceIndex){
+                    var currentContext = tokenizedSentence[contextIndex]
+
+
+                    var tokenCoordinate = tokens.indexOf(currentToken)
+                    var contextCoordinate = tokens.indexOf(currentContext)
+//                    print(listOf("Current Token:", currentToken, tokenCoordinate))
+//                    println(listOf("Current Context",currentContext, contextCoordinate))
+                    cooccurrenceSmileMatrix[tokenCoordinate, contextCoordinate] = cooccurrenceSmileMatrix[tokenCoordinate, contextCoordinate] + 1
+                }
+            }
+        }
+    }
+    print(cooccurrenceSmileMatrix)
+    return cooccurrenceSmileMatrix
+}
+
+/**
+ * Get an embedding from a matrix given matrix, index, and word
+ */
+//fun generateCooccurrenceMatrix(docString: String, windowSize: Int): Matrix  {
+fun wordEmbeddingQuery(targetWord: String, tokens: List<String>, cooccurrenceMatrix: Matrix): DoubleArray {
+    var targetWordIndex = tokens.indexOf(targetWord)
+    return cooccurrenceMatrix.col(targetWordIndex)
+
+}
+
+
+/**
+ * PPMI weighting
+ * Adjusted from:  https://stackoverflow.com/questions/58701337/how-to-construct-ppmi-matrix-from-a-text-corpus
+ * https://haifengl.github.io/api/java/smile/math/matrix/Matrix.html#colSums--
+ */
+//fun manualPPMI(cooccurrenceMatrix: Matrix, positive: Boolean): Matrix {
+//    // Get vector of column totals
+//    var columnTotals = colSums(cooccurrenceMatrix)
+//    // Get total sum of cooccurrences
+//    var totalSum = sum(columnTotals)
+//    // Get vector of row totals
+//    var rowTotals = rowSums(cooccurrenceMatrix)
+//    // "expected values" as the outer product of (row totals, col totals) / total
+//
+//    // Divide cooccurrence matrix by the expected values
+//
+//    // If positive, then fill in negatives with zero
+//}
+
+/**
+ * Calculate cosine similarity of two vectors (higher values are more similar)
+ */
+fun embeddingCosineSimilarity(vectorA: DoubleArray, vectorB: DoubleArray): Double {
+    return cos(vectorA, vectorB)
+
+}
+
+
--- src/test/kotlin/org/simbrain/util/TextUtilsTest.kt
+++ src/test/kotlin/org/simbrain/util/TextUtilsTest.kt
@@ -5,16 +5,18 @@ import org.junit.jupiter.api.Test
 
 class TextUtilsTest {
 
-    val simpleText = "This is a simple sentence. Ellis will do better."
+    val simpleText = "This is a simple sentence. This is not hard."
 
-    val harderText = "In spite of these three obstacles, the fragmentary Don Quixote of Menard is more subtle than that of Cervantes. The latter indulges in a rather coarse opposition between tales of knighthood and the meager, provincial relaity of his country; Menard chooses as 'reality' the land of Carmen during the century of Lepanto and Lope. What Hispanophile would not have advised Maurice Barres or Dr. Rodrigues Larreta to make such a choice! Menard, as if it were the most natural thing in the world, eludes them."
+    val similarText = "The cat can run. The dog can run. The cat eats food. The dog eats food. Please bring lunch to the table."
+
+    val harderText = "In spite of these three obstacles, the fragmentary Don Quixote of Menard is more subtle than that of Cervantes. The latter indulges in a rather coarse opposition between tales of knighthood and the meager, provincial reality of his country; Menard chooses as 'reality' the land of Carmen during the century of Lepanto and Lope. What Hispanophile would not have advised Maurice Barres or Dr. Rodrigues Larreta to make such a choice! Menard, as if it were the most natural thing in the world, eludes them."
 
     @Test
     fun `test sentence parsing`() {
-        val sentences =  tokenizeWordsFromSentence(simpleText)
+        val sentences =  tokenizeSentencesFromDoc(simpleText)
         // println(sentences.contentToString())
         assertEquals(2, sentences.size)
-        assertEquals(4, tokenizeWordsFromSentence(harderText).size)
+        assertEquals(4, tokenizeSentencesFromDoc(harderText).size)
     }
 
     @Test
@@ -29,5 +31,69 @@ class TextUtilsTest {
     fun `words parsed correctly from sentence`() {
         assertEquals(3, tokenizeWordsFromSentence("This, is text!").size)
     }
+
+    @Test
+    fun `test lowercasing`() {
+        var firstCapital = "Abc"
+        var middleCapital = "aBc"
+        assertEquals("abc", firstCapital.lowercase())
+        assertEquals("abc",middleCapital.lowercase())
+    }
+
+    @Test
+    fun `get unique tokens from sentences`() {
+//        var sentence = "A a b. B c b d c c" // test for capitalization
+        var sentence = "a A a b. B c b d c c"
+        var tokenizedSentence = tokenizeWordsFromSentence(sentence)
+        var uniqueTokens = uniqueTokensFromArray(tokenizedSentence)
+        println(uniqueTokens)
+        assertEquals(listOf("a","b","c", "d"), uniqueTokens)
+    }
+
+//    @Test
+//    fun `get unique targets, without stop words, from sentence`() {
+//        var testSetOfTokens = listOf("the","of","dog")
+//        var uniqueTokens = uniqueTargetsFromArray(testSetOfTokens)
+//        assertEquals(listOf("dog"), uniqueTokens)
+//        println(uniqueTokens)
+//    }
+
+    @Test
+    fun `creates co-occurrence matrix`() {
+        var tokenizedSentence = tokenizeWordsFromSentence(simpleText)
+        var tokens = uniqueTokensFromArray(tokenizedSentence)
+        var cooccurrenceMatrix = generateCooccurrenceMatrix(simpleText, 2)
+
+        var matrixSize = tokens.size
+        assertEquals(matrixSize, cooccurrenceMatrix.nrows())
+    }
+
+    @Test
+    fun `retrieve embedding from co-occurrence matrix`() {
+        var tokenizedSentence = tokenizeWordsFromSentence(simpleText)
+        var tokens = uniqueTokensFromArray(tokenizedSentence)
+        var cooccurrenceMatrix = generateCooccurrenceMatrix(simpleText, 2)
+        var targetWordA = "simple"
+        wordEmbeddingQuery(targetWordA,tokens,cooccurrenceMatrix)
+        var targetWordB = "this"
+        wordEmbeddingQuery(targetWordB,tokens,cooccurrenceMatrix)
+    }
+
+    @Test
+    fun `computes cosine similarity between two vectors`() {
+        var tokenizedSentence = tokenizeWordsFromSentence(similarText)
+        var tokens = uniqueTokensFromArray(tokenizedSentence)
+        var cooccurrenceMatrix = generateCooccurrenceMatrix(similarText, 2)
+        var targetWordA = "cat"
+        var vectorA = wordEmbeddingQuery(targetWordA,tokens,cooccurrenceMatrix)
+        var targetWordB = "dog"
+        var vectorB = wordEmbeddingQuery(targetWordB,tokens,cooccurrenceMatrix)
+        var targetWordC = "table"
+        var vectorC = wordEmbeddingQuery(targetWordC,tokens,cooccurrenceMatrix)
+
+        println(embeddingCosineSimilarity(vectorA, vectorB))
+        println(embeddingCosineSimilarity(vectorA, vectorC))
+    }
+
 }
 

28f999f5bd7c67aeb446e01eccd61a7750f15ea3
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2022-12-23 09:43:54 -0800

    Adding an initial cosine similarity panel

---
+++ src/main/kotlin/org/simbrain/world/textworld/gui/TextWorldDialogs.kt
@@ -0,0 +1,44 @@
+package org.simbrain.world.textworld.gui
+
+import net.miginfocom.swing.MigLayout
+import org.simbrain.util.StandardDialog
+import org.simbrain.util.Utils.createAction
+import org.simbrain.util.display
+import org.simbrain.util.embeddingSimilarity
+import org.simbrain.world.textworld.TextWorld
+import javax.swing.JComboBox
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+fun TextWorld.showComparisonDialog(): StandardDialog {
+    return StandardDialog().apply {
+
+        title = "Compare Word Vectors"
+        contentPane = JPanel()
+        layout = MigLayout("Debug")
+
+        val word1cb = JComboBox(tokenVectorMap.tokensMap.keys.toTypedArray())
+        val word2cb = JComboBox(tokenVectorMap.tokensMap.keys.toTypedArray())
+        val similarity = JLabel("Similarity: ")
+
+        val compareWords =  createAction("blah", "blah", "menu_icons/Gauge.png") {
+            val vec1 = tokenVectorMap.get(word1cb.selectedItem as String)
+            val vec2 = tokenVectorMap.get(word2cb.selectedItem as String)
+            println("Vector 1: ${vec1.contentToString()}")
+            println("Vector 2: ${vec2.contentToString()}")
+            similarity.text = "Similarity: ${embeddingSimilarity(vec1, vec2)}"
+        }
+        word1cb.action = compareWords
+        word2cb.action = compareWords
+
+        contentPane.add(word1cb)
+        contentPane.add(JLabel("and"))
+        contentPane.add(word2cb, "wrap")
+        contentPane.add(similarity)
+    }
+}
+
+fun main() {
+    val world = TextWorld()
+    world.showComparisonDialog().display()
+}

3a3916eac6da92d4fdafba243e395ed09c3a2537
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2023-03-03 19:01:02 -0800

    Multi scale pattern completion sim now has a reservoir and updates properly

--- src/main/kotlin/org/simbrain/custom_sims/simulations/multiScalePatternCompletion.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/multiScalePatternCompletion.kt
@@ -1,6 +1,18 @@
 package org.simbrain.custom_sims.simulations
 
+import org.simbrain.custom_sims.addNetworkComponent
 import org.simbrain.custom_sims.newSim
+import org.simbrain.custom_sims.updateAction
+import org.simbrain.network.connections.Sparse
+import org.simbrain.network.core.SynapseGroup2
+import org.simbrain.network.core.addNeuronCollection
+import org.simbrain.network.groups.NeuronCollection
+import org.simbrain.network.layouts.GridLayout
+import org.simbrain.network.layouts.LineLayout
+import org.simbrain.network.neuron_update_rules.LinearRule
+import org.simbrain.util.place
+import org.simbrain.util.point
+import org.simbrain.util.stats.distributions.NormalDistribution
 import java.util.*
 
 /**
@@ -8,54 +20,122 @@ import java.util.*
  */
 val multiScalePatternCompletion = newSim {
 
-    class State(val name: String) {
-        override fun toString(): String {
-            return name
-        }
-    }
-    class Transition(val from: State, val to: State, val probability: Double)
+    // TODOS:
+    // - Encapsulate state machine into a class
+    // - Put inputs in text world with couplings
 
-    fun List<Transition>.sampleFirst(): State {
+    class Transition(val from: String, val to: String, val probability: Double)
+
+    fun List<Transition>.sampleFirst(): String {
         val randomIndex = Random().nextInt(size)
         return this[randomIndex].from
     }
 
-    fun List<Transition>.sampleNext(firstState: State): State {
+    fun List<Transition>.sampleNext(firstState: String): String {
         var cumulativeProb = 0.0
         val randNum = Random().nextDouble()
         filter { it.from == firstState}.forEach {
             cumulativeProb += it.probability
             if (randNum < cumulativeProb ) return it.to
         }
-        return State("ERROR")
+        throw IllegalStateException("ERROR: Check that transition probabilities from source state add to 1")
     }
-
-    // Nouns
-    val man = State("man")
-    val dog = State("dog")
-
-    // Verbs
-    val walks = State("walks")
-    val bites = State("bites")
 
     val nounVerbTransitions = listOf(
-        Transition(man, walks, 0.75),
-        Transition(man, bites, 0.25),
-        Transition(dog, walks, 0.25),
-        Transition(dog, bites, 0.75))
+        Transition("man", "walks", 0.75),
+        Transition("man", "bites", 0.25),
+        Transition("dog", "walks", 0.25),
+        Transition("dog", "bites", 0.75))
 
     val verbNounTransitions = listOf(
-        Transition(walks, dog, 0.75),
-        Transition(walks, man, 0.25),
-        Transition(bites, dog, 0.25),
-        Transition(bites, man, 0.75))
+        Transition("walks", "dog", 0.75),
+        Transition("walks", "man", 0.25),
+        Transition("bites", "dog", 0.25),
+        Transition("bites", "man", 0.75))
 
+    // Create the input sequence
+    val inputSequence = mutableListOf<String>()
     repeat(10) {
         val firstWord = nounVerbTransitions.sampleFirst()
+        inputSequence.add(firstWord)
         val secondWord = nounVerbTransitions.sampleNext(firstWord)
+        inputSequence.add(secondWord)
         val thirdWord = verbNounTransitions.sampleNext(secondWord)
-        println("$firstWord $secondWord $thirdWord")
+        inputSequence.add(thirdWord)
+        // println("$firstWord $secondWord $thirdWord")
     }
 
+    // NETWORK
+
+    // Number of reservoir neurons
+    val numResNeurons = 100
+
+    // Basic setup
+    workspace.clearWorkspace()
+    val networkComponent = addNetworkComponent("Multi Scale Pattern Completion")
+    val network = networkComponent.network
+
+    // Reservoir
+    val reservoir = List(numResNeurons) {
+        AllostaticNeuron(network, AllostaticUpdateRule())
+    }.let {
+        network.addNetworkModels(it)
+        NeuronCollection(network, it)
+    }.apply {
+        label = "Reservoir"
+        layout(GridLayout())
+        location = point(0, 0)
+    }
+    network.addNetworkModel(reservoir)
+    val sparse = Sparse()
+    sparse.connectionDensity = .1
+    val reservoirSynapseGroup = SynapseGroup2(reservoir, reservoir, sparse)
+    val dist = NormalDistribution(1.0, .1)
+    reservoirSynapseGroup.synapses.forEach { s ->
+        s.strength = dist.sampleDouble()
+    }
+    network.addNetworkModel(reservoirSynapseGroup)?.join()
+
+    // Input nodes
+    val inputs = network.addNeuronCollection(5) {
+        updateRule = LinearRule()
+        network.addNetworkModel(this)
+    }.apply {
+        label = "Inputs"
+        setClamped(true)
+        layout(LineLayout())
+        location = point(-550, 0)
+    }
+
+    // Connect input nodes to reservoir
+    val inputsToRes = SynapseGroup2(inputs, reservoir, sparse)
+    inputsToRes.label = "Inputs to Res"
+    network.addNetworkModel(inputsToRes)
+    inputsToRes.synapses.forEach { s ->
+        s.strength = 0.75
+    }
+
+    // Location of the network in the desktop
+    withGui {
+        place(networkComponent) {
+            location = point(0, 0)
+            width = 800
+            height = 600
+        }
+    }
+
+    val inputEncodings = mapOf(
+        "man"   to doubleArrayOf(1.0, 0.0, 0.0, 0.0),
+        "dog"   to doubleArrayOf(0.0, 1.0, 0.0, 0.0),
+        "walks" to doubleArrayOf(0.0, 0.0, 1.0, 0.0),
+        "bites" to doubleArrayOf(0.0, 0.0, 0.0, 1.0)
+    )
+
+    var wordIndex = 0
+    network.updateManager.addAction(0, updateAction("Set inputs") {
+        val word = inputSequence[wordIndex++ % inputSequence.size]
+        println(word)
+        inputs.forceSetActivations(inputEncodings[word])
+    })
 }
 

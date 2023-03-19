71b0125cfbb5e384c4d424761e4f129b7167950b
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2023-03-19 10:57:39 -0700

    Adding projection plot to multiScalePatternCompletion.kt

--- src/main/kotlin/org/simbrain/custom_sims/simulations/multiScalePatternCompletion.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/multiScalePatternCompletion.kt
@@ -1,9 +1,6 @@
 package org.simbrain.custom_sims.simulations
 
-import org.simbrain.custom_sims.addNetworkComponent
-import org.simbrain.custom_sims.addTextWorld
-import org.simbrain.custom_sims.newSim
-import org.simbrain.custom_sims.updateAction
+import org.simbrain.custom_sims.*
 import org.simbrain.network.connections.Sparse
 import org.simbrain.network.core.SynapseGroup2
 import org.simbrain.network.core.addNeuronCollection
@@ -16,6 +13,7 @@ import org.simbrain.util.place
 import org.simbrain.util.point
 import org.simbrain.util.stats.distributions.NormalDistribution
 import org.simbrain.workspace.updater.UpdateComponent
+import org.simbrain.workspace.updater.UpdateCoupling
 import java.util.*
 
 /**
@@ -39,9 +37,9 @@ val multiScalePatternCompletion = newSim {
     fun List<Transition>.sampleNext(firstState: String): String {
         var cumulativeProb = 0.0
         val randNum = Random().nextDouble()
-        filter { it.from == firstState}.forEach {
+        filter { it.from == firstState }.forEach {
             cumulativeProb += it.probability
-            if (randNum < cumulativeProb ) return it.to
+            if (randNum < cumulativeProb) return it.to
         }
         throw IllegalStateException("ERROR: Check that transition probabilities from source state add to 1")
     }
@@ -50,13 +48,15 @@ val multiScalePatternCompletion = newSim {
         Transition("man", "walks", 0.75),
         Transition("man", "bites", 0.25),
         Transition("dog", "walks", 0.25),
-        Transition("dog", "bites", 0.75))
+        Transition("dog", "bites", 0.75)
+    )
 
     val verbNounTransitions = listOf(
         Transition("walks", "dog", 0.75),
         Transition("walks", "man", 0.25),
         Transition("bites", "dog", 0.25),
-        Transition("bites", "man", 0.75))
+        Transition("bites", "man", 0.75)
+    )
 
     // Create the input sequence
     val inputSequence = mutableListOf<String>()
@@ -108,7 +108,7 @@ val multiScalePatternCompletion = newSim {
         network.addNetworkModel(this)
     }.apply {
         label = "Inputs"
-        setLabels(listOf("man", "dog", "walks", "bites", "END" ))
+        setLabels(listOf("man", "dog", "walks", "bites", "END"))
         setClamped(true)
         layout(LineLayout())
         location = point(-550, 0)
@@ -143,24 +143,50 @@ val multiScalePatternCompletion = newSim {
         }
     }
 
+    // PCA
+    val pca = addProjectionPlot("Activations")
+    pca.projector.tolerance = .2
+    withGui {
+        place(pca) {
+            location = point(143, 200)
+            width = 500
+            height = 500
+        }
+    }
+
+    val textCoupling = with(couplingManager) {
+        val currentWord = textWorld.world.getProducer("getCurrentToken")
+        val pcaLabel = pca.getConsumer("setLabel")
+        couplingManager.createCoupling(currentWord, pcaLabel)
+    }
+    val networkPCACoupling = couplingManager.createCoupling(reservoir, pca)
+
+
+
     // WORKSPACE UPDATE RULES
 
     val inputEncodings = mapOf(
-        "man"   to doubleArrayOf(1.0, 0.0, 0.0, 0.0, 0.0),
-        "dog"   to doubleArrayOf(0.0, 1.0, 0.0, 0.0, 0.0),
+        "man" to doubleArrayOf(1.0, 0.0, 0.0, 0.0, 0.0),
+        "dog" to doubleArrayOf(0.0, 1.0, 0.0, 0.0, 0.0),
         "walks" to doubleArrayOf(0.0, 0.0, 1.0, 0.0, 0.0),
         "bites" to doubleArrayOf(0.0, 0.0, 0.0, 1.0, 0.0),
-        "END"   to doubleArrayOf(0.0, 0.0, 0.0, 0.0, 1.0)
+        "END" to doubleArrayOf(0.0, 0.0, 0.0, 0.0, 1.0)
     )
     val zeroInput = doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0)
 
     network.updateManager.addAction(0, updateAction("Set inputs") {
         val word = textWorld.world.currentItem?.text
-        inputs.forceSetActivations(inputEncodings[word]?:zeroInput )
+        inputs.forceSetActivations(inputEncodings[word] ?: zeroInput)
+        // val spikes = reservoir.neuronList.map{
+        //     if ((it as AllostaticNeuron).isSpike) 1.0 else 0.0
+        // }.toDoubleArray()
     })
 
     workspace.updater.updateManager.clear()
     workspace.updater.updateManager.addAction(UpdateComponent(textWorld))
     workspace.updater.updateManager.addAction(UpdateComponent(networkComponent))
+    workspace.updater.updateManager.addAction(UpdateCoupling(networkPCACoupling))
+    workspace.updater.updateManager.addAction(UpdateCoupling(textCoupling))
+    workspace.updater.updateManager.addAction(UpdateComponent(pca))
 }
 

d373852c2c5ad6be8a3a7878d4c093d0d940238c
LeoYulinLi <leoyulinli@gmail.com>
2023-04-25 18:39:11 -0700

    updated version number

--- etc/make_app.ps1
+++ etc/make_app.ps1
@@ -1,7 +1,7 @@
 # 1. Ant build
 # 2. Run the command at the bottom in power shell
 # 3. (skip if have done previously) install the .p12 certificate by double clicking the .p12 file and entering the password
-# 4. sign with signtool sign /a /fd SHA256 ..\dist\Simbrain-3.05.exe
+# 4. sign with signtool sign /a /fd SHA256 ..\dist\Simbrain-3.06.exe
 jpackage --input ..\build\main\ `
     --main-jar Simbrain.jar `
     --dest ..\dist `

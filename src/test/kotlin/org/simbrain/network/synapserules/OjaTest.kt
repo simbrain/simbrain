4afe362cd8071fd8bb55642e52d6a149a4e17d99
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2023-07-17 17:30:25 -0700

    Adding tests for Oja

--- src/test/kotlin/org/simbrain/network/synapserules/OjaTest.kt
+++ src/test/kotlin/org/simbrain/network/synapserules/OjaTest.kt
@@ -1,5 +1,6 @@
 package org.simbrain.network.synapserules
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.simbrain.network.core.Network
 import org.simbrain.network.core.Neuron
@@ -8,42 +9,103 @@ import org.simbrain.network.synapse_update_rules.OjaRule
 
 class OjaTest {
 
+    // 2->1 network
     var net = Network()
-    val n1 = Neuron(net)
-    val n2 = Neuron(net)
-    val out = Neuron(net)
-    var s1 = Synapse(n1,out)
-    var s2 = Synapse(n2,out)
+    val input = Neuron(net)
+    val output = Neuron(net)
+    var weight = Synapse(input,output)
 
     init {
-        net.addNetworkModelsAsync(n1, n2, out, s1, s2)
-        s1.learningRule = OjaRule().apply {
-            learningRate = 0.2
+        net.addNetworkModelsAsync(input, output, weight)
+        weight.learningRule = OjaRule().apply {
+            learningRate = 1.0
             normalizationFactor = 1.0
         }
-        s1.strength = 0.0
-        s2.learningRule = OjaRule().apply {
-            learningRate = 0.2
-            normalizationFactor = 1.0
-        }
-        s2.strength = 0.0
-
-        n1.isClamped = true
-        n2.isClamped = true
-        out.isClamped = true
+        weight.strength = 0.0
+        weight.upperBound = 10.0
+        weight.lowerBound = -10.0
+        input.isClamped = true
+        output.isClamped = true
     }
 
     @Test
-    fun `test basic update`() {
-        n1.forceSetActivation(1.0)
-        n2.forceSetActivation(1.0)
-        out.forceSetActivation(1.0)
-        repeat(1000) {net.update()}
-//        assertEquals(1.0,s12.strength )
-//        net.update()
-//        assertEquals(2.0,s12.strength )
-        println("Strength is ${s1.strength}")
-        println("Strength is ${s2.strength}")
-        println("Sum of squares is ${s1.strength * s1.strength + s2.strength * s2.strength}")
+    fun `test update for a single weight and clamped nodes`() {
+        input.forceSetActivation(1.0)
+        output.forceSetActivation(1.0)
+        net.update()
+
+        // delta-w  = rate (out(in - out * weight))
+        //          = 1 (1(1 - 1*0)) = 1
+        assertEquals(1.0, weight.strength)
+        net.update()
+        //          1 (1(1 - 1*1)) = 0
+        assertEquals(1.0, weight.strength)
+        repeat(10) {
+            net.update()
+        }
+        assertEquals(1.0, weight.strength)
     }
+
+    @Test
+    fun `test update with different source and target`() {
+        // High learning rate leads to divergence in this case
+        input.forceSetActivation(1.0)
+        output.forceSetActivation(2.0)
+        // delta-w  = rate (out(in - out * weight))
+        //          = 1 (2(1 - 2*0)) = 2
+        // Weight becomes 0 + 2 = 2
+        net.update()
+        assertEquals(2.0, weight.strength)
+        //          = 1 (2(1 - 2*2)) = -6
+        // Weight becomes 2 -6 = -4
+        net.update()
+        assertEquals(-4.0, weight.strength)
+        //          = 1 (2(1 - 2*-4)) = 18
+        // Weight becomes -6 + 18 = 12, which is clipped at 10
+        net.update()
+        assertEquals(10.0, weight.strength)
+    }
+
+    @Test
+    fun `test update with learning rate less than 1`() {
+        input.forceSetActivation(1.0)
+        output.forceSetActivation(1.0)
+        (weight.learningRule as OjaRule).learningRate = .1
+
+        // Should approach 1.
+        repeat(100) {
+            net.update()
+        }
+        assertEquals(1.0, weight.strength, .01)
+
+        // Should approach -1
+        output.forceSetActivation(-1.0)
+        repeat(100) {
+            net.update()
+        }
+        assertEquals(-1.0, weight.strength, .01)
+
+        // Should approach 1/2
+        output.forceSetActivation(2.0)
+        repeat(100) {
+            net.update()
+        }
+        assertEquals(.5, weight.strength, .01)
+
+        // Should approach 1/3
+        output.forceSetActivation(3.0)
+        repeat(100) {
+            net.update()
+        }
+        assertEquals(.33, weight.strength, .01)
+
+        // Should approach -1/3
+        output.forceSetActivation(-3.0)
+        repeat(100) {
+            net.update()
+        }
+        assertEquals(-.33, weight.strength, .01)
+
+    }
+
 }

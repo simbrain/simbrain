27756871e03d5eed3c2d30fbc524fa874f36a8b8
auxtin <austin.leonardo32@gmail.com>
2023-08-03 11:08:20 -0700

    - added codesigning as part of jpackage - removed `codesign` command - added notarization submission - added stapler command

--- etc/package_and_sign.sh
+++ etc/package_and_sign.sh
@@ -1,6 +1,15 @@
 VERSION="Simbrain-3.07"
 DIST_DIR="$(cd "$(dirname "$0")/.." && pwd)/dist/"
 
-codesign -fs "$CERT_NAME" "$DIST_DIR"/Simbrain.app
 hdiutil create -volname $VERSION -srcfolder "$DIST_DIR"/Simbrain.app -ov -format UDZO "$DIST_DIR"/"$VERSION".dmg
 rm -rf "$DIST_DIR"/Simbrain.app
+# submit + wait .dmg for notarization and acceptance
+output=$(xcrun notarytool submit "$VERSION".dmg -p "simbrain" --wait)
+echo $output
+# if accepted, staple ticket to dmg file for distribution
+if echo "$output" | grep -q "status: Accepted"; then
+  echo "Application has been accepted for notarization. Stapling ticket to .dmg and application is ready fo distribution."
+  xcrun stapler stable "$VERSION".dmg
+else
+  echo "Application has not been accepted for notarization, please check the 'id' for reason"
+fi

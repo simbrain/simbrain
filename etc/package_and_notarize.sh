e4bf3dc3f667d2df770f09c3e809f6a13f2b036d
auxtin <austin.leonardo32@gmail.com>
2023-08-03 11:46:50 -0700

    Renamed_package_andsign -> package_and_notaize - fixed scripts for stapling

--- etc/package_and_notarize.sh
+++ etc/package_and_notarize.sh
@@ -3,13 +3,13 @@ DIST_DIR="$(cd "$(dirname "$0")/.." && pwd)/dist/"
 
 hdiutil create -volname $VERSION -srcfolder "$DIST_DIR"/Simbrain.app -ov -format UDZO "$DIST_DIR"/"$VERSION".dmg
 rm -rf "$DIST_DIR"/Simbrain.app
-# submit + wait .dmg for notarization and acceptance
-output=$(xcrun notarytool submit "$VERSION".dmg -p "simbrain" --wait)
-echo $output
+
+# submit + wait .dmg for notarization and acceptance.
+output=$(xcrun notarytool submit "$DIST_DIR"/"$VERSION".dmg -p "simbrain" --wait | tee /dev/tty)
 # if accepted, staple ticket to dmg file for distribution
 if echo "$output" | grep -q "status: Accepted"; then
   echo "Application has been accepted for notarization. Stapling ticket to .dmg and application is ready fo distribution."
-  xcrun stapler stable "$VERSION".dmg
+  xcrun stapler staple "$DIST_DIR"/"$VERSION".dmg
 else
   echo "Application has not been accepted for notarization, please check the 'id' for reason"
 fi

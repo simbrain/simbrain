b7427d68f5daa1214a5876316c85182c257b5f51
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2023-09-22 17:55:02 -0700

    Adding a failing unit test to odor world test (sensors and effectors not currently being serialized)

--- src/test/kotlin/org/simbrain/world/odorworld/OdorWorldTest.kt
+++ src/test/kotlin/org/simbrain/world/odorworld/OdorWorldTest.kt
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.simbrain.util.point
+import org.simbrain.world.odorworld.effectors.StraightMovement
 import org.simbrain.world.odorworld.entities.EntityType
 import org.simbrain.world.odorworld.entities.OdorWorldEntity
+import org.simbrain.world.odorworld.sensors.ObjectSensor
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 import java.nio.charset.StandardCharsets
@@ -30,23 +32,29 @@ class OdorWorldTest {
         swiss.location = point(5,6)
         oc.world.addEntity(swiss)
         val mouse = OdorWorldEntity(oc.world, EntityType.MOUSE)
+        mouse.addSensor(ObjectSensor(EntityType.SWISS, 50.0, 45.0))
+        mouse.addEffector(StraightMovement())
+
         mouse.location = point(10,11)
         oc.world.addEntity(mouse)
 
         // Create xstream for world
         val xstream = oc.xml
-        println(xstream)
+        // println(xstream)
         val stream: InputStream = ByteArrayInputStream(xstream.toByteArray(StandardCharsets.UTF_8))
 
         // Unmarshall from xstream
         val oc2 = OdorWorldComponent.open(stream, "test2", "xml")
 
-        val swiss2 = oc2.world.entityList.firstOrNull { it.entityType == EntityType.SWISS }
-        val mouse2 = oc2.world.entityList.firstOrNull { it.entityType == EntityType.MOUSE }
-        assertNotNull(mouse2)
-        assertNotNull(swiss2)
-        assertEquals(mouse.location, mouse2?.location)
-        assertEquals(swiss.location, swiss2?.location)
+        val newSwiss = oc2.world.entityList.firstOrNull { it.entityType == EntityType.SWISS }
+        val newMouse = oc2.world.entityList.firstOrNull { it.entityType == EntityType.MOUSE }
+        assertNotNull(newMouse)
+        assertNotNull(newSwiss)
+        assertEquals(mouse.location, newMouse?.location)
+        assertEquals(swiss.location, newSwiss?.location)
+        assertEquals(1, newMouse?.sensors?.size)
+        assertEquals(1, newMouse?.sensors?.size)
+
     }
 
 }

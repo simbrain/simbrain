995b347497048f92c2ec273904b7707458692166
Kanly Thao <kanlythao14@gmail.com>
2024-02-14 18:05:56 -0800

    Changed CompetitiveSim patterns to "halo" patterns

--- src/main/kotlin/org/simbrain/custom_sims/simulations/demos/competitiveSim.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/demos/competitiveSim.kt
@@ -31,35 +31,28 @@ val competitiveSim = newSim {
 
             addButton("Pattern 1") {
                 competitive.inputLayer.neuronList.activations =
-                    listOf(1.0, 0.5, 0.0, 0.0, 1.0, 0.5, 0.5)
+                    listOf(0.5, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0)
             }
-
             addButton("Pattern 2") {
                 competitive.inputLayer.neuronList.activations =
-                    listOf(0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5)
+                    listOf(0.0, 0.5, 1.0, 0.5, 0.0, 0.0, 0.0)
             }
-
             addButton("Pattern 3") {
                 competitive.inputLayer.neuronList.activations =
-                    listOf(1.0, 0.0, 0.5, 1.0, 1.0, 0.0, 0.0)
+                    listOf(0.0, 0.0, 0.5, 1.0, 0.5, 0.0, 0.0)
             }
-
             addButton("Pattern 4") {
                 competitive.inputLayer.neuronList.activations =
-                    listOf(1.0, 1.0, 0.5, 0.0, 0.0, 0.0, 0.5)
+                    listOf(0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 0.0)
             }
-
             addButton("Pattern 5") {
                 competitive.inputLayer.neuronList.activations =
-                    listOf(0.5, 1.0, 0.0, 0.5, 0.0, 0.5, 0.0)
+                    listOf(0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5)
             }
-
             addButton("Train") {
                 competitive.inputLayer.activations = competitive.inputLayer.activations.add(NormalDistribution().sampleDouble(competitive.inputLayer.activations.size))
                 workspace.iterate()
             }
-
         }
     }
-
 }

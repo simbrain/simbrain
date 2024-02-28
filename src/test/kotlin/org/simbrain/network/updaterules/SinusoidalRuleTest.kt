df0be07eff86f862a0662862a9bb213a2a9d7600
Kanly Thao <kanlythao14@gmail.com>
2024-02-28 11:03:25 -0800

    First Iteration of the SinusoidalRuleTest

---
+++ src/test/kotlin/org/simbrain/network/updaterules/SinusoidalRuleTest.kt
@@ -0,0 +1,112 @@
+package org.simbrain.network.updaterules
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.simbrain.network.core.Network
+import org.simbrain.network.core.Neuron
+import org.simbrain.network.updaterules.activity_generators.SinusoidalRule
+
+class SinusoidalRuleTest {
+
+    // Default update rule is sinusoidal
+    val net = Network()
+    var rule1 = SinusoidalRule()
+    var rule2 = SinusoidalRule()
+    val n1 = Neuron(rule1)
+    val n2 = Neuron(rule2)
+
+    init {
+        net.addNetworkModelsAsync(n1, n2)
+        n1.activation = 0.0
+        rule1.upperBound = 1.0
+        rule1.lowerBound = -1.0
+        rule1.phase = 1.0
+        rule1.frequency = 1.0
+
+        n2.activation = 0.0
+        rule2.upperBound = 1.0
+        rule2.lowerBound = -1.0
+        rule2.phase = 1.0
+        rule2.frequency = 1.0
+    }
+
+    @Test
+    fun `Activation Stays Within the Bounds`() {
+        repeat(10) {
+            net.update()
+        }
+        assertTrue(n1.activation < 1.0)
+        assertTrue(n1.activation > -1.0)
+
+        net.resetTime()
+        rule1.upperBound = 2.0
+        repeat(10) {
+            net.update()
+        }
+        assertTrue(n1.activation < 2.0)
+    }
+
+    @Test
+    fun `Activation of Equal Phases are Equal`() {
+        repeat(10) {
+            net.update()
+        }
+        assertEquals(n1.activation, n2.activation)
+    }
+
+    @Test
+    fun `Testing Time Function to get Activation Approximately 1`() {
+        rule1.phase = 0.0
+        rule1.frequency = 1.0
+        net.timeType = Network.TimeType.CONTINUOUS
+        net.timeStep = 1.57
+        net.updateTime()
+        net.update()
+        assertEquals(1.0, n1.activation, .01) // Should get same activation as phase = 1.57, which is true
+    }
+
+    @Test
+    fun `Testing Phase Function to get Activation Approximately 1`(){
+        net.resetTime()
+        rule1.phase = 1.57
+        rule1.frequency = 1.0
+        net.update()
+        assertEquals(1.0, n1.activation, 0.01)
+
+    }
+
+    @Test
+    fun `Phase Function at 1_57 Equals TimeStep Function at 1_57`() {
+        assertEquals(`Testing Time Function to get Activation Approximately 1`(),`Testing Phase Function to get Activation Approximately 1`() )
+    }
+
+    @Test
+    fun `Activation Stays within the Bounds Still if Frequency Increases or Decreases`(){
+        rule1.frequency = 2.0
+        rule1.phase = 0.0
+        repeat(10) {
+            net.update()
+        }
+        assertTrue(n1.activation < 1.0)
+        assertTrue(n1.activation > -1.0)
+
+        rule2.frequency = -2.0
+        rule2.phase = 0.0
+        repeat(10) {
+            net.update()
+        }
+        assertTrue(n2.activation < 1.0)
+        assertTrue(n2.activation > -1.0)
+    }
+
+    @Test
+    fun `Activation Should Remain Constant at Frequency 0`() {
+        rule1.frequency = 0.0
+        rule2.frequency = 0.0
+        repeat(10) {
+            net.update()
+        }
+    assertTrue(n1.activation == n2.activation)
+    }
+}

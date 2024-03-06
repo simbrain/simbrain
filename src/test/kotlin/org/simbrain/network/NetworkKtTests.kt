035df148c564285c26dc20aaa704fbf7e6fac04b
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2024-03-05 19:21:11 -0800

    Adding a few new network tests, one broken (for the issue with clamped nodes and backing fields in serialization).  Also making a new kotlin network test file.

---
+++ src/test/kotlin/org/simbrain/network/NetworkKtTests.kt
@@ -0,0 +1,32 @@
+package org.simbrain.network
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.simbrain.network.core.Network
+import org.simbrain.network.core.Neuron
+import org.simbrain.network.core.getModelById
+import org.simbrain.network.core.getNetworkXStream
+
+class NetworkKtTests {
+
+    val net = Network()
+    val n1 = Neuron().apply {
+        clamped = true
+        forceSetActivation(1.0)
+    }
+    val n2 = Neuron()
+
+    init {
+        net.addNetworkModelsAsync(n1, n2)
+    }
+
+    @Test
+    fun `test deserialization`() {
+        val xmlRep = getNetworkXStream().toXML(net)
+        val fromXml = getNetworkXStream().fromXML(xmlRep) as Network
+        val n1 = fromXml.getModelById<Neuron>("Neuron_1")
+        assertEquals(1.0, n1.activation)
+    }
+
+
+}

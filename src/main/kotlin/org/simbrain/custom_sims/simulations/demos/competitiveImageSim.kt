95bbed4cb5b94eed18f8c58e73764f64586f3ee6
Kanly Thao <kanlythao14@gmail.com>
2024-04-09 18:58:26 -0700

    Initial competitiveImageSim patterns

--- src/main/kotlin/org/simbrain/custom_sims/simulations/demos/competitiveImageSim.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/demos/competitiveImageSim.kt
@@ -1,6 +1,7 @@
 package org.simbrain.custom_sims.simulations
 
 import org.simbrain.custom_sims.*
+import org.simbrain.network.core.activations
 import org.simbrain.network.subnetworks.CompetitiveNetwork
 import org.simbrain.util.add
 import org.simbrain.util.place
@@ -43,34 +44,86 @@ val competitiveImageSim = newSim {
         place(networkComponent, 139, 10, 868, 619)
         createControlPanel("Control Panel", 5, 10) {
 
-            // Maybe we can add in patterns that meant to add more variance (excitation rather than inhibition) in the network's activation.
-            // If so, use code below as reference.
-
-//            addButton("Pattern 1") {
-//                competitive.inputLayer.neuronList.activations =
-//                    listOf(1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0)
-//                winningLabel = "P1"
-//            }
-//            addButton("Pattern 2") {
-//                competitive.inputLayer.neuronList.activations =
-//                    listOf(0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0)
-//                winningLabel = "P2"
-//            }
-//            addButton("Pattern 3") {
-//                competitive.inputLayer.neuronList.activations =
-//                    listOf(0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0)
-//                winningLabel = "P3"
-//            }
-//            addButton("Pattern 4") {
-//                competitive.inputLayer.neuronList.activations =
-//                    listOf(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0)
-//                winningLabel = "P4"
-//            }
-//            addButton("Pattern 5") {
-//                competitive.inputLayer.neuronList.activations =
-//                    listOf(0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0)
-//                winningLabel = "P5"
-//            }
+            addButton("Pattern 1") {
+                competitive.inputLayer.neuronList.activations =
+                    listOf(
+                        1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                        1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                        0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+                        0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
+                    )
+                winningLabel = "P1"
+            }
+            addButton("Pattern 2") {
+                competitive.inputLayer.neuronList.activations =
+                    listOf(
+                        1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+                        0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+                        1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+                        1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0
+                    )
+                winningLabel = "P2"
+            }
+            addButton("Pattern 3") {
+                competitive.inputLayer.neuronList.activations =
+                    listOf(
+                        1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0
+                    )
+                winningLabel = "P3"
+            }
+            addButton("Pattern 4") {
+                competitive.inputLayer.neuronList.activations =
+                    listOf(
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+                    )
+                winningLabel = "P4"
+            }
+            addButton("Pattern 5") {
+                competitive.inputLayer.neuronList.activations =
+                    listOf(
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0,
+                        0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+                        0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0,
+                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+                    )
+                winningLabel = "P5"
+            }
 
                 addButton("Add Noise") {
                     competitive.inputLayer.activations = competitive.inputLayer.activations.add(NormalDistribution(standardDeviation = .01).sampleDouble(competitive.inputLayer.activations.size))

5560cd9da6149f8288baa19c1ab25e2a18c755f4
LeoYulinLi <LeoYulinLi@gmail.com>
2024-06-20 18:59:25 -0700

    minor threading and formatting improvements to binary reservoir

--- src/main/kotlin/org/simbrain/custom_sims/simulations/cogsci/BinaryReservoir.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/cogsci/BinaryReservoir.kt
@@ -1,5 +1,6 @@
 package org.simbrain.custom_sims.simulations
 
+import kotlinx.coroutines.awaitAll
 import org.simbrain.custom_sims.*
 import org.simbrain.network.connections.FixedDegree
 import org.simbrain.network.core.Network
@@ -50,7 +51,7 @@ val binaryReservoir = newSim {
     val responseIterations = 300
 
     // Noise used to generate u-bar
-    val inputNoise = TwoValued(-1.0,1.0, .5)
+    val inputNoise = TwoValued(-1.0, 1.0, .5)
 
     // Distribution used to generate synapses
     var normalDist = NormalDistribution(0.0, 1.0)
@@ -69,18 +70,17 @@ val binaryReservoir = newSim {
     val resNeurons = List(numNeurons) {
         val rule = BinaryRule()
         rule.threshold = .5
-        val neuron = Neuron(rule)
-        neuron
+        Neuron(rule)
     }
-    network.addNetworkModels(resNeurons)
+    network.addNetworkModels(resNeurons).awaitAll()
     val reservoir = NeuronCollection(resNeurons)
-    network.addNetworkModel(reservoir)
+    network.addNetworkModel(reservoir)?.await()
     reservoir.label = "Reservoir"
     reservoir.layout(GridLayout())
     reservoir.location = point(0, 0)
 
     val conn = FixedDegree(degree = k)
-    conn.connectNeurons(resNeurons, resNeurons).addToNetwork(network)
+    conn.connectNeurons(resNeurons, resNeurons).addToNetwork(network).awaitAll()
 
     /**
      * Resets the variance by rescaling synapses. Assumes valid variance to begin with.
@@ -103,14 +103,14 @@ val binaryReservoir = newSim {
         println("Variance: ${variance}")
 
         // Clear the network and activations
-        with(network) { reservoir.randomize() }
+        reservoir.randomize()
         activations.clear()
 
         // Baseline window
         repeat(baseIterations) { network.update() }
 
         // Perturb nodes
-        resNeurons.take(numNodesToPerturb).forEach { n -> n.addInputValue(1.0)  }
+        resNeurons.take(numNodesToPerturb).forEach { n -> n.addInputValue(1.0) }
 
         // Response window
         repeat(responseIterations) { network.update() }
@@ -118,7 +118,7 @@ val binaryReservoir = newSim {
 
     network.addUpdateAction(updateAction("Record activations") {
         val u = inputNoise.sampleDouble()
-        resNeurons.map{n -> n.addInputValue(u)}
+        resNeurons.map { n -> n.addInputValue(u) }
         activations.add(resNeurons.map { n -> n.activation })
         // println("" + activations.last() + ",")
     })
@@ -146,17 +146,17 @@ val binaryReservoir = newSim {
 
 
             addButton("Run one trial per parameter") {
-                    val path = showDirectorySelectionDialog()
-                    if (path != null) {
-                        createGeometricProgression(.1, 1.25)
-                            .takeWhile{it < 10}
-                            .forEach {
-                                setVariance(it)
-                                perturbAndRunNetwork(network)
-                                // println("${variance}: ${activations}")
-                                File(path + FS + "activations${it}.csv").writeText(activations.toCsvString())
+                val path = showDirectorySelectionDialog()
+                if (path != null) {
+                    createGeometricProgression(.1, 1.25)
+                        .takeWhile { it < 10 }
+                        .forEach {
+                            setVariance(it)
+                            perturbAndRunNetwork(network)
+                            // println("${variance}: ${activations}")
+                            File(path + FS + "activations${it}.csv").writeText(activations.toCsvString())
                         }
-                    }
+                }
 
             }
 

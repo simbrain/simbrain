86ff8ad1bd27efc77f54b1ba997352081e47e44f
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2024-07-31 17:18:18 -0700

    Added temperature parameter to softmax group.

--- src/main/kotlin/org/simbrain/network/neurongroups/SoftmaxGroup.kt
+++ src/main/kotlin/org/simbrain/network/neurongroups/SoftmaxGroup.kt
@@ -4,8 +4,10 @@ import org.simbrain.network.core.Network
 import org.simbrain.network.core.Neuron
 import org.simbrain.network.core.XStreamConstructor
 import org.simbrain.network.core.activations
+import org.simbrain.util.UserParameter
 import org.simbrain.util.propertyeditor.CopyableObject
 import org.simbrain.util.propertyeditor.CustomTypeName
+import org.simbrain.util.propertyeditor.GuiEditable
 import kotlin.math.exp
 
 /**
@@ -15,7 +17,17 @@ import kotlin.math.exp
  * This applies the activation rules of the underlying nodes before normalizing so be sure to check that it is
  * appropriate, e.g. that any min and max value is appropriate.
  */
-class SoftmaxGroup(neurons: List<Neuron>): NeuronGroup(), CopyableObject {
+class SoftmaxGroup @JvmOverloads constructor(
+    neurons: List<Neuron>,
+    params: SoftmaxParams = SoftmaxParams()
+) : NeuronGroup() {
+
+    var params by GuiEditable(
+        label = "Softmax Parameters",
+        description = "Parameters for the Softmax Group",
+        initValue = params.apply { creationMode = false },
+        order = 50
+    )
 
     constructor(numNeurons: Int): this(List(numNeurons) { Neuron() })
 
@@ -31,24 +43,33 @@ class SoftmaxGroup(neurons: List<Neuron>): NeuronGroup(), CopyableObject {
     override fun update() {
         neuronList.forEach { it.accumulateInputs() }
         neuronList.forEach { it.update() }
-        val exponentials = neuronList.activations.map { exp(it) }
+        val exponentials = neuronList.activations.map { exp(it/params.T) }
         val total = exponentials.sum()
-        neuronList.forEachIndexed { i, n -> n.activation = exponentials[i]/total }
+        neuronList.forEachIndexed { i, n -> n.activation = (exponentials[i])/total }
     }
 
-    override fun copy() = SoftmaxGroup(neuronList.map { it.copy() })
+    override fun copy() = SoftmaxGroup(neuronList.map { it.copy() }, params.copy())
 }
 
 @CustomTypeName("Softmax")
 class SoftmaxParams: NeuronGroupParams() {
 
+    @UserParameter(
+        label = "Temperature",
+        description = """1 is default. 0 to 1 is a flatter distribution. Above 1 is a sharper distribution.""",
+        minimumValue = 0.0,
+        increment = .1,
+        order = 10)
+    var T = 1.0
+
     override fun create(): SoftmaxGroup {
-        return SoftmaxGroup(List(numNeurons) { Neuron() })
+        return SoftmaxGroup(List(numNeurons) { Neuron() }, this)
     }
 
-    override fun copy(): CopyableObject {
+    override fun copy(): SoftmaxParams {
         return SoftmaxParams().also {
             commonCopy(it)
+            it.T = T
         }
     }
 }

6a88811d125067475004c4097e2889679c293724
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2024-08-05 17:39:56 -0700

    Adding a step and return icon from Soraya

--- src/main/kotlin/org/simbrain/util/table/TableActions.kt
+++ src/main/kotlin/org/simbrain/util/table/TableActions.kt
@@ -334,7 +334,7 @@ fun SimbrainJTable.createAdvanceRowAction() = createAction(
 fun SimbrainJTable.createApplyAndAdvanceAction(applyInputs: suspend (selectedRow: Int) -> Unit) = createAction(
     name = "Apply and Advance",
     description = "Apply current row as input and increment selected row",
-    iconPath = "menu_icons/Step.png",
+    iconPath = "menu_icons/StepReturn.png",
 ) {
     initRowSelection()
     applyInputs(selectedRow)

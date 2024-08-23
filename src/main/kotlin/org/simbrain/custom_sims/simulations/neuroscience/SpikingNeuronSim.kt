084086fdbb70c099a60f70278046f97e3663f58a
Kanly Thao <kanlythao14@gmail.com>
2024-08-22 21:07:23 -0700

    Initial pass at the SpikingNeuronSim docViewer information.

--- src/main/kotlin/org/simbrain/custom_sims/simulations/neuroscience/SpikingNeuronSim.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/neuroscience/SpikingNeuronSim.kt
@@ -55,15 +55,31 @@ val spikingNeuron = newSim {
     val docViewer = addDocViewer(
         "Spiking Neuron",
         """ 
-            # Introduction
-            Increase the activation on the input neuron to increase the spike rate of the spiking neuron, and observe
-            changes in the post-synaptic response.
+           # Introduction
             
-            To change the parameters of the neuron double click on it. The online docs for Izhekevich describe many parmaeter
-            settings that produce different and interesting dynamics.
+            This is a **[spiking neuron model](https://docs.simbrain.net/docs/network/spikingneurons.html)** using the Izhekevich neuron model to model the firing events of real neurons. In this spiking neuron simulation, you can explore different
+             dynamical systems of neural firing through different parameter settings.
             
-            To change the post-synaptic response double click on the weight from the spiking neuron to the post-synaptic
-             response neuron and edit or change the spike responder.
+            # Spiking Neuron
+            
+            To explore the neuron model, double click on the **spiking neuron** and change its parameters. To get a feel of the parameters, the online doc for **[Izhekevich neuron](https://docs.simbrain.net/docs/network/neurons/izhikevich.html)** 
+            describes many parameter settings that produce different, yet interesting dynamical systems that is expressed in the top-right time series. This online doc can be accessed through the link attached before 
+            or, through the **help icon** after double clicking on the spiking neuron.
+            
+            # Post-Synaptic Response Neuron
+            
+            To change the post-synaptic response, double click on the weight from the spiking neuron to the post-synaptic response neuron. This post-synaptic response is expressed in the bottom-right time series. To see a comparison between 
+            different types of post-synaptic responses, open the **Spike Responders** simulation, a simulation showing different post-synaptic responses in one time series.
+            
+            # Other Spiking Neuron Models
+            
+            To explore different spiking neuron models, double click on the spiking neuron and change its update rule to another rule. An example spiking neuron update rule would be the
+            **[Integrate And Fire Rule](https://docs.simbrain.net/docs/network/neurons/integrateAndFire.html)**. The Integrate and Fire model is an easier, interpretable spiking neuron in comparison to the
+            Izhekevich neuron model, an alternative model. To look at other spiking neuron models, the link attached in the _Introduction_ section mentions about other examples of spiking neuron models.
+            
+            # Other Things To Change and Observe
+            
+            An additional option is to increase or decrease the activation of the **input neuron** to influence the spike rate of the spiking neuron, and observe the resulting changes in the post-synaptic response in the time series.
             
         """.trimIndent()
     )

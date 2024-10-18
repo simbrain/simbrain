12f6a5fa5031e3f48d754ac3e54e1bc5c82ea065
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2024-10-17 17:46:37 -0700

    Adding missing item from neuron array copy function

--- src/main/kotlin/org/simbrain/network/core/NeuronArray.kt
+++ src/main/kotlin/org/simbrain/network/core/NeuronArray.kt
@@ -156,6 +156,7 @@ class NeuronArray(inputSize: Int) : ArrayLayer(inputSize), EditableObject, Attri
         val copy = NeuronArray(size)
         copy.location = location
         copy.gridMode = gridMode
+        copy.circleMode = circleMode
         copy.verticalLayout = verticalLayout
         copy.activations.copyFrom(activations)
         copy.biases.copyFrom(biases)

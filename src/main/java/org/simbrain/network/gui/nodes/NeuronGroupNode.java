f078eda874d6aeb189b4d5b3608f57d167ba6fef
LeoYulinLi <LeoYulinLi@gmail.com>
2025-05-19 19:33:36 -0700

    added activation recorder for neuron collection neuron group

--- src/main/java/org/simbrain/network/gui/nodes/NeuronGroupNode.java
+++ src/main/java/org/simbrain/network/gui/nodes/NeuronGroupNode.java
@@ -223,6 +223,7 @@ public class NeuronGroupNode extends AbstractNeuronCollectionNode {
                     )
             );
         }
+        menu.add(getNetworkPanel().getNetworkActions().createRecordActivationAction(neuronGroup));
 
         // Coupling menu
         menu.addSeparator();

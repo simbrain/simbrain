dcfb2a7e040bca5c8c910f843d26a3f450e92955
jguo55 <148511131+jguo55@users.noreply.github.com>
2025-06-12 17:39:39 -0500

    Hopfield Infodoc + Dynamic Learning Rate

--- src/main/kotlin/org/simbrain/custom_sims/simulations/hebb/HopfieldPatterns.kt
+++ src/main/kotlin/org/simbrain/custom_sims/simulations/hebb/HopfieldPatterns.kt
@@ -25,7 +25,7 @@ val hopfieldPatterns = newSim {
 
     // Hopfield network
     val hopfield = Hopfield(100)
-    hopfield.learningRate = 1.0
+    var patternCnt = 0.0
     network.addNetworkModel(hopfield)
 
     addSidebarInfo(
@@ -37,18 +37,45 @@ val hopfieldPatterns = newSim {
             In this simulation you can compare how patterns that overlap more or less to see how many you get it to to learn, based on their correlations.
             
             For a more systematic approach to testing memory retrieval, try the `Discrete Hopfield` or `Continuous Hopfield` simulations, which has more features. 
-
-            # Training on One pattern         
+            
+            # Simulation Details
+            
+            ## Training
+            Each neuron in this network is connected to each other neuron using the Hebb rule ("neurons that wire
+            together, fire together"). To start, this network is initialized with all weights equal to zero.
+            When you train this network on a pattern, it takes the dot product of each neuron activation in the network with all other neuron activations in the network.
+            Even though we show 1s and 0s here, we actually use bipolar (-1s and 1s) to train and update the network.
+            This matrix is then multiplied by a learning rate, typically 1/(pattern count), and then added to the weight matrix. Note that the diagonals need to be zeroed (can you think about why?).
+            
+            ## Testing
+            Typically in a hopfield network, the neurons are updated asynchonously. They are updated randomly, one at a time for a set amount of updates.
+            However, in this example, we perform a synchronous update, updating all the neurons at once. Run the network and you will observe
+            that it will converge to some pattern (hopefully it's one that you trained!). You will also see that the network goes to a lower energy state.
+            
+            ## Patterns
+            Each pattern shown on the sidebar was compiled to have maximum orthogonality, yet still be
+            rememberable to humans.
+                        
+            # What to Do        
             
             - Select one of the patterns on the button panel. 
             - Press the train button to train the network on that pattern. Each time you press "train" it will "burn in" the pattern further.
             - Note that it will learn both the pattern and its anti-pattern.
             - To confirm the pattern is remembered, try randomzing the network with `N -> R` and then iterating by pressing space to see if the pattern is recreated.
             - You can also manually create part of the pattern you trained the network and see if it can recreate it.
+            - You can also select an already trained pattern and changing a few of the neurons to see if it will go back to the pattern.
             
-            # Other things to observe
+            # A few experiments...
             
-            When you iterate the network it tends to go to lower energy states.      
+            - How many patterns can you fit in the network before it starts to break down?
+            - How many patterns should the network theoretically be able to remember?
+            - Compare the two values. Why do you think this is? Hint: what kinds patterns do Hopfield networks remember well?
+
+            # Credits
+            
+            [Jensen Guo](https://www.linkedin.com/in/jensen-guo/) 
+            
+            [Jeff Yoshimi](www.jeffyoshimi.net)
         
         """.trimIndent()
     )
@@ -264,21 +291,21 @@ val hopfieldPatterns = newSim {
     )[i] }
 
     val patternMap = mapOf(
-        "U Pattern" to UShape,
-        "Zero Pattern" to zeroShape,
-        "D Pattern" to DShape,
-        "E Pattern" to EShape,
-        "Y Pattern" to YShape,
-        "Z Pattern" to ZShape,
-        "H Pattern" to HShape,
-        "V Pattern" to VShape,
-        "Eight Pattern" to eightShape,
         "A Pattern" to AShape,
         "B Pattern" to BShape,
-        "S Pattern" to SShape,
+        "D Pattern" to DShape,
+        "E Pattern" to EShape,
+        "H Pattern" to HShape,
+        "I Pattern" to IShape,
         "O Pattern" to OShape,
         "Q Pattern" to QShape,
-        "I Pattern" to IShape
+        "S Pattern" to SShape,
+        "U Pattern" to UShape,
+        "V Pattern" to VShape,
+        "Y Pattern" to YShape,
+        "Z Pattern" to ZShape,
+        "Zero Pattern" to zeroShape,
+        "Eight Pattern" to eightShape,
     )
     withGui {
         place(networkComponent, 249, 0, 509, 619)
@@ -291,42 +318,47 @@ val hopfieldPatterns = newSim {
             }
             addSeparator()
             addButton("Train on current pattern") {
+                hopfield.weightMatrix.setMatrixValues(hopfield.weightMatrix.weights.mul(patternCnt/(patternCnt+1)))
+                hopfield.learningRate = 1/(patternCnt+1)
                 with(network) { hopfield.trainOnCurrentPattern() }
+                patternCnt += 1
+
             }
             addButton("Show correlations") {
                 CorrPlotPanel(
-                    listOf("U",
-                        "zero",
+                    listOf("A",
+                        "B",
                         "D",
                         "E",
-                        "Y",
-                        "Z",
                         "H",
-                        "V",
-                        "eight",
-                        "A",
-                        "B",
-                        "S",
+                        "I",
                         "O",
                         "Q",
-                        "I"),
+                        "S",
+                        "U",
+                        "V",
+                        "Y",
+                        "Z",
+                        "eight",
+                        "zero",
+                        ),
                     computeCorrelationMatrix(
                         arrayOf(
-                            UShape,
-                            zeroShape,
-                            DShape,
-                            EShape,
-                            YShape,
-                            ZShape,
-                            HShape,
-                            VShape,
-                            eightShape,
                             AShape,
                             BShape,
-                            SShape,
+                            DShape,
+                            EShape,
+                            HShape,
+                            IShape,
                             OShape,
                             QShape,
-                            IShape
+                            SShape,
+                            UShape,
+                            VShape,
+                            YShape,
+                            ZShape,
+                            eightShape,
+                            zeroShape,
                         )
                     )
                 ).displayInDialog().apply {

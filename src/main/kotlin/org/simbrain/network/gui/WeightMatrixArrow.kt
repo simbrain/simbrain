a3ab2ddaa6d276608ad16fc1b7ccae5062969855
LeoYulinLi <LeoYulinLi@gmail.com>
2025-06-13 16:44:44 -0700

    fixed WeightMatrixArrow for AbstractNeuronCollectionNode

--- src/main/kotlin/org/simbrain/network/gui/WeightMatrixArrow.kt
+++ src/main/kotlin/org/simbrain/network/gui/WeightMatrixArrow.kt
@@ -3,6 +3,7 @@ package org.simbrain.network.gui
 import org.piccolo2d.PNode
 import org.simbrain.network.core.ArrayLayer
 import org.simbrain.network.gui.dialogs.NetworkPreferences
+import org.simbrain.network.gui.nodes.AbstractNeuronCollectionNode
 import org.simbrain.network.gui.nodes.WeightMatrixNode
 import org.simbrain.util.*
 import org.simbrain.util.widgets.BezierArrow
@@ -12,9 +13,19 @@ import org.simbrain.util.widgets.bezierArrow
 class WeightMatrixArrow(private val weightMatrixNode: WeightMatrixNode) : PNode() {
 
     private val source get() = weightMatrixNode.model.source
-    private val sourceNode get() = weightMatrixNode.sourceNode
+    private val sourceNodeBounds get() = with(weightMatrixNode.sourceNode) {
+        when (this) {
+            is AbstractNeuronCollectionNode -> outlinedObjects.globalFullBounds
+            else -> this.globalBounds
+        }
+    }
     private val target get() = weightMatrixNode.model.target
-    private val targetNode get() = weightMatrixNode.targetNode
+    private val targetNodeBounds get() = with(weightMatrixNode.targetNode) {
+        when (this) {
+            is AbstractNeuronCollectionNode -> outlinedObjects.globalFullBounds
+            else -> this.globalBounds
+        }
+    }
     private fun isBidirectional() = target.outgoingConnectors.any { it.target == source }
 
     private val arrow = if (source == target) {
@@ -49,8 +60,8 @@ class WeightMatrixArrow(private val weightMatrixNode: WeightMatrixNode) : PNode(
 
     override fun layoutChildren() {
         when (arrow) {
-            is RecurrentArrow -> arrow.layout(sourceNode.globalBounds.centerLeft + point(15, 0)) { (x, y) -> weightMatrixNode.imageBox.centerFullBoundsOnPoint(x, y) }
-            is BezierArrow -> arrow.layout(sourceNode.globalBounds.outlines, targetNode.globalBounds.outlines, isBidirectional())
+            is RecurrentArrow -> arrow.layout(sourceNodeBounds.centerLeft + point(15, 0)) { (x, y) -> weightMatrixNode.imageBox.centerFullBoundsOnPoint(x, y) }
+            is BezierArrow -> arrow.layout(sourceNodeBounds.outlines, targetNodeBounds.outlines, isBidirectional())
         }
     }
 }

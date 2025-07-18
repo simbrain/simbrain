7be518e17c10a9433f613c931bdaf6e81b2132cd
Jeff Yoshimi <jeffyoshimi@yahoo.com>
2025-07-18 10:13:25 -0700

    Removing lingering headers in kt files

--- src/main/java/org/simbrain/world/odorworld/dialogs/AddEffectorDialog.java
+++ src/main/java/org/simbrain/world/odorworld/dialogs/AddEffectorDialog.java
@@ -1,16 +1,3 @@
-/*
- * Part of Simbrain--a java-based neural network kit Copyright (C) 2005,2007 The
- * Authors. See http://www.simbrain.net/credits This program is free software;
- * you can redistribute it and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version. This program is
- * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
- * should have received a copy of the GNU General Public License along with this
- * program; if not, write to the Free Software Foundation, Inc., 59 Temple Place
- * - Suite 330, Boston, MA 02111-1307, USA.
- */
 package org.simbrain.world.odorworld.dialogs;
 
 import org.simbrain.util.StandardDialog;
@@ -35,7 +22,7 @@ public class AddEffectorDialog extends StandardDialog {
     /**
      * Entity to which effector is being added.
      */
-    private OdorWorldEntity entity;
+    private final OdorWorldEntity entity;
 
     /**
      * Main editing panel.
@@ -45,7 +32,7 @@ public class AddEffectorDialog extends StandardDialog {
     /**
      * Main dialog box.
      */
-    private Box mainPanel = Box.createVerticalBox();
+    private final Box mainPanel = Box.createVerticalBox();
 
     /**
      * Effector Dialog add effector constructor.

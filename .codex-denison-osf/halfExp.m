f4d73d4d840ef49fb9d621e6c1dbfa359f5fffbb
Jensen Guo <jguoprof@gmail.com>
2026-07-02 17:04:04 -0700

    remove extraneous files

--- .codex-denison-osf/halfExp.m
+++
@@ -1,7 +0,0 @@
-function [x] = halfExp(base,n)
-
-if ~exist('n','var')
-    n=1;
-end
-
-x = (max(0,base)).^n;
--- .codex-denison-osf/makePrefilter.m
+++
@@ -1,5 +0,0 @@
-function h = makePrefilter(x, posShape, posScale, negShape, negScale, ampNeg)
-
-hPos = makeGamma(x,[],posShape,posScale,1);
-hNeg = makeGamma(x,[],negShape,negScale,ampNeg);
-h = hPos - hNeg;
